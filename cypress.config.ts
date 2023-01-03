5e9cad5ced0c617664c7f998ca86abc886d7ba8e
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-01-03 01:38:22 -0800

    ci(cypress): enable e2e test recording

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    projectId: '9dj6b9',
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
       const port = process.env.PORT ?? (isDev ? '3000' : '8811');
