9e1ebb7b8957dc780c1a7707404550e785d86d62
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-01-04 18:53:49 -0800

    feat: use custom univers font family
    This is the font that used to be used on the MacBook keyboard and is
    currently used on Teenage Engineering devices. To learn more:
    
    See: https://www.fonts.com/font/linotype/univers/story

--- app/root.tsx
+++ app/root.tsx
@@ -12,9 +12,52 @@ import { json } from '@remix-run/node';
 import { getUser } from './session.server';
 import tailwindStylesheetUrl from './styles/tailwind.css';
 
-export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: tailwindStylesheetUrl }];
-};
+export const links: LinksFunction = () => [
+  {
+    rel: 'preload',
+    href: '/fonts/875712e3-9c93-4f0b-a46a-a278e69a71f6.woff',
+    crossOrigin: 'anonymous',
+    type: 'font/woff',
+    as: 'font',
+  },
+  {
+    rel: 'preload',
+    href: '/fonts/a1c4330f-ba73-4f1f-aa05-bd237c58ba51.woff',
+    crossOrigin: 'anonymous',
+    type: 'font/woff',
+    as: 'font',
+  },
+  {
+    rel: 'preload',
+    href: '/fonts/TE20L-0A212F48-0D4B-43DA-A24C-F36D66965FA4.woff2',
+    crossOrigin: 'anonymous',
+    type: 'font/woff2',
+    as: 'font',
+  },
+  {
+    rel: 'preload',
+    href: '/fonts/TE20T-0A212F48-0D4B-43DA-A24C-F36D66965FA4.woff2',
+    crossOrigin: 'anonymous',
+    type: 'font/woff2',
+    as: 'font',
+  },
+  {
+    rel: 'preload',
+    href: '/fonts/TE40L-0A212F48-0D4B-43DA-A24C-F36D66965FA4.woff2',
+    crossOrigin: 'anonymous',
+    type: 'font/woff2',
+    as: 'font',
+  },
+  {
+    rel: 'preload',
+    href: '/fonts/TE40T-0A212F48-0D4B-43DA-A24C-F36D66965FA4.woff2',
+    crossOrigin: 'anonymous',
+    type: 'font/woff2',
+    as: 'font',
+  },
+  { rel: 'stylesheet', href: '/fonts.css' },
+  { rel: 'stylesheet', href: tailwindStylesheetUrl },
+];
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./app/**/*.{ts,tsx,jsx,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        'UniversTE20T',
+        'UnicodeT',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+        'sans-serif',
+      ],
+    },
   },
   plugins: [],
 };
