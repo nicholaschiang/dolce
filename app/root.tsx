33596cc88d217709b9e7774422515da04215e43d
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-01-04 19:40:34 -0800

    feat(app/root): update website title in meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -83,7 +83,7 @@ export const links: LinksFunction = () => [
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'Remix Notes',
+  title: 'nicholas engineering',
   viewport: 'width=device-width,initial-scale=1',
 });
 
