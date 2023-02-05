02c09eb7ecd0279316a4b91bb97d460beedc65e2
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-02-04 18:31:45 -0700

    feat(layout): center header text and link to portfolio
    This commit centers the header text and adds "a project by nicholas
    chiang" subtitle (that links to my current portfolio site).

--- app/routes/__layout.tsx
+++ app/routes/__layout.tsx
@@ -1,32 +1,29 @@
 import { Link, Outlet } from '@remix-run/react';
-import { LightningBoltIcon } from '@radix-ui/react-icons';
 
 import ThemeSwitcher from '~/components/theme-switcher';
 
 function Header() {
   return (
-    <header className='flex shrink-0 items-center justify-between border-b border-gray-200 bg-white/75 px-12 py-3 backdrop-blur-lg dark:border-gray-700 dark:bg-gray-900/75'>
-      <Link
-        to='/'
-        prefetch='intent'
-        className='flex items-center justify-center'
-      >
-        <h1 className='leading-none'>nicholas.engineering</h1>
-        <LightningBoltIcon className='ml-1.5 mr-0.5 h-3 w-3 text-gray-400 dark:text-gray-500' />
-        <p className='mt-px text-3xs leading-none text-gray-400 dark:text-gray-500'>
-          {new Date().toLocaleString(undefined, {
-            month: 'numeric',
-            day: 'numeric',
-            year: 'numeric',
-            timeZoneName: 'short',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
-            hour12: false,
-          })}
-        </p>
-      </Link>
-      <ThemeSwitcher />
+    <header className='relative shrink-0 border-b border-gray-200 bg-white/75 px-12 py-3 text-center backdrop-blur-lg dark:border-gray-700 dark:bg-gray-900/75'>
+      <h1 className='leading-none'>
+        <Link to='/' prefetch='intent'>
+          nicholas.engineering
+        </Link>
+      </h1>
+      <p className='mt-px text-3xs leading-none text-gray-400 dark:text-gray-500'>
+        a project by{' '}
+        <a
+          href='https://nicholaschiang.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='underline'
+        >
+          nicholas chiang
+        </a>
+      </p>
+      <div className='absolute inset-y-0 right-12 flex items-center'>
+        <ThemeSwitcher />
+      </div>
     </header>
   );
 }
