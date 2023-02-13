f2a922e0dde3d8d6afcd4df2e53fd5eaaa3143de
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-02-12 21:27:42 -0700

    feat(routes): add basic product placeholder page

---
+++ app/routes/__layout/products/$productId.tsx
@@ -0,0 +1,31 @@
+import type { LoaderFunction } from '@remix-run/node'
+import type { Product } from '@prisma/client'
+import invariant from 'tiny-invariant'
+import { useLoaderData } from '@remix-run/react'
+
+import { prisma } from 'db.server'
+
+export type LoaderData = Product
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.productId, 'productId is required')
+  const id = Number(params.productId)
+  if (Number.isNaN(id))
+    throw new Response('productId must be a number', { status: 400 })
+  const product = await prisma.product.findUnique({ where: { id } })
+  if (product === undefined) throw new Response('Not Found', { status: 404 })
+  return product
+}
+
+export default function ProductPage() {
+  const product = useLoaderData<LoaderData>()
+  return (
+    <main className='flex h-full w-full items-center justify-center text-center'>
+      <h1>{product.name}</h1>
+      <span className='mx-2'>·</span>
+      <p>{product.level}</p>
+      <span className='mx-2'>·</span>
+      <p>${product.msrp}</p>
+    </main>
+  )
+}
