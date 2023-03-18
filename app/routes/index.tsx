a7e109d947c4c227920fc5d8104fdcb737dd53b5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-03-18 17:29:35 -0600

    feat(life): add weeks in a lifetime diagram files
    This commit adds a "weeks in your lifetime" diagram that was heavily
    inspired by [[1]] and [[2]]. The diagram is available in PDF, SVG, PNG,
    JPG, and the original Adobe Illustrator file formats.
    
    [1]: https://waitbutwhy.com/2014/05/life-weeks.html
    [2]: https://www.nytimes.com/2022/02/25/opinion/covid-pandemic-depressing-math.html?unlocked_article_code=AAAAAAAAAAAAAAAACEIPuomT1JKd6J17Vw1cRCfTTMQmqxCdw_PIxftm3iWka3DPDmwfiOMNAo6B_EGKbq19Ztd2xz2dSNJEMbRqSPFq1OJEJE1rREytq4eE3pQZJiF_4aSCYlQL5bOfF7Yp7W2tKWCjNOZ0wLD44B6NPWHuDPfA1yYkcRJip8NnaQmg2ScXxvrGFe902tF13PtyFYk6EWlbHFSCu_bpDxh5Pt-UPFqLukRtBbYvCXyElsWc6rkAbAxSFVnBKXt-6mw4495ZU8gFaOe9d1VzPZqj3shCTzBgP4yrBJYuRoLLlLsMsbTFqxOEzcTe1OyY6A2M02VgYd6vBV1brKMp

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,10 @@
-import { ArrowTopRightIcon, CardStackIcon } from '@radix-ui/react-icons'
+import {
+  ArrowTopRightIcon,
+  CardStackIcon,
+  PersonIcon,
+} from '@radix-ui/react-icons'
+import type { FunctionComponent } from 'react'
+import type { IconProps } from '@radix-ui/react-icons/dist/types'
 import { Link } from '@remix-run/react'
 
 import { ThemeSwitcher } from 'components/theme-switcher'
@@ -14,9 +20,20 @@ interface MilestoneProps {
   title: string
   subtitle: string
   url?: string
+  icon?: FunctionComponent<IconProps>
+  openInNew?: boolean
 }
 
-function Milestone({ start, end, title, subtitle, url }: MilestoneProps) {
+function Milestone({
+  start,
+  end,
+  title,
+  subtitle,
+  url,
+  icon,
+  openInNew,
+}: MilestoneProps) {
+  const Icon = icon ?? ArrowTopRightIcon
   return (
     <article className='before:content-[" "] relative m-12 text-lg leading-none before:absolute before:-left-6.5 before:top-1/2 before:h-1 before:w-1 before:-translate-y-1/2 before:rounded-full before:bg-gray-300 after:absolute after:-left-[calc(1.5rem_+_0.5px)] after:top-1/2 after:h-[calc(100%_+_3rem)] after:border-l after:border-gray-300 last:after:hidden dark:before:bg-gray-100 dark:after:border-gray-100'>
       <p className='text-xs lowercase text-gray-900/50 dark:text-gray-100/50'>
@@ -25,17 +42,17 @@ function Milestone({ start, end, title, subtitle, url }: MilestoneProps) {
       </p>
       <h1 className='my-2 font-semibold'>
         {!url && title}
-        {url?.startsWith('/') && (
+        {url && !openInNew && (
           <Link
             to={url}
             prefetch='intent'
             className='items-top link group flex underline'
           >
             {title}
-            <CardStackIcon className='ml-1 inline-block h-4 w-4 text-gray-400 dark:text-gray-500' />
+            <Icon className='ml-1 inline-block h-4 w-4 text-gray-400 dark:text-gray-500' />
           </Link>
         )}
-        {url?.startsWith('http') && (
+        {url && openInNew && (
           <a
             href={url}
             target='_blank'
@@ -43,7 +60,7 @@ function Milestone({ start, end, title, subtitle, url }: MilestoneProps) {
             className='items-top link group flex underline'
           >
             {title}
-            <ArrowTopRightIcon className='inline-block h-4 w-4 text-gray-400 dark:text-gray-500' />
+            <Icon className='ml-1 inline-block h-4 w-4 text-gray-400 dark:text-gray-500' />
           </a>
         )}
       </h1>
@@ -60,22 +77,33 @@ export default function IndexPage() {
       </div>
       <section>
         <Milestone
+          start={new Date(2023, 3, 18)}
+          title='/life'
+          subtitle='a graphical depiction'
+          url='/life/life.pdf'
+          icon={PersonIcon}
+          openInNew
+        />
+        <Milestone
           start={new Date(2023, 0, 2)}
           title='/products'
           subtitle='a fashion archive'
           url='/products'
+          icon={CardStackIcon}
         />
         <Milestone
           start={new Date(2023, 0, 1)}
           title='nicholas.engineering'
           subtitle='yet another portfolio'
           url='https://github.com/nicholaschiang/site'
+          openInNew
         />
         <Milestone
           start={new Date(2022, 5, 1)}
           title='numbersstation.ai'
           subtitle='a dbt generator'
           url='https://numbersstation.ai'
+          openInNew
         />
         <Milestone
           start={new Date(2022, 2, 1)}
@@ -83,6 +111,7 @@ export default function IndexPage() {
           title='tweetscape.co'
           subtitle='a twitter client'
           url='https://github.com/rooteco/tweetscape'
+          openInNew
         />
         <Milestone
           start={new Date(2021, 3, 1)}
@@ -90,6 +119,7 @@ export default function IndexPage() {
           title='readhammock.com'
           subtitle='a newsletter reader'
           url='https://github.com/nicholaschiang/hammock'
+          openInNew
         />
         <Milestone
           start={new Date(2019, 1, 1)}
@@ -97,6 +127,7 @@ export default function IndexPage() {
           title='tutorbook.org'
           subtitle='a booking platform'
           url='https://github.com/tutorbookapp/tutorbook'
+          openInNew
         />
         <Milestone
           start={new Date(2018, 8, 1)}
@@ -104,6 +135,7 @@ export default function IndexPage() {
           title='sing.stanford.edu'
           subtitle='some ml research'
           url='https://dl.acm.org/doi/10.1145/3391906?cid=99659274049'
+          openInNew
         />
       </section>
     </main>
