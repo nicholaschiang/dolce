nicholaschiang/dolce

commit 006c7469543ebe7daa113a175d7dbcb0887fbb1e

feat(products): show secondary image on hover
This patch updates the product pages for the new variant-based images
and adds some CSS-based image pagination on the products list where the
second image appears on hover.

app/routes/__layout/products.tsx
@@ -57,13 +57,17 @@ async function getProducts({ request }: LoaderArgs) {
   )
   const products = (
     await prisma.product.findMany({
-      include: { images: true },
+      include: { variants: { include: { images: true } } },
       where: { [join]: filters.map(filterToPrismaWhere) },
+      take: 100,
     })
   ).map((product) => ({
     id: product.id,
     name: product.name,
-    images: product.images.map((image) => image.url),
+    images: product.variants
+      .map((v) => v.images)
+      .flat()
+      .map((image) => image.url),
     // real users don't care about cents. most reputable brands won't include
     // cents in their prices anyway. prices that do include cents are usually
     // intended to be misleading (e.g. $69.70 instead of $70).
@@ -89,11 +93,7 @@ export async function loader(args: LoaderArgs) {
 }
 
 // Don't allow users to filter on back-end only fields.
-const hiddenFields: (keyof Prisma.ProductSelect)[] = [
-  'prices',
-  'videos',
-  'images',
-]
+const hiddenFields: (keyof Prisma.ProductSelect)[] = []
 
 // There must be at least one product per row.
 const minResultsPerRow = 1
@@ -287,7 +287,7 @@ function ProductItem({
   const location = useLocation()
   return (
     <li
-      className='shrink-0 grow-0'
+      className='group shrink-0 grow-0'
       style={{ flexBasis: `${(1 / resultsPerRow) * 100}%` }}
     >
       <div className='relative m-2'>
@@ -296,13 +296,24 @@ function ProductItem({
           style={{ paddingTop: `${widthToHeightImageRatio * 100}%` }}
         >
           <Image
-            className='absolute top-0 h-full w-full overflow-hidden rounded-md bg-gray-100 dark:bg-gray-800'
+            className='absolute top-0 z-20 h-full w-full overflow-hidden rounded-md bg-gray-100 opacity-0 transition-opacity duration-300 group-hover:opacity-100 dark:bg-gray-800'
+            loading='lazy'
+            decoding='async'
+            src={images[1]}
+            responsive={[200, 300, 400, 500, 600, 700, 800, 900, 1000].map(
+              (width) => ({
+                size: { width, height: width * widthToHeightImageRatio },
+                maxWidth: width * resultsPerRow,
+              }),
+            )}
+          />
+          <Image
+            className='absolute top-0 z-10 h-full w-full overflow-hidden rounded-md bg-gray-100 dark:bg-gray-800'
             loading={index < resultsPerRow * rowsToEagerLoad ? 'eager' : 'lazy'}
             decoding={
               index < resultsPerRow * rowsToEagerLoad ? 'sync' : 'async'
             }
-            src={images.slice(-1)[0]}
-            data-image={images.slice(-1)[0]}
+            src={images[0]}
             responsive={[200, 300, 400, 500, 600, 700, 800, 900, 1000].map(
               (width) => ({
                 size: { width, height: width * widthToHeightImageRatio },
@@ -313,7 +324,7 @@ function ProductItem({
         </div>
         <Link prefetch='intent' to={`${id}${location.search}`}>
           <div
-            className='relative mb-2 rounded-md'
+            className='relative mb-2 rounded-md z-30'
             style={{ paddingTop: `${widthToHeightImageRatio * 100}%` }}
           />
           <h2 className='leading-none'>{name}</h2>

app/components/dialog.tsx
@@ -21,10 +21,10 @@ export function Dialog({
     <DialogPrimitive.Root open={open} onOpenChange={onOpenChange}>
       <DialogPrimitive.Trigger />
       <DialogPrimitive.Portal>
-        <DialogPrimitive.Overlay className='fixed inset-0 bg-white/50 dark:bg-gray-900/50' />
+        <DialogPrimitive.Overlay className='fixed inset-0 z-40 bg-white/50 dark:bg-gray-900/50' />
         <DialogPrimitive.Content
           className={cn(
-            'center fixed overflow-hidden rounded-xl border border-gray-200/50 bg-white shadow-2xl focus:outline-none dark:border-gray-700/50 dark:bg-gray-900',
+            'center fixed z-50 overflow-hidden rounded-xl border border-gray-200/50 bg-white shadow-2xl focus:outline-none dark:border-gray-700/50 dark:bg-gray-900',
             className,
           )}
           onOpenAutoFocus={onOpenAutoFocus}

app/components/filters.tsx
@@ -227,6 +227,7 @@ function AddFilterButton({ model, hiddenFields }: AddFilterButtonProps) {
       <Popover.Portal>
         <Popover.Content
           align='start'
+          className='z-50'
           onCloseAutoFocus={(event) => event.preventDefault()}
         >
           <MenuContext.Provider value={setOpen}>

app/components/menu.tsx
@@ -12,7 +12,7 @@ export type RootProps = {
 
 export function Root({ children }: RootProps) {
   return (
-    <Command className='frosted fixed z-40 mt-0.5 flex min-w-min max-w-xl origin-top-left flex-col overflow-hidden rounded-lg border border-gray-200 text-xs shadow-xl will-change-transform dark:border-gray-700'>
+    <Command className='frosted fixed mt-0.5 flex min-w-min max-w-xl origin-top-left flex-col overflow-hidden rounded-lg border border-gray-200 text-xs shadow-xl will-change-transform dark:border-gray-700'>
       {children}
     </Command>
   )

app/routes/__layout/products/$productId.tsx
@@ -28,14 +28,12 @@ export async function loader({ params }: LoaderArgs) {
   const product = await prisma.product.findUnique({
     where: { id },
     include: {
-      images: true,
       sizes: true,
-      variants: true,
+      variants: { include: { images: true, prices: true } },
       styles: true,
       collections: true,
       designers: true,
       brands: true,
-      prices: true,
     },
   })
   if (product === null) throw new Response('Not Found', { status: 404 })
@@ -101,6 +99,7 @@ export default function ProductPage() {
     },
     [nav, productIds, location.search],
   )
+  const images = product.variants.map((v) => v.images).flat()
   return (
     <Dialog
       open
@@ -145,8 +144,7 @@ export default function ProductPage() {
           className='w-0 flex-1 overflow-hidden rounded-md bg-gray-100 dark:bg-gray-800'
           loading='eager'
           decoding='sync'
-          src={product.images.slice(-1)[0].url}
-          data-image={product.images.slice(-1)[0].url}
+          src={images[0].url}
           responsive={[200, 300, 400, 500, 600, 700, 800, 900, 1000].map(
             (width) => ({
               size: { width, height: width * widthToHeightImageRatio },
@@ -160,11 +158,7 @@ export default function ProductPage() {
               {product.level}
             </p>
             <Dialog.Title className='text-2xl'>{product.name}</Dialog.Title>
-            <p className='text-2xs'>
-              This slim-fit turtleneck made of superfine wool is decorated with
-              the jacquard lettering logo at the bottom of the neck for an
-              iconic, minimalist touch.
-            </p>
+            <p className='text-2xs'>{product.description}</p>
           </Section>
           <Section>
             <Section.Title>

commit 7bbafe17b96ab09430d17f771823def6846ed58a

fix(app/products): only show hover image if it exists
This patch updates the hover image to only appear if there are more than
one image associated with a product and its variants.

Fixes: 006c7469543e ("feat(products): show secondary image on hover")

app/routes/__layout/products.tsx
@@ -295,18 +295,20 @@ function ProductItem({
           className='absolute w-full'
           style={{ paddingTop: `${widthToHeightImageRatio * 100}%` }}
         >
-          <Image
-            className='absolute top-0 z-20 h-full w-full overflow-hidden rounded-md bg-gray-100 opacity-0 transition-opacity duration-300 group-hover:opacity-100 dark:bg-gray-800'
-            loading='lazy'
-            decoding='async'
-            src={images[1]}
-            responsive={[200, 300, 400, 500, 600, 700, 800, 900, 1000].map(
-              (width) => ({
-                size: { width, height: width * widthToHeightImageRatio },
-                maxWidth: width * resultsPerRow,
-              }),
-            )}
-          />
+          {images.length > 1 && (
+            <Image
+              className='absolute top-0 z-20 h-full w-full overflow-hidden rounded-md bg-gray-100 opacity-0 transition-opacity duration-300 group-hover:opacity-100 dark:bg-gray-800'
+              loading='lazy'
+              decoding='async'
+              src={images[1]}
+              responsive={[200, 300, 400, 500, 600, 700, 800, 900, 1000].map(
+                (width) => ({
+                  size: { width, height: width * widthToHeightImageRatio },
+                  maxWidth: width * resultsPerRow,
+                }),
+              )}
+            />
+          )}
           <Image
             className='absolute top-0 z-10 h-full w-full overflow-hidden rounded-md bg-gray-100 dark:bg-gray-800'
             loading={index < resultsPerRow * rowsToEagerLoad ? 'eager' : 'lazy'}
@@ -324,7 +326,7 @@ function ProductItem({
         </div>
         <Link prefetch='intent' to={`${id}${location.search}`}>
           <div
-            className='relative mb-2 rounded-md z-30'
+            className='relative z-30 mb-2 rounded-md'
             style={{ paddingTop: `${widthToHeightImageRatio * 100}%` }}
           />
           <h2 className='leading-none'>{name}</h2>