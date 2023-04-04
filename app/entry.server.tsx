fee9204a34f95ae18e4b8762eff438ac1ceee0df
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-04-04 15:20:12 -0600

    feat: use `vercel` built-in image optimization
    This patch updates my `<Image>` component to use the Vercel built-in
    image optimization features [1] [2] [3] [4].
    
    [1]: https://github.com/theMosaad/remix-vercel-image/blob/main/app/routes/index.tsx
    [2]: https://vercel.com/docs/concepts/image-optimization
    [3]: https://vercel.com/docs/build-output-api/v3/configuration#images
    [4]: https://github.com/vercel/vercel/pull/8566

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,54 +1,17 @@
-import { PassThrough } from 'stream'
-
 import type { EntryContext } from '@vercel/remix'
 import { RemixServer } from '@remix-run/react'
-import { Response } from '@vercel/remix'
-import isbot from 'isbot'
-import { renderToPipeableStream } from 'react-dom/server'
+import { handleRequest } from '@vercel/remix'
 
-const ABORT_DELAY = 5000
-
-export default function handleRequest(
+export default function entry(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const callbackName = isbot(request.headers.get('user-agent'))
-    ? 'onAllReady'
-    : 'onShellReady'
-
-  return new Promise((resolve, reject) => {
-    let didError = false
-
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        [callbackName]: () => {
-          const body = new PassThrough()
-
-          responseHeaders.set('Content-Type', 'text/html')
-
-          resolve(
-            new Response(body, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            }),
-          )
-
-          pipe(body)
-        },
-        onShellError: (err: unknown) => {
-          reject(err)
-        },
-        onError: (error: unknown) => {
-          didError = true
-
-          console.error(error)
-        },
-      },
-    )
-
-    setTimeout(abort, ABORT_DELAY)
-  })
+  return handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    <RemixServer context={remixContext} url={request.url} />,
+  )
 }
