4c77c7e402a6b221fcc172ca4e0c9363b861ff00
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-04-06 13:02:40 -0600

    feat: report real web vital analytics to vercel
    This patch implements web vital reporting to Vercel [1] [2].
    
    [1]: https://vercel.com/docs/concepts/analytics/api#getting-started
    [2]: https://github.com/vercel/vercel/blob/main/examples/create-react-app/src/reportWebVitals.js

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,3 +1,4 @@
+import type { Metric, ReportCallback } from 'web-vitals'
 import { StrictMode, startTransition } from 'react'
 import { RemixBrowser } from '@remix-run/react'
 import { hydrateRoot } from 'react-dom/client'
@@ -22,4 +23,58 @@ if (window.requestIdleCallback) {
   // Safari doesn't support requestIdleCallback
   // https://caniuse.com/requestidlecallback
   window.setTimeout(hydrate, 1)
+}
+
+reportWebVitals(sendToVercelAnalytics)
+
+function getConnectionSpeed() {
+  return 'connection' in navigator &&
+    typeof navigator.connection === 'object' &&
+    navigator.connection !== null &&
+    'effectiveType' in navigator.connection &&
+    typeof navigator.connection.effectiveType === 'string'
+    ? navigator.connection.effectiveType
+    : ''
+}
+
+function sendToVercelAnalytics(metric: Metric) {
+  const analyticsId = window.env.VERCEL_ANALYTICS_ID
+  if (!analyticsId) return
+  const body = {
+    dsn: analyticsId,
+    id: metric.id,
+    page: window.location.pathname,
+    href: window.location.href,
+    event_name: metric.name,
+    value: metric.value.toString(),
+    speed: getConnectionSpeed(),
+  }
+  const vitalsUrl = 'https://vitals.vercel-analytics.com/v1/vitals'
+  const blob = new Blob([new URLSearchParams(body).toString()], {
+    // This content type is necessary for `sendBeacon`
+    type: 'application/x-www-form-urlencoded',
+  })
+  if (navigator.sendBeacon) navigator.sendBeacon(vitalsUrl, blob)
+  else
+    void fetch(vitalsUrl, {
+      body: blob,
+      method: 'POST',
+      credentials: 'omit',
+      keepalive: true,
+    })
+}
+
+function reportWebVitals(onPerfEntry: ReportCallback) {
+  if (onPerfEntry && onPerfEntry instanceof Function)
+    void import('web-vitals').then(
+      ({ onCLS, onFCP, onFID, onINP, onLCP, onTTFB }) => {
+        onCLS(onPerfEntry)
+        onFCP(onPerfEntry)
+        onFID(onPerfEntry)
+        onINP(onPerfEntry)
+        onLCP(onPerfEntry)
+        onTTFB(onPerfEntry)
+        return null
+      },
+    )
 }
