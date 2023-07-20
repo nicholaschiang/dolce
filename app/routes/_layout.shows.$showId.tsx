1352cf87bebd77038ebd54a0712954b31323fa19
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-07-19 21:39:43 -0600

    fix(routes/show): move about to the left
    This patch switches the "about" panel (with the scores and collection
    title) to the left side as most users will be reading left-to-right.
    Eventually, I should decide which direction to put these based on the
    LTR / RTL orientation of the user's i18n. But that'll only be added once
    I have real i18n setup... which will likely be never. :)

--- app/routes/_layout.shows.$showId.tsx
+++ app/routes/_layout.shows.$showId.tsx
@@ -9,6 +9,7 @@ import { Textarea } from 'atoms/Textarea'
 
 import { prisma } from 'db.server'
 import { log } from 'log.server'
+import { cn } from 'utils/cn'
 
 export async function loader({ params }: LoaderArgs) {
   log.debug('getting show...')
@@ -31,104 +32,114 @@ export async function loader({ params }: LoaderArgs) {
 }
 
 export default function ShowPage() {
+  return (
+    <main className='h-full flex-1 overflow-hidden max-w-screen-xl mx-auto grid grid-cols-5 gap-6 px-6'>
+      <About className='col-span-3 py-6' />
+      <Looks className='col-span-2 py-6' />
+    </main>
+  )
+}
+
+function About({ className }: { className: string }) {
   const show = useLoaderData<typeof loader>()
   return (
-    <main className='h-full flex-1 overflow-hidden max-w-screen-xl mx-auto grid grid-cols-5'>
-      <div className='overflow-auto col-span-2 pl-6 pr-3 py-6'>
-        <header className='mb-4 bg-white dark:bg-gray-900'>
-          <h1 className='text-center uppercase text-xl'>Looks</h1>
-        </header>
-        <ol className='grid grid-cols-2 gap-x-2 gap-y-6'>
-          {show.looks.map((look) => (
-            <li key={look.id}>
-              <img src={look.image.url} alt={`Look ${look.number}`} />
-              <p className='mt-0.5 text-sm'>Look {look.number}</p>
-            </li>
-          ))}
-        </ol>
-      </div>
-      <div className='overflow-auto col-span-3 grid gap-10 py-6 pr-6 pl-3'>
-        <div className='grid gap-2'>
-          <video
-            className='aspect-video w-full bg-gray-100 dark:bg-gray-800'
-            controls
-            autoPlay
-            playsInline
-            muted
-          >
-            <source src={show.video.url} type={show.video.mimeType} />
-            Download the <a href={show.video.url}>MP4</a> video.
-          </video>
-          <div className='flex gap-2'>
-            <div className='flex-none w-40 bg-gray-100 dark:bg-gray-800 flex items-center justify-center overflow-hidden'>
-              <img src={show.looks[0].image.url} alt='' />
-            </div>
-            <article className='flex-1 bg-gray-100 dark:bg-gray-800 text-center p-6 flex flex-col justify-center'>
-              <h1 className='font-serif font-bold text-5xl mb-1'>
-                {show.brands.map((brand) => brand.name).join(', ')}
-              </h1>
-              <h2 className='uppercase mb-6 text-sm'>
-                {show.season.name.replace('_', '-')} {show.season.year}{' '}
-                READY-TO-WEAR
-              </h2>
-              <ul className='grid grid-cols-2 gap-2'>
-                <Score value={0.82} label='Critic Score' count='339 Reviews' />
-                <Score
-                  value={0.94}
-                  label='Consumer Score'
-                  count='10,000+ Ratings'
-                />
-              </ul>
-            </article>
+    <div className={cn('overflow-auto grid gap-10', className)}>
+      <div className='grid gap-2'>
+        <video
+          className='aspect-video w-full bg-gray-100 dark:bg-gray-800'
+          controls
+          autoPlay
+          playsInline
+          muted
+        >
+          <source src={show.video.url} type={show.video.mimeType} />
+          Download the <a href={show.video.url}>MP4</a> video.
+        </video>
+        <div className='flex gap-2'>
+          <div className='flex-none w-40 bg-gray-100 dark:bg-gray-800 flex items-center justify-center overflow-hidden'>
+            <img src={show.looks[0].image.url} alt='' />
           </div>
+          <article className='flex-1 bg-gray-100 dark:bg-gray-800 text-center p-6 flex flex-col justify-center'>
+            <h1 className='font-serif font-bold text-5xl mb-1'>
+              {show.brands.map((brand) => brand.name).join(', ')}
+            </h1>
+            <h2 className='uppercase mb-6 text-sm'>
+              {show.season.name.replace('_', '-')} {show.season.year}{' '}
+              READY-TO-WEAR
+            </h2>
+            <ul className='grid grid-cols-2 gap-2'>
+              <Score value={0.82} label='Critic Score' count='339 Reviews' />
+              <Score
+                value={0.94}
+                label='Consumer Score'
+                count='10,000+ Ratings'
+              />
+            </ul>
+          </article>
         </div>
-        <Section header='What to know'>
-          <Subheader>Critics Consensus</Subheader>
-          {show.criticReviewSummary ? (
-            <p className='mb-2'>{show.criticReviewSummary}</p>
-          ) : (
-            <Empty className='mb-2'>
-              There is no Critics Consensus because there are not enough reviews
-              yet.
-            </Empty>
-          )}
-          <Subheader>Consumers Say</Subheader>
-          {show.consumerReviewSummary ? (
-            <p>{show.consumerReviewSummary}</p>
-          ) : (
-            <Empty>
-              There is no Consumer Summary because there are not enough reviews
-              yet.
-            </Empty>
-          )}
-        </Section>
-        <Section header='Collection info'>
-          <article>{show.description}</article>
-        </Section>
-        <Section header='Rate and review'>
-          <div className='mt-2 grid gap-2'>
-            <Textarea placeholder='What did you think of the collection? (optional)' />
-            <Button>Submit</Button>
-          </div>
-        </Section>
-        <Section header={`Critic reviews for ${show.name}`}>
-          <ol className='mt-2 grid gap-4'>
-            {show.reviews
-              .filter((r) => r.publication != null && r.url != null)
-              .map((review) => (
-                <li key={review.id}>
-                  <Review
-                    author={review.author}
-                    publication={review.publication?.name as string}
-                    url={review.url as string}
-                    content={review.content}
-                  />
-                </li>
-              ))}
-          </ol>
-        </Section>
       </div>
-    </main>
+      <Section header='What to know'>
+        <Subheader>Critics Consensus</Subheader>
+        {show.criticReviewSummary ? (
+          <p className='mb-2'>{show.criticReviewSummary}</p>
+        ) : (
+          <Empty className='mb-2'>
+            There is no Critics Consensus because there are not enough reviews
+            yet.
+          </Empty>
+        )}
+        <Subheader>Consumers Say</Subheader>
+        {show.consumerReviewSummary ? (
+          <p>{show.consumerReviewSummary}</p>
+        ) : (
+          <Empty>
+            There is no Consumer Summary because there are not enough reviews
+            yet.
+          </Empty>
+        )}
+      </Section>
+      <Section header='Collection info'>
+        <article>{show.description}</article>
+      </Section>
+      <Section header='Rate and review'>
+        <div className='mt-2 grid gap-2'>
+          <Textarea placeholder='What did you think of the collection? (optional)' />
+          <Button>Submit</Button>
+        </div>
+      </Section>
+      <Section header={`Critic reviews for ${show.name}`}>
+        <ol className='mt-2 grid gap-4'>
+          {show.reviews
+            .filter((r) => r.publication != null && r.url != null)
+            .map((review) => (
+              <li key={review.id}>
+                <Review
+                  author={review.author}
+                  publication={review.publication?.name as string}
+                  url={review.url as string}
+                  content={review.content}
+                />
+              </li>
+            ))}
+        </ol>
+      </Section>
+    </div>
+  )
+}
+
+function Looks({ className }: { className: string }) {
+  const show = useLoaderData<typeof loader>()
+  return (
+    <div className={cn('overflow-auto', className)}>
+      <ol className='grid grid-cols-2 gap-x-2 gap-y-6'>
+        {show.looks.map((look) => (
+          <li key={look.id}>
+            <img src={look.image.url} alt={`Look ${look.number}`} />
+            <p className='mt-0.5 text-sm'>Look {look.number}</p>
+          </li>
+        ))}
+      </ol>
+    </div>
   )
 }
 
