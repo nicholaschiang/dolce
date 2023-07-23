58c0c2381e53789945cfe1ab728075baf7f279bc
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-07-23 16:15:39 -0600

    feat(routes/show): add links to section headers
    This patch adds a little link icon that appears when a user hovers over
    a section header that will link to that section's ID.

--- app/routes/shows.$showId/section.tsx
+++ app/routes/shows.$showId/section.tsx
@@ -1,3 +1,5 @@
+import { Link } from '@remix-run/react'
+import { Link as LinkIcon } from 'lucide-react'
 import { type PropsWithChildren } from 'react'
 
 export function Section({
@@ -7,8 +9,14 @@ export function Section({
 }: PropsWithChildren<{ id: string; header: string }>) {
   return (
     <section className='grid gap-2 pt-10' id={id}>
-      <h1 className='border-l-2 border-emerald-700 pl-1.5 font-medium text-base uppercase'>
+      <h1 className='border-l-2 border-emerald-700 pl-1.5 font-medium text-base uppercase flex items-center gap-1 group'>
         {header}
+        <Link
+          to={`#${id}`}
+          className='opacity-0 group-hover:opacity-100 transition-opacity'
+        >
+          <LinkIcon className='w-4 h-4 text-gray-400 dark:text-gray-600' />
+        </Link>
       </h1>
       {children}
     </section>
