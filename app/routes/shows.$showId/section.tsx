bd37e47f92efbce2b232b45ef47a7b498b3a30e5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-07-23 16:00:23 -0600

    fix(routes/show): links to sections show headers
    This patch updates the show page styling so that an anchor-based link
    (`#`) to a specific section on the show page will include that show's
    header. Previously, the show's header was being hidden by the top app
    bar. Now, I moved the gap between sections to the section padding
    itself, which makes the section headings appear when linking directly to
    them.

--- app/routes/shows.$showId/section.tsx
+++ app/routes/shows.$showId/section.tsx
@@ -6,7 +6,7 @@ export function Section({
   children,
 }: PropsWithChildren<{ id: string; header: string }>) {
   return (
-    <section className='grid gap-2' id={id}>
+    <section className='grid gap-2 pt-10' id={id}>
       <h1 className='border-l-2 border-emerald-700 pl-1.5 font-medium text-base uppercase'>
         {header}
       </h1>
