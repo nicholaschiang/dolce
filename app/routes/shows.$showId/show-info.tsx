nicholaschiang/dolce

commit e6a4cf7f0c4195396a3c74a2f5d117de6be2e981

fix: set show location to `NULL` if unknown
This patch updates my Prisma schema to make the show location field
optional. I will set this field to `NULL` when I do not know it
conclusively. I will show "Unknown" in the front-end if it is missing.

app/routes/shows.$showId/show-info.tsx
@@ -19,7 +19,7 @@ export function ShowInfo() {
             dateStyle: 'long',
           })}
         </InfoItem>
-        <InfoItem label='Location'>{show.location}</InfoItem>
+        <InfoItem label='Location'>{show.location ?? 'Unknown'}</InfoItem>
         <InfoItem label='Looks'>{show.looks.length}</InfoItem>
         <InfoItem
           label={show.brands.length === 1 ? 'Brand' : 'Brands'}

prisma/migrations/20230725033924_make_show_location_optional/migration.sql
@@ -1,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Show" ALTER COLUMN "location" DROP NOT NULL;

prisma/schema.prisma
@@ -856,7 +856,7 @@ model Show {
 
   // The runway show's location.
   // @todo perhaps this should be a separate model and simply a relation here.
-  location String
+  location String?
 
   // The collections that were presented at the show. Often, there is only one.
   collections Collection[]

commit 4b45e5549e8acc389f4786140e0403c921903d80

fix(routes/show-info): render description as HTML
This patch updates the show info section on the show page to render the
show description as HTML. I imported all the Vogue article HTML as the
show descriptions, and thus this change was required.

This patch also hides the "Location:" info item when it does not exist.

Fixes: e6a4cf7f0c41 ("fix: set show location to `NULL` if unknown")

app/routes/shows.$showId/show-info.tsx
@@ -12,14 +12,19 @@ export function ShowInfo() {
   const show = useLoaderData<typeof loader>()
   return (
     <Section header='Show info' id='show-info'>
-      <article>{show.description}</article>
+      <article
+        className='prose dark:prose-invert max-w-none'
+        dangerouslySetInnerHTML={{ __html: show.description }}
+      />
       <dl className='mt-2'>
         <InfoItem label='Date'>
           {new Date(show.date).toLocaleDateString(undefined, {
             dateStyle: 'long',
           })}
         </InfoItem>
-        <InfoItem label='Location'>{show.location ?? 'Unknown'}</InfoItem>
+        {show.location != null && (
+          <InfoItem label='Location'>{show.location}</InfoItem>
+        )}
         <InfoItem label='Looks'>{show.looks.length}</InfoItem>
         <InfoItem
           label={show.brands.length === 1 ? 'Brand' : 'Brands'}