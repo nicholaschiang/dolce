nicholaschiang/dolce

commit 56a5a273c668868746396898da28072a500ec820

refactor(scripts): rename `scraper` to `scripts`
This patch moves the `scraper` directory to `scripts` as it no longer
just houses web scraping related scripts but essentially any small TS
script that I want to run with Node.js locally.

.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     project: [
       path.resolve(__dirname, 'tsconfig.json'),
       path.resolve(__dirname, 'cypress/tsconfig.json'),
-      path.resolve(__dirname, 'scraper/node/tsconfig.json'),
+      path.resolve(__dirname, 'scripts/node/tsconfig.json'),
     ],
   },
   extends: [

commit ff422af165355c0f7e728f001927966f0e9be364

chore(eslint): allow dev dependencies in `scripts`
Fixes: 56a5a273c668 ("refactor(scripts): rename `scraper` to `scripts`")

.eslintrc.js
@@ -58,7 +58,7 @@ module.exports = {
       {
         devDependencies: [
           'cypress/**',
-          'scraper/**',
+          'scripts/**',
           'test/**',
           'tests/**',
           'spec/**',