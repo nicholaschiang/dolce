631a4206161a195f8456b341c32f7c8ee4014909
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-07-24 23:32:52 -0600

    fix(shows): add "menswear" part of season name
    This patch updates the `/shows` page to properly append "Menswear" when
    applicable to the season name shown below the show brand name.
    
    This is more necessary now after I've imported all of the shows from
    Vogue and am currently showing them side-by-side (i.e. they are all
    sorted by name and not by season).

---
+++ app/utils/show.ts
@@ -0,0 +1,27 @@
+import { Sex, type Collection, type Season } from '@prisma/client'
+import { type SerializeFrom } from '@vercel/remix'
+
+/**
+ * Get the show sex header (i.e. "Menswear" or "") based on the collections sex.
+ * If every collection in the show has a sex of "MAN", then "Menswear" will be
+ * appended to the header. Otherwise, nothing will be appended.
+ */
+function getShowSex(show: SerializeFrom<{ collections: Collection[] }>) {
+  const sex = show.collections
+    .map((collection) => collection.sex)
+    .every((value) => value === Sex.MAN)
+    ? 'Menswear'
+    : ''
+  return sex
+}
+
+/**
+ * Get the show season header (i.e. "SPRING 2021 MENSWEAR" or "RESORT 2024").
+ * This is more convoluted than normal due to the weirdness with "Menswear".
+ */
+export function getShowSeason(
+  show: SerializeFrom<{ collections: Collection[]; season: Season }>,
+) {
+  const sex = getShowSex(show)
+  return `${show.season.name.replace('_', '-')} ${show.season.year} ${sex}`
+}
