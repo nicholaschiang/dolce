nicholaschiang/dolce

commit 986e27d445b05473c774696740901b5b53b1d1e3

feat(scripts): scrape and save shows from vogue
This patch adds a Scrapy Python script to scrape show data and reviews
from Vogue. Note that this approach only gets the first nine looks for
each show. Vogue implements some JS-only pagination and thus Scrapy is
unable to "load more" looks. I will look into a JS-based scraping
solution that e.g. runs a headless browser, but for now, this works OK.

This patch then adds a Node.js script that will import all those shows
(that are saved from Vogue.com to JSON via Scrapy) to my Postgres
database via the Prisma ORM.

Closes: NC-644

scripts/node/save/vogue.ts
@@ -1,0 +1,154 @@
+// Script to save the data scraped via `scripts/python/spiders/vogue.py` to the
+// Postgres database via the Prisma ORM for type safe data transformations.
+
+import fs from 'fs'
+import path from 'path'
+
+import { type Prisma, Sex, SeasonName, PrismaClient } from '@prisma/client'
+import ProgressBar from 'progress'
+
+import example from './vogue/resort-2024.json'
+
+const PATH = '../../../public/data/vogue/shows.json'
+
+type Show = (typeof example)[number]
+
+const vogue: Prisma.PublicationCreateInput = {
+  name: 'Vogue',
+  avatar: 'https://www.vogue.com/verso/static/vogue/assets/us/logo.svg',
+}
+
+const prisma = new PrismaClient({
+  datasources: { db: { url: process.env.DATABASE_URL } },
+})
+
+export async function save() {
+  const json = fs.readFileSync(path.resolve(__dirname, PATH), 'utf8')
+  const shows = JSON.parse(json) as Show[]
+  const bar = new ProgressBar(
+    'saving vogue shows [:bar] :rate/sps :current/:total :percent :etas',
+    {
+      complete: '=',
+      incomplete: ' ',
+      width: 20,
+      total: shows.length,
+    },
+  )
+  /* eslint-disable-next-line no-restricted-syntax */
+  for await (const show of shows) {
+    await prisma.show.create({ data: getData(show) })
+    bar.tick()
+  }
+}
+
+void save()
+
+//////////////////////////////////////////////////////////////////
+
+function caps(sentence: string): string {
+  return sentence
+    .split(' ')
+    .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+    .join(' ')
+}
+
+function getSeason(show: Show) {
+  // e.g. "SPRING 2023 READY-TO-WEAR", "RESORT 2024"
+  const [season, yr] = show.season.split(' ')
+  let name: SeasonName
+  switch (season) {
+    case 'SPRING':
+      name = SeasonName.SPRING
+      break
+    case 'RESORT':
+      name = SeasonName.RESORT
+      break
+    case 'FALL':
+      name = SeasonName.FALL
+      break
+    default:
+      throw new Error(`Unknown season: ${season}`)
+  }
+  const year = Number(yr)
+  if (Number.isNaN(year)) throw new Error(`Invalid year: ${year}`)
+  return { name, year }
+}
+
+function getData(show: Show) {
+  const season: Prisma.SeasonCreateInput = getSeason(show)
+  let review: Prisma.ReviewCreateWithoutShowInput | undefined
+  if (show.author_name && show.author_url)
+    review = {
+      title: show.title,
+      subtitle: null,
+      score: null,
+      summary: null,
+      content: show.content.join('\n'),
+      url: show.url,
+      date: show.date ? new Date(show.date) : null,
+      author: {
+        connectOrCreate: {
+          where: { name: show.author_name },
+          create: {
+            name: show.author_name,
+            url: show.author_url,
+            avatar: show.author_avatar,
+          },
+        },
+      },
+      publication: {
+        connectOrCreate: { where: { name: vogue.name }, create: vogue },
+      },
+    }
+  let name = `${show.brand} ${caps(show.season)}`
+  if (show.title.includes('Menswear')) name += ' Menswear'
+
+  // TODO Vogue will list the same collection twice (once under "Menswear" and
+  // again without "Menswear") if that collection is unisex. I can fix this by
+  // looking for collections that have the same review content and other data,
+  // and then merge those collections under the same unisex name.
+  const collection: Prisma.CollectionCreateInput = {
+    name,
+    sex: show.title.includes('Menswear') ? Sex.MAN : Sex.WOMAN,
+    season: {
+      connectOrCreate: {
+        where: { name_year: { name: season.name, year: season.year } },
+        create: season,
+      },
+    },
+  }
+  const looks: Prisma.LookCreateWithoutShowInput[] = show.looks.map((look) => {
+    const image: Prisma.ImageCreateWithoutLookInput = { url: look.src }
+    return {
+      number: look.number,
+      image: { connectOrCreate: { where: { url: image.url }, create: image } },
+    }
+  })
+  const brand: Prisma.BrandCreateInput = {
+    name: show.brand,
+    description: null,
+    tier: null,
+    avatar: null,
+    url: null,
+  }
+  const data: Prisma.ShowCreateInput = {
+    name,
+    url: show.url,
+    description: null,
+    criticReviewSummary: null,
+    consumerReviewSummary: null,
+    date: show.date ? new Date(show.date) : null,
+    location: null,
+    reviews: review ? { create: review } : undefined,
+    season: {
+      connectOrCreate: {
+        where: { name_year: { name: season.name, year: season.year } },
+        create: season,
+      },
+    },
+    collections: { connectOrCreate: { where: { name }, create: collection } },
+    looks: { create: looks },
+    brands: { connectOrCreate: { where: { name: brand.name }, create: brand } },
+  }
+  return data
+}

scripts/node/save/shows/hermes.ts
@@ -35,7 +35,8 @@ const looks = Array(NUM_LOOKS)
 const designer: Prisma.UserCreateInput = {
   name: 'Véronique Nichanian',
   url: 'https://fr.wikipedia.org/wiki/V%C3%A9ronique_Nichanian',
-  avatar: 'https://static.nicholas.engineering/designers/veronique-nichanian/avatar.jpg',
+  avatar:
+    'https://static.nicholas.engineering/designers/veronique-nichanian/avatar.jpg',
   description: `
 <p>
   Véronique Nichanian was born on May 3, 1954 in Boulogne-Billancourt.In 1976,

scripts/python/spiders/vogue.py
@@ -1,0 +1,59 @@
+"""Scrapy spider for extracting show information from Vogue."""
+
+import scrapy
+
+
+# This works fine for extracting basic show metadata but, as far as I know,
+# cannot be used to extract the look images as Vogue only loads the first nine
+# and the remainder must be loaded via JS by clicking "show more".
+class VogueSpider(scrapy.Spider):
+    """Spider for extracting show information from Vogue."""
+
+    name = "vogue"
+    start_urls = [
+        "https://www.vogue.com/fashion-shows/spring-2024-ready-to-wear",
+        "https://www.vogue.com/fashion-shows/spring-2024-menswear",
+        "https://www.vogue.com/fashion-shows/fall-2023-ready-to-wear",
+        "https://www.vogue.com/fashion-shows/fall-2023-menswear",
+        "https://www.vogue.com/fashion-shows/spring-2023-ready-to-wear",
+        "https://www.vogue.com/fashion-shows/spring-2023-menswear",
+        "https://www.vogue.com/fashion-shows/resort-2023",
+        "https://www.vogue.com/fashion-shows/resort-2023-menswear",
+    ]
+
+    def parse(self, response):
+        """Extract the show links from the main shows list."""
+        show_page_links = response.css(
+            "[data-testid=GroupedNavigationWrapper] "
+            + "[data-testid=navigation__list-item] "
+            + "a[data-testid=navigation__internal-link]"
+        )
+        yield from response.follow_all(show_page_links, self.parse_show)
+
+    def parse_show(self, response):
+        """Extract the show metadata from the show page."""
+        content = response.css("[data-testid=BodyWrapper] > div > p").getall()
+        header = response.css("[data-testid=ContentHeaderContainer]")
+        author = header.css("[data-testid=BylineName] a")
+        collection = response.css("[data-testid=RunwayShowGallery]")
+        looks = []
+        for index, item in enumerate(collection.css(".grid--item")):
+            look = {
+                "number": index + 1,
+                "url": response.urljoin(item.css("a::attr(href)").get()),
+                "src": item.css("img::attr(src)").get(),
+                "srcset": item.css("img::attr(srcset)").get(),
+            }
+            looks.append(look)
+        yield {
+            "url": response.url,
+            "title": response.css("title::text").get(),
+            "content": content,
+            "brand": response.css("[data-testid=SectionHeaderHed] a::text").get(),
+            "season": response.css("[data-testid=SectionHeaderSubhed] a::text").get(),
+            "author_name": author.css("::text").get(),
+            "author_url": response.urljoin(author.css("::attr(href)").get()),
+            "author_avatar": header.css(".responsive-image__image::attr(src)").get(),
+            "date": header.css("[data-testid=ContentHeaderPublishDate]::text").get(),
+            "looks": looks,
+        }

commit 86cdaab299821af195a21386f1e557de4f6dd454

fix(scripts/vogue): omit duplicate "menswear" in show title
Previously, I was adding duplicate "Menswear" suffixes to the show
titles because the Vogue season name already includes that suffix.

Fixes: 986e27d445b0 ("feat(scripts): scrape and save shows from vogue")

scripts/node/save/vogue.ts
@@ -48,7 +48,7 @@ void save()
 function caps(sentence: string): string {
   return sentence
     .split(' ')
-    .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+    .map((w) => `${w.charAt(0).toUpperCase()}${w.slice(1).toLowerCase()}`)
     .join(' ')
 }
 
@@ -100,8 +100,7 @@ function getData(show: Show) {
         connectOrCreate: { where: { name: vogue.name }, create: vogue },
       },
     }
-  let name = `${show.brand} ${caps(show.season)}`
-  if (show.title.includes('Menswear')) name += ' Menswear'
+  const name = `${show.brand} ${caps(show.season)}`
 
   // TODO Vogue will list the same collection twice (once under "Menswear" and
   // again without "Menswear") if that collection is unisex. I can fix this by