nicholaschiang/dolce

commit 92e99bb086d5816678cda0c5a0a2f840adc66a86

feat: use `vercel` built-in image optimization
This patch updates my `<Image>` component to use the Vercel built-in
image optimization features [1] [2] [3] [4].

[1]: https://github.com/theMosaad/remix-vercel-image/blob/main/app/routes/index.tsx
[2]: https://vercel.com/docs/concepts/image-optimization
[3]: https://vercel.com/docs/build-output-api/v3/configuration#images
[4]: https://github.com/vercel/vercel/pull/8566

remix.config.js
@@ -5,4 +5,10 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   serverDependenciesToBundle: ['nanoid'],
+  images: {
+    sizes: [200, 300, 400, 500, 600, 700, 800, 900, 1000],
+    domains: ['aritzia.scene7.com'],
+    minimumCacheTTL: 60,
+    formats: ['image/webp', 'image/avif'],
+  },
 }

app/components/image.tsx
@@ -1,22 +1,19 @@
 import type { ComponentPropsWithoutRef } from 'react'
 
 export function Image({
-  optimizerUrl = '/api/image',
+  optimizerUrl = '/_vercel/image',
   responsive,
   src,
   alt,
   ...rest
 }: ComponentPropsWithoutRef<'img'> & {
+  src: string
   optimizerUrl?: string
-  responsive?: {
-    maxWidth?: number
-    size: { width: number; height?: number }
-  }[]
+  responsive?: { maxWidth?: number; size: { width: number } }[]
 }) {
-  const url = src ? `${optimizerUrl}?src=${encodeURIComponent(src)}` : src
-
+  const url = `${optimizerUrl}?url=${encodeURIComponent(src)}`
   const props: ComponentPropsWithoutRef<'img'> = {
-    src: `${url}&width=${rest.width || ''}&height=${rest.height || ''}`,
+    src: `${url}&w=${rest.width ?? ''}`,
   }
 
   let largestImageWidth = 0
@@ -28,9 +25,7 @@ export function Image({
       if (srcSet) {
         srcSet += ', '
       }
-      const srcSetUrl = `${url}&width=${size.width}&height=${
-        size.height || ''
-      } ${size.width}w`
+      const srcSetUrl = `${url}&w=${size.width} ${size.width}w`
       srcSet += srcSetUrl
 
       if (maxWidth) {
@@ -50,9 +45,8 @@ export function Image({
     props.src = ''
   }
 
-  if (largestImageSrc && (!rest.width || largestImageWidth > rest.width)) {
+  if (largestImageSrc && (!rest.width || largestImageWidth > rest.width))
     props.src = largestImageSrc
-  }
 
   return <img alt={alt ?? ''} {...rest} {...props} />
 }

app/entry.server.tsx
@@ -1,54 +1,17 @@
-import { PassThrough } from 'stream'
-
 import type { EntryContext } from '@vercel/remix'
 import { RemixServer } from '@remix-run/react'
-import { Response } from '@vercel/remix'
-import isbot from 'isbot'
-import { renderToPipeableStream } from 'react-dom/server'
+import { handleRequest } from '@vercel/remix'
 
-const ABORT_DELAY = 5000
-
-export default function handleRequest(
+export default function entry(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const callbackName = isbot(request.headers.get('user-agent'))
-    ? 'onAllReady'
-    : 'onShellReady'
-
-  return new Promise((resolve, reject) => {
-    let didError = false
-
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        [callbackName]: () => {
-          const body = new PassThrough()
-
-          responseHeaders.set('Content-Type', 'text/html')
-
-          resolve(
-            new Response(body, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            }),
-          )
-
-          pipe(body)
-        },
-        onShellError: (err: unknown) => {
-          reject(err)
-        },
-        onError: (error: unknown) => {
-          didError = true
-
-          console.error(error)
-        },
-      },
-    )
-
-    setTimeout(abort, ABORT_DELAY)
-  })
+  return handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    <RemixServer context={remixContext} url={request.url} />,
+  )
 }

app/image.server.ts
@@ -1,150 +1,0 @@
-import { PassThrough } from 'stream'
-import type { Readable } from 'stream'
-import { createHash } from 'crypto'
-import fs from 'fs'
-import fsp from 'fs/promises'
-import https from 'https'
-import path from 'path'
-
-import type { LoaderFunction, Request as NodeRequest } from '@vercel/remix'
-import {
-  Response as NodeResponse,
-  writeReadableStreamToWritable,
-} from '@vercel/remix'
-import type { FitEnum } from 'sharp'
-import sharp from 'sharp'
-
-import { log } from 'log.server'
-
-const badImageBase64 =
-  'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
-
-function badImageResponse() {
-  const buffer = Buffer.from(badImageBase64, 'base64')
-  return new Response(buffer, {
-    status: 500,
-    headers: {
-      'Cache-Control': 'max-age=0',
-      'Content-Type': 'image/gif;base64',
-      'Content-Length': buffer.length.toFixed(0),
-    },
-  })
-}
-
-function getIntOrNull(value: string | null) {
-  return value === null ? null : Number.parseInt(value, 10)
-}
-
-export const loader: LoaderFunction = async ({ request }) => {
-  const url = new URL(request.url)
-
-  const src = url.searchParams.get('src')
-  if (!src) return badImageResponse()
-  log.debug('optimizing image... %s', src)
-
-  if (process.env.OPTIMIZE_IMAGES === 'false') {
-    const { href } = new URL(src, request.url)
-    log.warn('skipping optimized image due to feature flag... %s', href)
-    return fetch(href, request)
-  }
-
-  const width = getIntOrNull(url.searchParams.get('width'))
-  const height = getIntOrNull(url.searchParams.get('height'))
-  const fit = (url.searchParams.get('fit') as keyof FitEnum) || 'cover'
-
-  const hash = createHash('sha256')
-  hash.update('v1')
-  hash.update(request.method)
-  hash.update(request.url)
-  hash.update(width?.toString() || '0')
-  hash.update(height?.toString() || '0')
-  hash.update(fit)
-  const key = hash.digest('hex')
-  const cachedFile = path.resolve(path.join('.cache/images', `${key}.webp`))
-
-  try {
-    const exists = await fsp
-      .stat(cachedFile)
-      .then((s) => s.isFile())
-      .catch(() => false)
-
-    if (exists) {
-      const fileStream = fs.createReadStream(cachedFile)
-
-      return new NodeResponse(fileStream, {
-        status: 200,
-        headers: {
-          'Content-Type': 'image/webp',
-          'Cache-Control': 'public, max-age=31536000, immutable',
-        },
-      }) as unknown as Response
-    }
-    log.debug('cache skipped for: %s', cachedFile)
-  } catch (error) {
-    log.error(error)
-  }
-
-  try {
-    let imageBody: Readable | undefined
-    let status = 200
-
-    if (src.startsWith('/') && (src.length === 1 || src[1] !== '/')) {
-      log.debug('fetching local image: %s', src.slice(1))
-      imageBody = fs.createReadStream(path.resolve('public', src.slice(1)))
-    } else {
-      log.debug('fetching remote image: %s', src.toString())
-      const imgRequest = new Request(src.toString()) as unknown as NodeRequest
-      imgRequest.agent = new https.Agent({ rejectUnauthorized: false })
-      const imageResponse = await fetch(imgRequest as unknown as Request)
-      log.debug('fetched remote image status: %d', imageResponse.status)
-      status = imageResponse.status
-      if (imageResponse.body) {
-        const passThrough = new PassThrough()
-        imageBody = passThrough
-        await writeReadableStreamToWritable(imageResponse.body, passThrough)
-      }
-    }
-
-    if (!imageBody) return badImageResponse()
-
-    const sharpInstance = sharp()
-    sharpInstance.on('error', (error) => log.error(error))
-
-    if (width || height) sharpInstance.resize(width, height, { fit })
-    sharpInstance.webp({ effort: 6 })
-
-    const imageManipulationStream = imageBody.pipe(sharpInstance)
-
-    await fsp
-      .mkdir(path.dirname(cachedFile), { recursive: true })
-      .catch(() => {})
-    const cacheFileStream = fs.createWriteStream(cachedFile)
-
-    await new Promise<void>((resolve) => {
-      imageManipulationStream.pipe(cacheFileStream)
-      imageManipulationStream.on('end', () => {
-        resolve()
-        imageBody?.destroy()
-      })
-      imageManipulationStream.on('error', (error: Error) => {
-        log.error('error manipulating image: %s', error.message)
-        imageBody?.destroy()
-        void fsp.rm(cachedFile).catch(() => {})
-      })
-    })
-
-    log.debug('streaming cached response: %s', cachedFile)
-    const fileStream = fs.createReadStream(cachedFile)
-
-    return new NodeResponse(fileStream, {
-      status,
-      headers: {
-        'Content-Type': 'image/webp',
-        'Cache-Control': 'public, max-age=31536000, immutable',
-      },
-    }) as unknown as Response
-  } catch (error) {
-    log.error(error)
-    return badImageResponse()
-  }
-}

app/routes/api.image.ts
@@ -1,1 +1,0 @@
-export { loader } from 'image.server'

commit 6563d298e664dbb27503bf45bc5b19a7649158b5

chore(remix.config): remove unnecessary `images`
This patch removes the unused `images` section of the Remix
configuration. Vercel will instead read this configuration from
`vercel.json`.

Fixes: 92e99bb086d5 ("feat: use `vercel` built-in image optimization")
Ref: https://github.com/vercel/satori/discussions/524#discussioncomment-6557664

remix.config.js
@@ -14,10 +14,4 @@ module.exports = {
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   serverModuleFormat: 'cjs',
   serverDependenciesToBundle: ['nanoid/non-secure'],
-  images: {
-    sizes: [200, 300, 400, 500, 600, 700, 800, 900, 1000],
-    domains: ['aritzia.scene7.com'],
-    minimumCacheTTL: 60,
-    formats: ['image/webp', 'image/avif'],
-  },
 }
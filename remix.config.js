6563d298e664dbb27503bf45bc5b19a7649158b5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-07-26 21:06:22 -0600

    chore(remix.config): remove unnecessary `images`
    This patch removes the unused `images` section of the Remix
    configuration. Vercel will instead read this configuration from
    `vercel.json`.
    
    Fixes: 92e99bb086d5 ("feat: use `vercel` built-in image optimization")
    Ref: https://github.com/vercel/satori/discussions/524#discussioncomment-6557664

--- remix.config.js
+++ remix.config.js
@@ -14,10 +14,4 @@ module.exports = {
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   serverModuleFormat: 'cjs',
   serverDependenciesToBundle: ['nanoid/non-secure'],
-  images: {
-    sizes: [200, 300, 400, 500, 600, 700, 800, 900, 1000],
-    domains: ['aritzia.scene7.com'],
-    minimumCacheTTL: 60,
-    formats: ['image/webp', 'image/avif'],
-  },
 }
