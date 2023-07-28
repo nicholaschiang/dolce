nicholaschiang/dolce

commit fbbef0873cf9d3f2c83428e5e3b43e80f2f69b95

feat(routes): add user friendly show paths
This patch replaces the `$showId` path with a more user-friendly URL
path for shows that includes the show season year, name, the show sex
(i.e. "man" or "woman"), and the show brand slug. This mimics the
behavior of Vogue, but with more nesting (which I think is better).

Note that there is still some discussion to be had on what the order of
that nesting should be (e.g. are "man" and "woman" top-level categories,
even higher up the hierarchy than the season year?) but this current
setup makes sense, as it is how you would read it: "2023's fall menswear
collection by Acne Studios".

I also have made the choice that collections for women will omit the sex
identifier from the URL path. Most collections are for women. This
choice mimics what Vogue and other publishers do. However, once I have
successfully combined the duplicate menswear collections with their
female counterparts (see NC-660), I should revert this change and
include "woman" in the URL path and omit the sex field when it is
unisex.

Ref: https://developers.google.com/search/docs/fundamentals/seo-starter-guide?visit_id=638258623280664328-3236374080&rd=1#simpleurlsconveyinfo
Ref: https://developers.google.com/search/docs/fundamentals/seo-starter-guide?visit_id=638258623280664328-3236374080&rd=1#navisimportant

Closes: NC-673

app/routes/shows.$seasonYear.$seasonName.($sex).$brandSlug/route.tsx
@@ -8,7 +8,10 @@ import {
 import { prisma } from 'db.server'
 import { log } from 'log.server'
 import { type Handle } from 'root'
+import { invert } from 'utils'
 import { cn } from 'utils/cn'
+import { SEASON_NAME_TO_SLUG } from 'utils/season'
+import { SEX_TO_SLUG } from 'utils/sex'
 import { getShowSeason } from 'utils/show'
 
 import { ConsumerReviews } from './consumer-reviews'
@@ -58,33 +61,42 @@ export const handle: Handle = {
 
 export async function loader({ request, params }: LoaderArgs) {
   log.debug('getting show...')
-  const showId = Number(params.showId)
-  if (Number.isNaN(showId)) throw new Response(null, { status: 404 })
-  const [show, scores, review] = await Promise.all([
-    prisma.show.findUnique({
-      where: { id: showId },
-      include: {
-        video: true,
-        season: true,
-        brand: true,
-        collections: {
-          include: {
-            links: { include: { brand: true, retailer: true } },
-            designers: true,
-          },
+  const seasonYear = Number(params.seasonYear)
+  const miss = new Response(null, { status: 404, statusText: 'Not Found' })
+  if (Number.isNaN(seasonYear) || !params.seasonName || !params.brandSlug)
+    throw miss
+  const seasonName = invert(SEASON_NAME_TO_SLUG)[params.seasonName]
+  const sex = invert(SEX_TO_SLUG)[params.sex ?? '']
+  if (!sex || !seasonName) throw miss
+  const show = await prisma.show.findFirst({
+    where: {
+      brand: { slug: params.brandSlug },
+      season: { year: seasonYear, name: seasonName },
+      sex,
+    },
+    include: {
+      video: true,
+      season: true,
+      brand: true,
+      collections: {
+        include: {
+          links: { include: { brand: true, retailer: true } },
+          designers: true,
         },
-        reviews: {
-          include: { author: true, publication: true },
-          orderBy: { updatedAt: 'desc' },
-        },
-        looks: { include: { image: true }, orderBy: { number: 'asc' } },
       },
-    }),
-    getScores(showId),
-    getReview(showId, request),
-  ])
+      reviews: {
+        include: { author: true, publication: true },
+        orderBy: { updatedAt: 'desc' },
+      },
+      looks: { include: { image: true }, orderBy: { number: 'asc' } },
+    },
+  })
+  if (show == null) throw miss
   log.debug('got show %o', show)
-  if (show == null) throw new Response(null, { status: 404 })
+  const [scores, review] = await Promise.all([
+    getScores(show.id),
+    getReview(show.id, request),
+  ])
   return { ...show, scores, review }
 }
 

app/utils.ts
@@ -11,6 +11,22 @@ export const clone = rfdc()
 const DEFAULT_REDIRECT = '/'
 
 /**
+ * Swaps the keys and values of an object.
+ * @param obj The object to swap.
+ * @returns The swapped object.
+ * @see {@link https://stackoverflow.com/a/23013726}
+ */
+export function invert<T extends string, U extends string>(
+  obj: Record<T, U>,
+): Record<U, T> {
+  const ret: Partial<Record<U, T>> = {}
+  Object.keys(obj).forEach((key) => {
+    ret[obj[key as T]] = key as T
+  })
+  return ret as Record<U, T>
+}
+
+/**
  * Capitalizes the first letter of each word in the given string. Lowercases all
  * the other letters in each string (e.g. "RESORT 2024" -> "Resort 2024").
  * @param str The string to capitalize.

app/utils/season.ts
@@ -1,4 +1,4 @@
-import { type Season } from '@prisma/client'
+import { type Season, SeasonName } from '@prisma/client'
 import { type SerializeFrom } from '@vercel/remix'
 
 import { caps } from 'utils'
@@ -10,4 +10,14 @@ import { caps } from 'utils'
  */
 export function getSeasonName(season: SerializeFrom<Season>): string {
   return `${caps(season.name.replace('_', '-'))} ${season.year}`
+}
+
+export const SEASON_NAME_TO_SLUG: Record<SeasonName, string> = {
+  [SeasonName.FALL]: 'fall',
+  [SeasonName.WINTER]: 'winter',
+  [SeasonName.SPRING]: 'spring',
+  [SeasonName.SUMMER]: 'summer',
+  [SeasonName.RESORT]: 'resort',
+  [SeasonName.SPRING_SUMMER]: 'ss',
+  [SeasonName.FALL_WINTER]: 'fw',
 }

app/utils/show.ts
@@ -1,16 +1,13 @@
-import { Sex, type Show, type Collection, type Season } from '@prisma/client'
+import {
+  type Show,
+  type Brand,
+  type Collection,
+  type Season,
+} from '@prisma/client'
 import { type SerializeFrom } from '@vercel/remix'
 
-import { getSeasonName } from 'utils/season'
-
-/**
- * Get the show sex header (i.e. "Menswear" or "") based on the collections sex.
- * If every collection in the show has a sex of "MAN", then "Menswear" will be
- * appended to the header. Otherwise, nothing will be appended.
- */
-function getShowSex(show: SerializeFrom<Show>) {
-  return show.sex === Sex.MAN ? 'Menswear' : ''
-}
+import { SEASON_NAME_TO_SLUG, getSeasonName } from 'utils/season'
+import { SEX_TO_SLUG, getSexName } from 'utils/sex'
 
 /**
  * Get the show season header (i.e. "SPRING 2021 MENSWEAR" or "RESORT 2024").
@@ -19,5 +16,17 @@ function getShowSex(show: SerializeFrom<Show>) {
 export function getShowSeason(
   show: SerializeFrom<Show & { collections: Collection[]; season: Season }>,
 ) {
-  return `${getSeasonName(show.season)} ${getShowSex(show)}`
+  return `${getSeasonName(show.season)} ${getSexName(show.sex)}`
+}
+
+export function getShowPath(
+  show: SerializeFrom<Show & { season: Season; brand: Brand }>,
+) {
+  const path = [
+    show.season.year,
+    SEASON_NAME_TO_SLUG[show.season.name],
+    SEX_TO_SLUG[show.sex],
+    show.brand.slug,
+  ]
+  return `/shows/${path.filter((p) => p !== '').join('/')}`
 }

commit 75f0ca86f63d2eda60a9b4539cfe4906e720d220

fix(routes/show): submit reviews to correct path
Fixes: fbbef0873cf9 ("feat(routes): add user friendly show paths")

app/routes/shows.$seasonYear.$seasonName.($sex).$brandSlug/rate-and-review.tsx
@@ -27,7 +27,10 @@ import { Textarea } from 'components/ui/textarea'
 import { prisma } from 'db.server'
 import { log } from 'log.server'
 import { getUserId } from 'session.server'
-import { useOptionalUser } from 'utils'
+import { invert, useOptionalUser } from 'utils'
+import { SEASON_NAME_TO_SLUG } from 'utils/season'
+import { SEX_TO_SLUG } from 'utils/sex'
+import { getShowPath } from 'utils/show'
 
 import { type loader } from './route'
 import { Section } from './section'
@@ -57,18 +60,32 @@ export async function getReview(showId: number, request: Request) {
 }
 
 export async function action({ request, params }: ActionArgs) {
-  const showId = Number(params.showId)
-  if (Number.isNaN(showId)) throw new Response(null, { status: 404 })
+  const seasonYear = Number(params.seasonYear)
+  const miss = new Response(null, { status: 404, statusText: 'Not Found' })
+  if (Number.isNaN(seasonYear) || !params.seasonName || !params.brandSlug)
+    throw miss
+  const seasonName = invert(SEASON_NAME_TO_SLUG)[params.seasonName]
+  const sex = invert(SEX_TO_SLUG)[params.sex ?? '']
+  if (!sex || !seasonName) throw miss
+  const show = await prisma.show.findFirst({
+    where: {
+      brand: { slug: params.brandSlug },
+      season: { year: seasonYear, name: seasonName },
+      sex,
+    },
+    include: { season: true, brand: true },
+  })
+  if (show == null) throw miss
   const userId = await getUserId(request)
   if (userId == null)
-    return redirect(`/login?redirectTo=/shows/${showId}%23${id}`)
+    return redirect(`/login?redirectTo=${getShowPath(show)}%23${id}`)
   const formData = await request.formData()
   const submission = parse(formData, { schema })
   if (!submission.value || submission.intent !== 'submit')
     return json(submission, { status: 400 })
   log.info('creating review... %o', submission.value)
   const review = await prisma.review.upsert({
-    where: { authorId_showId: { showId, authorId: userId } },
+    where: { authorId_showId: { showId: show.id, authorId: userId } },
     update: {
       score: submission.value.score / 5,
       content: submission.value.content,
@@ -77,11 +94,11 @@ export async function action({ request, params }: ActionArgs) {
       score: submission.value.score / 5,
       content: submission.value.content,
       author: { connect: { id: userId } },
-      show: { connect: { id: showId } },
+      show: { connect: { id: show.id } },
     },
   })
   log.info('created review: %o', review)
-  return redirect(`/shows/${showId}`)
+  return redirect('.')
 }
 
 export function RateAndReview() {

app/routes/shows.$seasonYear.$seasonName.($sex).$brandSlug/route.tsx
@@ -12,7 +12,7 @@ import { invert } from 'utils'
 import { cn } from 'utils/cn'
 import { SEASON_NAME_TO_SLUG } from 'utils/season'
 import { SEX_TO_SLUG } from 'utils/sex'
-import { getShowSeason } from 'utils/show'
+import { getShowSeason, getShowPath } from 'utils/show'
 
 import { ConsumerReviews } from './consumer-reviews'
 import { CriticReviews } from './critic-reviews'
@@ -52,11 +52,14 @@ export const meta: V2_MetaFunction<typeof loader> = ({ data }) => {
 }
 
 export const handle: Handle = {
-  breadcrumb: (match) => (
-    <Link prefetch='intent' to={`/shows/${match.params.showId as string}`}>
-      {(match.data as SerializeFrom<typeof loader>)?.name ?? '404'}
-    </Link>
-  ),
+  breadcrumb: (match) => {
+    const data = match.data as SerializeFrom<typeof loader> | undefined
+    return (
+      <Link prefetch='intent' to={data ? getShowPath(data) : '.'}>
+        {data?.name ?? '404'}
+      </Link>
+    )
+  },
 }
 
 export async function loader({ request, params }: LoaderArgs) {

app/utils.ts
@@ -10,6 +10,8 @@ export const clone = rfdc()
 
 const DEFAULT_REDIRECT = '/'
 
+export type Serialize<T extends AppData> = SerializeFrom<T> | T
+
 /**
  * Swaps the keys and values of an object.
  * @param obj The object to swap.

app/utils/season.ts
@@ -1,14 +1,13 @@
 import { type Season, SeasonName } from '@prisma/client'
-import { type SerializeFrom } from '@vercel/remix'
 
-import { caps } from 'utils'
+import { type Serialize, caps } from 'utils'
 
 /**
  * Get the user-friendly season name from a season object (e.g. "Resort 2024").
  * @param season The season object.
  * @returns The user-friendly season name.
  */
-export function getSeasonName(season: SerializeFrom<Season>): string {
+export function getSeasonName(season: Serialize<Season>): string {
   return `${caps(season.name.replace('_', '-'))} ${season.year}`
 }
 

app/utils/show.ts
@@ -4,8 +4,8 @@ import {
   type Collection,
   type Season,
 } from '@prisma/client'
-import { type SerializeFrom } from '@vercel/remix'
 
+import { type Serialize } from 'utils'
 import { SEASON_NAME_TO_SLUG, getSeasonName } from 'utils/season'
 import { SEX_TO_SLUG, getSexName } from 'utils/sex'
 
@@ -14,13 +14,13 @@ import { SEX_TO_SLUG, getSexName } from 'utils/sex'
  * This is more convoluted than normal due to the weirdness with "Menswear".
  */
 export function getShowSeason(
-  show: SerializeFrom<Show & { collections: Collection[]; season: Season }>,
+  show: Serialize<Show & { collections: Collection[]; season: Season }>,
 ) {
   return `${getSeasonName(show.season)} ${getSexName(show.sex)}`
 }
 
 export function getShowPath(
-  show: SerializeFrom<Show & { season: Season; brand: Brand }>,
+  show: Serialize<Show & { season: Season; brand: Brand }>,
 ) {
   const path = [
     show.season.year,