nicholaschiang/dolce

commit 01e93094e9c7a14e9344bb045c34711dc5ae9e1e

chore(eslint): allow dev dependencies in `scripts`
Fixes: 56a5a273c668 ("refactor(scripts): rename `scraper` to `scripts`")

.eslintrc.js
@@ -58,7 +58,7 @@ module.exports = {
       {
         devDependencies: [
           'cypress/**',
-          'scraper/**',
+          'scripts/**',
           'test/**',
           'tests/**',
           'spec/**',

commit bcca5d7012a0e5dab660ebc34f0b5a487376a419

chore(eslint): allow development deps in tests
Previously, these were not being resolved properly (as they are
determined by the relative path of where the linter is executed which,
when using `vim`, is probably not the root of the repository).

Fixes: 01e93094e9c7 ("chore(eslint): allow dev dependencies in `scripts`")

.eslintrc.js
@@ -57,11 +57,11 @@ module.exports = {
       'error',
       {
         devDependencies: [
-          'cypress/**',
-          'scripts/**',
-          'test/**',
-          'tests/**',
-          'spec/**',
+          '**/cypress/**',
+          '**/scripts/**',
+          '**/test/**',
+          '**/tests/**',
+          '**/spec/**',
           '**/__tests__/**',
           '**/__mocks__/**',
           'test.{js,jsx}',