nicholaschiang/dolce

commit da8bec3af43d11580d85dcd1fe0777bca0af4b1a

feat(scripts/wwd): add import script and migrations
This patch adds the WWD Node.js import script that uses Prisma's
`upsert` capabilities to augment the existing Vogue shows (if they
exist) or create new show records (if a show isn't already recorded).

Note that this script requires some transient Prisma migrations to work
properly (one to ensure each show name is consistently defined and a
second to temporarily remove the look unique constraint).

prisma/schema.prisma
@@ -530,6 +530,8 @@ model Image {
   variantId Int?
 
   // The runway look the image is of.
+  // look   Look? @relation(fields: [lookId], references: [id], onDelete: Cascade, onUpdate: Cascade)
+  // lookId Int?
   look Look?
 
   // @todo store information on the models in the image (e.g. insta, etc).
@@ -758,11 +760,12 @@ model Look {
   products Product[]
 
   // The look's picture (if known).
+  // images Image[]
   image   Image @relation(fields: [imageId], references: [id], onDelete: Cascade, onUpdate: Cascade)
   imageId Int   @unique
 
   // Each look must have a unique number in the show.
-  @@unique([number, showId])
+  // @@unique([number, showId])
 }
 
 // A review is a review for a show from a fashion critic or consumer.

prisma/migrations/20230802061514_derive_show_name_from_unique_columns/migration.sql
@@ -1,0 +1,10 @@
+-- Derive the show name from the other unique show columns. The name column is
+-- really redundant and is only used for convenience when importing shows.
+UPDATE "Show"
+SET "name" = TRIM(CONCAT(
+    "Brand"."name", ' ', "Season"."name", ' ', "Season"."year", ' ',
+    "Show"."sex", ' ', "Show"."level", ' ', "Show"."location"
+))
+FROM "Brand", "Season"
+WHERE "Show"."seasonId" = "Season"."id"
+	AND "Show"."brandId" = "Brand"."id";

prisma/migrations/20230802071834_temporarily_remove_look_unique_constraint/migration.sql
@@ -1,0 +1,2 @@
+-- DropIndex
+DROP INDEX "Look_number_showId_key";

pnpm-lock.yaml
@@ -213,6 +213,9 @@ devDependencies:
   '@types/express-prometheus-middleware':
     specifier: ^1.2.1
     version: 1.2.1
+  '@types/luxon':
+    specifier: ^3.3.1
+    version: 3.3.1
   '@types/morgan':
     specifier: ^1.9.4
     version: 1.9.4
@@ -315,6 +318,9 @@ devDependencies:
   husky:
     specifier: ^8.0.3
     version: 8.0.3
+  luxon:
+    specifier: ^3.3.0
+    version: 3.3.0
   msw:
     specifier: ^1.2.3
     version: 1.2.3(typescript@4.9.5)
@@ -4706,6 +4712,10 @@ packages:
     resolution: {integrity: sha512-BQ5aZNSCpj7D6K2ksrRCTmKRLEpnPvWDiLPfoGyhZ++8YtiK9d/3DBKPJgry359X/P1PfruyYwvnvwFjuEiEIg==}
     dependencies:
       '@types/node': 18.17.1
+    dev: true
+
+  /@types/luxon@3.3.1:
+    resolution: {integrity: sha512-XOS5nBcgEeP2PpcqJHjCWhUCAzGfXIU8ILOSLpx2FhxqMW9KdxgCGXNOEKGVBfveKtIpztHzKK5vSRVLyW/NqA==}
     dev: true
 
   /@types/mdast@3.0.11:
@@ -10531,6 +10541,11 @@ packages:
     dependencies:
       react: 18.2.0
     dev: false
+
+  /luxon@3.3.0:
+    resolution: {integrity: sha512-An0UCfG/rSiqtAIiBPO0Y9/zAnHUZxAMiCpTd5h2smgsj7GGmcenvrvww2cqNA8/4A5ZrD1gJpHN2mIHZQF+Mg==}
+    engines: {node: '>=12'}
+    dev: true
 
   /lz-string@1.5.0:
     resolution: {integrity: sha512-h5bgJWpxJNswbU7qCrV0tIKQCaS3blPDrqKWx+QxzuzL1zGUzij9XCWLrSLsJPu5t+eWA/ycetzYAO5IOMcWAQ==}

scripts/node/save/vogue.ts
@@ -17,7 +17,7 @@ import ProgressBar from 'progress'
 import { slug } from './utils'
 import example from './vogue/resort-2024.json'
 
-const PATH = '../../../public/data/vogue/shows.json'
+const PATH = '../../../../static/data/vogue.json'
 
 type Show = (typeof example)[number]
 

scripts/node/save/wwd.ts
@@ -1,0 +1,308 @@
+// Script to save the data scraped via `scripts/python/spiders/wwd.py` to the
+// Postgres database via the Prisma ORM for type safe data transformations.
+//
+// Note that this script is meant to be run after the data from Vogue has
+// already been imported. I intentionally use `upsert` instead of `create` to
+// simply augment the existing shows. This _should_ also work in a clean
+// database (i.e. if you just want WWD shows), but I haven't tested it.
+//
+// Another caveat for running this script, is that you *must* first remove the
+// look unique index and ensure that the show names conform to the standard
+// format. I have two Prisma migrations for this:
+// - `20230802061514_derive_show_name_from_unique_columns`
+// - `20230802071834_temporarily_remove_look_unique_constraint`
+//
+// Once you are done importing this data, you can run the following migration to
+// combine duplicate looks and re-add the unique constraint:
+// - `20230802193037_looks_can_have_multiple_images`
+
+import fs from 'fs'
+import path from 'path'
+
+import {
+  type Prisma,
+  Sex,
+  Level,
+  Location,
+  SeasonName,
+  PrismaClient,
+} from '@prisma/client'
+import { DateTime } from 'luxon'
+import ProgressBar from 'progress'
+
+import { slug } from './utils'
+import lookType from './wwd/look.json'
+import showType from './wwd/show.json'
+
+const DEBUG = false
+const DRY_RUN = false
+const PATH = '../../../../static/data/wwd.json'
+
+type Show = typeof showType
+type Look = typeof lookType
+
+const seasonsWithParseWarnings = new Set<string>()
+
+const wwd: Prisma.PublicationCreateInput = { name: 'WWD' }
+
+const prisma = new PrismaClient({
+  datasources: { db: { url: process.env.DATABASE_URL } },
+})
+
+export async function save() {
+  const json = fs.readFileSync(path.resolve(__dirname, PATH), 'utf8')
+  const everything = JSON.parse(json) as (Show | Look)[]
+  const shows = everything.filter(
+    (thing) => thing['@type'] === 'Show',
+  ) as Show[]
+  const looks = everything.filter(
+    (thing) => thing['@type'] === 'Look',
+  ) as Look[]
+  const withLooks = shows.map((show) => ({
+    ...show,
+    looks: looks.filter((look) => look.show_url === show.url),
+  }))
+  log(`Parsing ${shows.length} shows...`)
+  const data = withLooks.map(getData)
+  log(`Parsed ${data.length} shows.`)
+  let index = Number(process.argv[2])
+  if (Number.isNaN(index)) index = 0
+  const final = data.slice(index)
+  log(`Starting at index ${index}, saving ${final.length} shows...`)
+  const bar = new ProgressBar(
+    'Saving WWD shows... [:bar] :rate/sps :current/:total :percent :etas',
+    {
+      complete: '=',
+      incomplete: ' ',
+      width: 20,
+      total: final.length,
+    },
+  )
+  /* eslint-disable-next-line no-restricted-syntax */
+  for await (const show of final) {
+    try {
+      if (!DRY_RUN)
+        await prisma.show.upsert({
+          where: { name: show.name },
+          create: show,
+          update: show,
+        })
+      bar.tick()
+    } catch (error) {
+      console.error(`Error while saving show:`, JSON.stringify(show, null, 2))
+      console.error(error)
+    }
+  }
+}
+
+void save()
+
+//////////////////////////////////////////////////////////////////
+
+function log(...args: Parameters<typeof console.log>) {
+  if (DEBUG) console.log(...args)
+}
+
+function strip(str: string) {
+  return str.replace(/(?=\s)[^\r\n\t]/g, ' ').trim()
+}
+
+type ParsedSeason = {
+  season: { name: SeasonName; year: number }
+  level: Level
+  sex: Sex
+  location?: Location
+}
+
+function parseSeason(season: string, header: string): ParsedSeason {
+  // e.g. "SPRING 2023 READY-TO-WEAR", "RESORT 2024", "TOKYO SPRING 2024"
+  const sex =
+    season.includes('Men') || header.includes('Men’s') ? Sex.MAN : Sex.WOMAN
+  const level = season.includes('Couture') ? Level.COUTURE : Level.RTW
+  const name = season.includes('Resort')
+    ? SeasonName.RESORT
+    : season.includes('Spring')
+    ? SeasonName.SPRING
+    : season.includes('Pre-Fall')
+    ? SeasonName.PRE_FALL
+    : season.includes('Fall')
+    ? SeasonName.FALL
+    : undefined
+  if (name == null) throw new Error(`Could not find season name: ${season}`)
+  const year = Number(/(\d{4})/.exec(season)?.[1])
+  if (Number.isNaN(year)) throw new Error(`Could not find year: ${season}`)
+  const location = season.toUpperCase().includes('NEW YORK')
+    ? Location.NEW_YORK
+    : season.toUpperCase().includes('LONDON')
+    ? Location.LONDON
+    : season.toUpperCase().includes('MILAN')
+    ? Location.MILAN
+    : season.toUpperCase().includes('PARIS')
+    ? Location.PARIS
+    : season.toUpperCase().includes('TOKYO')
+    ? Location.TOKYO
+    : season.toUpperCase().includes('BERLIN')
+    ? Location.BERLIN
+    : season.toUpperCase().includes('FLORENCE')
+    ? Location.FLORENCE
+    : season.toUpperCase().includes('LOS ANGELES')
+    ? Location.LOS_ANGELES
+    : season.toUpperCase().includes('MADRID')
+    ? Location.MADRID
+    : season.toUpperCase().includes('COPENHAGEN')
+    ? Location.COPENHAGEN
+    : season.toUpperCase().includes('SHANGHAI')
+    ? Location.SHANGHAI
+    : season.toUpperCase().includes('AUSTRALIA')
+    ? Location.AUSTRALIA
+    : season.toUpperCase().includes('STOCKHOLM')
+    ? Location.STOCKHOLM
+    : season.toUpperCase().includes('MEXICO')
+    ? Location.MEXICO
+    : season.toUpperCase().includes('MEXICO CITY')
+    ? Location.MEXICO_CITY
+    : season.toUpperCase().includes('KIEV')
+    ? Location.KIEV
+    : season.toUpperCase().includes('TBILISI')
+    ? Location.TBILISI
+    : season.toUpperCase().includes('SEOUL')
+    ? Location.SEOUL
+    : season.toUpperCase().includes('RUSSIA')
+    ? Location.RUSSIA
+    : season.toUpperCase().includes('UKRAINE')
+    ? Location.UKRAINE
+    : season.toUpperCase().includes('SÃO PAULO')
+    ? Location.SAO_PAOLO
+    : season.toUpperCase().includes('BRIDAL')
+    ? Location.BRIDAL
+    : undefined
+  if (location == null && seasonsWithParseWarnings.has(season) === false) {
+    console.warn(`Could not find location: ${season}`)
+    seasonsWithParseWarnings.add(season)
+  }
+  return {
+    season: { name, year },
+    level,
+    sex,
+    location,
+  }
+}
+
+function getData(show: Show & { looks: Look[] }) {
+  try {
+    const { season, sex, level, location, ...etc } = parseSeason(
+      show.season,
+      show.header,
+    )
+    let review: Prisma.ReviewCreateWithoutShowInput | undefined
+    const date = show.date
+      ? DateTime.fromFormat(strip(show.date), 'LLLL d, yyyy, h:mma').toString()
+      : null
+    log('Date:', strip(show.date))
+    log('ISO:', date)
+    if (show.author_name && show.author_url) {
+      const author = { name: strip(show.author_name), url: show.author_url }
+      log('Author:', author)
+      review = {
+        date,
+        title: show.header,
+        subtitle: show.excerpt ? strip(show.excerpt) : null,
+        score: null,
+        summary: null,
+        content: show.content.join('\n'),
+        url: show.url,
+        author: {
+          connectOrCreate: { where: { name: author.name }, create: author },
+        },
+        publication: {
+          connectOrCreate: { where: { name: wwd.name }, create: wwd },
+        },
+      }
+    }
+    const header = strip(show.header)
+    const matches =
+      /(.+?) (?=Men’s|Couture|RTW|Resort|Pre-Fall|Fall|Spring|Summer|\d{4})/.exec(
+        header,
+      )
+    const [_, brandName, ...everythingElse] = matches ?? []
+    log('Header:', header)
+    log('Matches:', matches)
+    log('Derived:', { brandName, everythingElse })
+    log('Parsed:', { season, sex, level, location, ...etc })
+    const brand: Prisma.BrandCreateInput = {
+      name: brandName,
+      slug: slug(brandName),
+      description: null,
+      tier: null,
+      avatar: null,
+      url: null,
+    }
+
+    // The purpose of this `name` field is to match existing records imported from
+    // other news sources (i.e. Vogue). Vogue does not include the season location
+    // if that location is PARIS, so I will omit that from the name when matching.
+    //
+    // This does not lead to any loss of information as the show location is still
+    // saved to our database even while the name doesn't include it.
+    const loc = location === Location.PARIS ? '' : location ?? ''
+    const derived = `${season.name} ${season.year} ${sex} ${level} ${loc}`
+    const name = `${brandName} ${derived}`.trim()
+    log('Name:', name)
+
+    // TODO WWD will list the same collection twice (once under "Menswear" and
+    // again without "Menswear") if that collection is unisex. I can fix this by
+    // looking for collections that have the same review content and other data,
+    // and then merge those collections under the same unisex name.
+    const collection: Prisma.CollectionCreateInput = {
+      name,
+      season: {
+        connectOrCreate: {
+          where: { name_year: { name: season.name, year: season.year } },
+          create: season,
+        },
+      },
+    }
+    const looks: Prisma.LookCreateWithoutShowInput[] = show.looks.map(
+      (look) => {
+        const image: Prisma.ImageCreateWithoutLookInput = {
+          url: look.src.replace(/\?w=\d+$/, ''),
+        }
+        return {
+          number: look.number,
+          image: {
+            connectOrCreate: { where: { url: image.url }, create: image },
+          },
+        }
+      },
+    )
+    const data: Prisma.ShowCreateInput = {
+      ...etc,
+      name,
+      sex,
+      level,
+      location,
+      date,
+      url: show.url,
+      description: null,
+      criticReviewSummary: null,
+      consumerReviewSummary: null,
+      reviews: review ? { create: review } : undefined,
+      season: {
+        connectOrCreate: {
+          where: { name_year: { name: season.name, year: season.year } },
+          create: season,
+        },
+      },
+      collections: { connectOrCreate: { where: { name }, create: collection } },
+      looks: { create: looks },
+      brand: {
+        connectOrCreate: { where: { slug: brand.slug }, create: brand },
+      },
+    }
+    log('Data:', data)
+    return data
+  } catch (error) {
+    console.error(`Error parsing show:`, JSON.stringify(show, null, 2))
+    throw error
+  }
+}

commit 8f8244a7c42f1408c89c5c2552bcc0583b3c0bc2

fix: combine duplicate show looks and images
This patch updates my database schema to support saving multiple images
for a single look. This change was decided to preserve any additional
information saving duplicate looks from WWD and Vogue may add.
Sometimes, the look image that WWD has is different than the one Vogue
does. We can show a mini-slideshow per look to showcase all the
different views of it.

Ref: https://wwd.com/wp-content/uploads/2021/06/Hermes-mens-ss22-photos-008.jpg
Ref: https://assets.vogue.com/photos/60d71ab107311268fe04b92c/master/w_2560%2Cc_limit/_ALE0101.jpg

Fixes: da8bec3af43d ("feat(scripts/wwd): add import script and migrations")

app/routes/shows.$seasonYear.$seasonName.($sex).$brandSlug/route.tsx
@@ -101,7 +101,7 @@ export async function loader({ request, params }: LoaderArgs) {
         orderBy: { updatedAt: 'desc' },
       },
       looks: {
-        include: { image: true, model: true },
+        include: { images: true, model: true },
         orderBy: { number: 'asc' },
       },
     },
@@ -145,7 +145,7 @@ function Looks({ className }: { className: string }) {
                 decoding={
                   index < looksPerRow * rowsToEagerLoad ? 'sync' : 'async'
                 }
-                src={look.image.url}
+                src={look.images[0]?.url}
                 alt=''
               />
             </div>

app/routes/shows.$seasonYear.$seasonName.($sex).$brandSlug/scores-header.tsx
@@ -37,7 +37,7 @@ export function ScoresHeader() {
         <div className='flex-none w-40 bg-gray-100 dark:bg-gray-900 h-0 min-h-full'>
           <img
             className='object-cover h-full w-full'
-            src={show.looks[0].image.url}
+            src={show.looks[0]?.images[0]?.url}
             loading='eager'
             decoding='sync'
             alt=''

app/routes/shows._index.tsx
@@ -28,7 +28,11 @@ export async function loader() {
       season: true,
       brand: true,
       collections: true,
-      looks: { include: { image: true }, orderBy: { number: 'asc' }, take: 1 },
+      looks: {
+        include: { images: { take: 1 } },
+        orderBy: { number: 'asc' },
+        take: 1,
+      },
     },
     orderBy: [{ brand: { name: 'asc' } }, { season: { year: 'desc' } }],
   })
@@ -67,7 +71,7 @@ export default function ShowsPage() {
                       decoding={
                         index < showsPerRow * rowsToEagerLoad ? 'sync' : 'async'
                       }
-                      src={show.looks[0].image.url}
+                      src={show.looks[0].images[0]?.url}
                       responsive={[
                         100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
                       ].map((width) => ({

app/utils/look.ts
@@ -4,12 +4,12 @@ import { type CreativeWork } from 'schema-dts'
 import { type Serialize, url } from 'utils'
 
 export function getLookSchema(
-  look: Serialize<Look & { image: Image }>,
+  look: Serialize<Look & { images: Image[] }>,
 ): CreativeWork {
   return {
     '@type': 'CreativeWork',
     '@id': look.id.toString(),
     'name': `Look ${look.number}`,
-    'image': url(look.image.url),
+    'image': look.images.map((image) => url(image.url)),
   }
 }

app/utils/show.ts
@@ -69,7 +69,7 @@ export function getShowKeywords(
 export function getShowSchema(
   show: Serialize<
     Show & {
-      looks: (Look & { image: Image; model: User | null })[]
+      looks: (Look & { images: Image[]; model: User | null })[]
       collections: (Collection & { designers: User[] })[]
       season: Season
       brand: Brand
@@ -82,7 +82,7 @@ export function getShowSchema(
     '@type': 'ExhibitionEvent',
     '@id': show.id.toString(),
     'name': show.name,
-    'image': url(show.looks[0]?.image?.url),
+    'image': url(show.looks[0]?.images[0]?.url),
     'startDate': show.date?.toString() ?? undefined,
     'location': show.location ?? undefined,
     'description': show.description ?? undefined,

prisma/migrations/20230802193037_looks_can_have_multiple_images/migration.sql
@@ -1,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageId` on the `Look` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[number,showId]` on the table `Look` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "Image" ADD COLUMN     "lookId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD CONSTRAINT "Image_lookId_fkey" FOREIGN KEY ("lookId") REFERENCES "Look"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AlterTable (migrate the one-to-one image relation to the new one-to-many relation)
+UPDATE "Image" SET "lookId" = "Look"."id" FROM "Look" WHERE "Image"."id" = "Look"."imageId";
+
+-- DropForeignKey
+ALTER TABLE "Look" DROP CONSTRAINT "Look_imageId_fkey";
+
+-- DropIndex
+DROP INDEX "Look_imageId_key";
+
+-- AlterTable
+ALTER TABLE "Look" DROP COLUMN "imageId";
+
+-- AlterTable (combine duplicate looks from the same collection)
+UPDATE "Image" SET "lookId" = "DuplicateLook"."idmin" FROM (
+  SELECT ARRAY_AGG("id") "id", MIN("id") "idmin"
+	FROM "Look"
+	GROUP BY "showId", "number"
+	HAVING COUNT(*) > 1
+ ) AS "DuplicateLook"
+WHERE "Image"."lookId" = ANY("DuplicateLook"."id") AND "Image"."lookId" != "DuplicateLook"."idmin";
+ 
+DELETE FROM "Look" USING (
+  SELECT ARRAY_AGG("id") "id", MIN("id") "idmin"
+	FROM "Look"
+	GROUP BY "showId", "number"
+	HAVING COUNT(*) > 1
+ ) AS "DuplicateLook"
+WHERE "Look"."id" = ANY("DuplicateLook"."id") AND "Look"."id" != "DuplicateLook"."idmin";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Look_number_showId_key" ON "Look"("number", "showId");

prisma/schema.prisma
@@ -530,9 +530,8 @@ model Image {
   variantId Int?
 
   // The runway look the image is of.
-  // look   Look? @relation(fields: [lookId], references: [id], onDelete: Cascade, onUpdate: Cascade)
-  // lookId Int?
-  look Look?
+  look   Look? @relation(fields: [lookId], references: [id], onDelete: Cascade, onUpdate: Cascade)
+  lookId Int?
 
   // @todo store information on the models in the image (e.g. insta, etc).
 }
@@ -760,12 +759,10 @@ model Look {
   products Product[]
 
   // The look's picture (if known).
-  // images Image[]
-  image   Image @relation(fields: [imageId], references: [id], onDelete: Cascade, onUpdate: Cascade)
-  imageId Int   @unique
+  images Image[]
 
   // Each look must have a unique number in the show.
-  // @@unique([number, showId])
+  @@unique([number, showId])
 }
 
 // A review is a review for a show from a fashion critic or consumer.

scripts/node/save/shows/hermes.ts
@@ -35,9 +35,7 @@ export const looks = Array(NUM_LOOKS)
     }
     const look: Prisma.LookCreateWithoutShowInput = {
       number,
-      image: {
-        connectOrCreate: { where: { url: image.url }, create: image },
-      },
+      images: { connectOrCreate: { where: { url: image.url }, create: image } },
     }
     return look
   })

scripts/node/save/shows/isabel-marant.ts
@@ -35,9 +35,7 @@ const looks = Array(NUM_LOOKS)
     }
     const look: Prisma.LookCreateWithoutShowInput = {
       number,
-      image: {
-        connectOrCreate: { where: { url: image.url }, create: image },
-      },
+      images: { connectOrCreate: { where: { url: image.url }, create: image } },
     }
     return look
   })