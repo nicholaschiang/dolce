223cea4add69a9b745b2a10b5914cd1cd5666d4f
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-02 19:23:28 -0600

    perf(shows): virtualize and add infinite scrolling
    This patch virtualizes the shows page grid and adds infinite scrolling
    so that only the necessary subset of data is loaded and rendered at any
    given time. This change was necessary as I now have 20,857 fashion
    shows, which should not be rendered all at once.
    
    This implementation was heavily inspired by the masonry example on the
    Tanstack documentation and the infinite scrolling Remix example.
    
    Note that this is a purely FE performance improvement; the PostgreSQL
    queries will not be _fully_ optimized as I was forced to use offset
    based pagination instead of cursor based pagination (due to the custom
    ordering the shows page requires).
    
    This patch also ensures that the user's scroll position on the shows
    page is restored when navigating from anywhere (including breadcrumbs).
    
    Ref: https://tanstack.com/virtual/v3/docs/examples/react/variable
    Ref: https://github.com/remix-run/examples/blob/main/infinite-scrolling/app/routes/offset/advanced.tsx
    Ref: https://www.prisma.io/docs/concepts/components/prisma-client/pagination#-cons-of-offset-pagination
    
    Closes: NC-691
    Closes: NC-659

--- app/routes/shows._index.tsx
+++ app/routes/shows._index.tsx
@@ -1,11 +1,20 @@
-import { Link, useLoaderData } from '@remix-run/react'
-import { type V2_MetaFunction } from '@vercel/remix'
+import {
+  Link,
+  useNavigation,
+  useLoaderData,
+  useSearchParams,
+  useBeforeUnload,
+} from '@remix-run/react'
+import { useVirtualizer } from '@tanstack/react-virtual'
+import { type LoaderArgs, type V2_MetaFunction } from '@vercel/remix'
+import { useEffect, useRef, useCallback, useLayoutEffect } from 'react'
 
 import { Empty } from 'components/empty'
 import { Image } from 'components/image'
 
 import { prisma } from 'db.server'
 import { log } from 'log.server'
+import { cn } from 'utils/cn'
 import { getShowSeason, getShowPath } from 'utils/show'
 
 export const meta: V2_MetaFunction = () => [
@@ -21,23 +30,44 @@ export const meta: V2_MetaFunction = () => [
   },
 ]
 
-export async function loader() {
+// Load shows in pages of 100 (each page has 100 shows).
+const defaultTake = 200
+
+// Pre-load the next page of shows on scroll.
+const overscan = 40
+
+const getStartLimit = (searchParams: URLSearchParams) => ({
+  skip: Number(searchParams.get('skip') || '0'),
+  take: Number(searchParams.get('take') || defaultTake.toString()),
+})
+
+export async function loader({ request }: LoaderArgs) {
   log.debug('getting shows...')
-  const shows = await prisma.show.findMany({
-    include: {
-      season: true,
-      brand: true,
-      collections: true,
-      looks: {
-        include: { images: { take: 1 } },
-        orderBy: { number: 'asc' },
-        take: 1,
+  const [shows, count] = await Promise.all([
+    prisma.show.findMany({
+      ...getStartLimit(new URL(request.url).searchParams),
+      include: {
+        season: true,
+        brand: true,
+        collections: true,
+        looks: {
+          include: { images: { take: 1 } },
+          orderBy: { number: 'asc' },
+          take: 1,
+        },
       },
-    },
-    orderBy: [{ brand: { name: 'asc' } }, { season: { year: 'desc' } }],
-  })
+      orderBy: [
+        { brand: { name: 'asc' } },
+        { season: { year: 'desc' } },
+        { season: { name: 'asc' } },
+        { level: 'asc' },
+        { sex: 'asc' },
+      ],
+    }),
+    prisma.show.count(),
+  ])
   log.debug('got %d shows', shows.length)
-  return shows
+  return { shows, count }
 }
 
 // Eagerly load images for the first two rows of shows (above the fold).
@@ -46,56 +76,188 @@ const rowsToEagerLoad = 2
 // How many shows are shown in each row of results.
 const showsPerRow = 5
 
+// The height of each row (480px for item + 40px for margin).
+const rowHeight = 520
+
+// Do not attempt to run layout effects server-side.
+const isServerRender = typeof document === 'undefined'
+const useSSRLayoutEffect = isServerRender ? () => {} : useLayoutEffect
+
 export default function ShowsPage() {
-  const shows = useLoaderData<typeof loader>()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const { skip, take } = getStartLimit(searchParams)
+  const { shows, count } = useLoaderData<typeof loader>()
+
+  // Infinite scroll the shows grid list.
+  const parentRef = useRef<HTMLElement>(null)
+  const rowVirtualizer = useVirtualizer({
+    count,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => rowHeight,
+    lanes: showsPerRow,
+    overscan: 10,
+  })
+
+  // Save the user's scroll position and restore it upon revisiting the page.
+  const navigation = useNavigation()
+  useEffect(() => {
+    if (navigation.location && parentRef.current)
+      sessionStorage.setItem(
+        'infiniteScrollTop',
+        parentRef.current.scrollTop.toString(),
+      )
+  }, [navigation])
+  useBeforeUnload(
+    useCallback(() => {
+      if (parentRef.current)
+        sessionStorage.setItem(
+          'infiniteScrollTop',
+          parentRef.current.scrollTop.toString(),
+        )
+    }, []),
+  )
+  useSSRLayoutEffect(() => {
+    const infiniteScrollTop = sessionStorage.getItem('infiniteScrollTop')
+    if (parentRef.current && infiniteScrollTop)
+      parentRef.current.scrollTop = Number(infiniteScrollTop)
+  }, [])
+
+  // Load the results necessary to show the current window of data.
+  const lowerBoundary = skip + overscan
+  const upperBoundary = skip + take - overscan
+  const middleCount = Math.ceil(take / 2)
+
+  const [firstVirtualItem] = rowVirtualizer.getVirtualItems()
+  const [lastVirtualItem] = [...rowVirtualizer.getVirtualItems()].reverse()
+  if (!firstVirtualItem || !lastVirtualItem)
+    throw new Error('this should never happen')
+
+  let neededSkip = skip
+
+  if (firstVirtualItem.index < lowerBoundary) {
+    // User is scrolling up. Move the window up.
+    neededSkip =
+      Math.floor((firstVirtualItem.index - middleCount) / overscan) * overscan
+  } else if (lastVirtualItem.index > upperBoundary) {
+    // User is scrolling down. Move the window down.
+    neededSkip =
+      Math.ceil((lastVirtualItem.index - middleCount) / overscan) * overscan
+  }
+
+  // Can't go below zero.
+  if (neededSkip < 0) neededSkip = 0
+
+  // Can't go above our data count.
+  if (neededSkip + take > count) neededSkip = count - take
+
+  const isMountedRef = useRef(false)
+  useEffect(() => {
+    // We use a `setTimeout` here to avoid DOM exceptions when a user is
+    // scrolling too quickly and there are too many navigate calls.
+    if (isMountedRef.current && neededSkip !== skip) {
+      const timeoutId = setTimeout(() => {
+        setSearchParams(
+          { skip: String(neededSkip), take: defaultTake.toString() },
+          { replace: true },
+        )
+      }, 100)
+      // This cleanup function is needed in strict mode to prevent an
+      // unnecessary update to the search params `skip`. In strict mode, React
+      // runs effects twice. The first time the effect runs, the virtualizer
+      // hasn't measured the DOM yet and the `firstVirtualItem.index` is `0`.
+      // The second (correct) time it runs, the virtualizer will have measured
+      // the DOM and the index will be correctly set.
+      // @see {@link https://react.dev/learn/synchronizing-with-effects}
+      return () => clearTimeout(timeoutId)
+    }
+  }, [skip, neededSkip, setSearchParams])
+  useEffect(() => {
+    isMountedRef.current = true
+  }, [])
+
   return (
-    <main className='p-6 mx-auto max-w-screen-xl'>
-      <h1 className='text-6xl mb-6'>shows</h1>
-      {shows.length > 0 ? (
-        <ul
-          className='grid gap-x-2 gap-y-10'
-          style={{
-            gridTemplateColumns: `repeat(${showsPerRow}, minmax(0, 1fr))`,
-          }}
-        >
-          {shows.map((show, index) => (
-            <li key={show.id}>
-              <Link prefetch='intent' to={getShowPath(show)}>
-                <div className='bg-gray-100 dark:bg-gray-900 aspect-person mb-2'>
-                  {show.looks.length > 0 && (
-                    <Image
-                      className='object-cover h-full w-full'
-                      loading={
-                        index < showsPerRow * rowsToEagerLoad ? 'eager' : 'lazy'
-                      }
-                      decoding={
-                        index < showsPerRow * rowsToEagerLoad ? 'sync' : 'async'
-                      }
-                      src={show.looks[0].images[0]?.url}
-                      responsive={[
-                        100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
-                      ].map((width) => ({
-                        size: { width },
-                        maxWidth: width * showsPerRow,
-                      }))}
-                    />
-                  )}
-                </div>
-                <h2 className='text-xl font-serif font-semibold text-center'>
-                  {show.brand.name}
-                </h2>
-                <h3 className='text-xs uppercase text-center'>
-                  {getShowSeason(show)}
-                </h3>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <Empty>
-          There are no fashion shows to show yet. Please come again later.
-        </Empty>
-      )}
+    <main
+      ref={parentRef}
+      className='fixed inset-x-0 top-10 bottom-0 overflow-auto'
+    >
+      <div className='p-6 mx-auto max-w-screen-xl'>
+        <h1 className='text-6xl mb-6'>shows</h1>
+        {shows.length > 0 ? (
+          <ol
+            className='-m-1'
+            style={{
+              height: `${rowVirtualizer.getTotalSize()}px`,
+              position: 'relative',
+            }}
+          >
+            {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+              const index = isMountedRef.current
+                ? Math.abs(skip - virtualRow.index)
+                : virtualRow.index
+              const show = shows[index]
+              return (
+                <li
+                  data-id={show?.id}
+                  data-index={index}
+                  data-virtual-index={virtualRow.index}
+                  key={virtualRow.key}
+                  className={cn('p-1', show == null && 'cursor-wait')}
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: `${(virtualRow.lane / showsPerRow) * 100}%`,
+                    width: `${(1 / showsPerRow) * 100}%`,
+                    height: `${virtualRow.size}px`,
+                    transform: `translateY(${virtualRow.start}px)`,
+                  }}
+                >
+                  <Link prefetch='intent' to={show ? getShowPath(show) : ''}>
+                    <div
+                      className={cn(
+                        'bg-gray-100 dark:bg-gray-900 aspect-person mb-2',
+                        show == null && 'animate-pulse',
+                      )}
+                    >
+                      {show != null && show.looks.length > 0 && (
+                        <Image
+                          className='object-cover h-full w-full'
+                          loading={
+                            virtualRow.index < showsPerRow * rowsToEagerLoad
+                              ? 'eager'
+                              : 'lazy'
+                          }
+                          decoding={
+                            virtualRow.index < showsPerRow * rowsToEagerLoad
+                              ? 'sync'
+                              : 'async'
+                          }
+                          src={show.looks[0].images[0]?.url}
+                          responsive={[
+                            100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
+                          ].map((width) => ({
+                            size: { width },
+                            maxWidth: width * showsPerRow,
+                          }))}
+                        />
+                      )}
+                    </div>
+                    <h2 className='text-xl font-serif font-semibold text-center'>
+                      {show?.brand.name}
+                    </h2>
+                    <h3 className='text-xs uppercase text-center'>
+                      {show ? getShowSeason(show) : ''}
+                    </h3>
+                  </Link>
+                </li>
+              )
+            })}
+          </ol>
+        ) : (
+          <Empty>
+            There are no fashion shows to show yet. Please come again later.
+          </Empty>
+        )}
+      </div>
     </main>
   )
 }
