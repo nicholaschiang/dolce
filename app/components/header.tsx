5996d84d854ccf4c446cb94c153d53b34b63e189
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-06 18:19:00 -0600

    feat(index): add custom header to index page
    This patch adds a custom header to the index page that has links to a
    few important destinations. Note that I will probably be updating this
    soon. Most other e-commerce websites have the same header persistent
    across all their pages, and opt to only include breadcrumbs in a small
    sub-header at the top of each individual page (that isn't sticky).
    
    That design (persistent header + non-sticky breadcrumbs) has better
    discoverability than my current setup. I also will want to consider how
    to make my website feel more like an application like Notion with the
    way navigation is setup (e.g. a dedicated sidebar where users can
    drag-and-drop items into different collections/folders).
    
    This works for now.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,6 @@
 import { Link, useMatches } from '@remix-run/react'
 import { User, LogIn, LogOut, ChevronRight } from 'lucide-react'
-import { Fragment } from 'react'
+import { type PropsWithChildren, Fragment } from 'react'
 
 import { ThemeSwitcher } from 'components/theme-switcher'
 import { buttonVariants } from 'components/ui/button'
@@ -10,8 +10,18 @@ import { useOptionalUser } from 'utils'
 import { cn } from 'utils/cn'
 
 export function Header({ className }: { className?: string }) {
-  const matches = useMatches()
-  const user = useOptionalUser()
+  return (
+    <HeaderWrapper className={className}>
+      <HeaderBreadcrumbs />
+      <HeaderActions />
+    </HeaderWrapper>
+  )
+}
+
+export function HeaderWrapper({
+  className,
+  children,
+}: PropsWithChildren<{ className?: string }>) {
   return (
     <header
       className={cn(
@@ -19,43 +29,57 @@ export function Header({ className }: { className?: string }) {
         className,
       )}
     >
-      <ol className='flex items-center gap-2 text-lg tracking-tighter lowercase'>
-        {matches
-          .filter((match) => match.handle && match.handle.breadcrumb)
-          .map((match, index) => (
-            <Fragment key={match.id}>
-              {index !== 0 && (
-                <ChevronRight className='text-gray-300 dark:text-gray-600 h-4 w-4 mt-0.5' />
-              )}
-              <li>{(match.handle as Handle).breadcrumb(match)}</li>
-            </Fragment>
-          ))}
-      </ol>
-      <div className='flex items-center'>
-        {!matches.some((match) => match.id.includes('login')) && (
-          <Link
-            aria-label={user ? 'Log out' : 'Log in'}
-            className={buttonVariants({ size: 'icon', variant: 'ghost' })}
-            to={user ? '/logout' : '/login'}
-          >
-            {user ? (
-              <LogOut className='w-3 h-3' />
-            ) : (
-              <LogIn className='w-3 h-3' />
-            )}
-          </Link>
-        )}
-        {user && (
-          <Link
-            aria-label='Edit profile'
-            className={buttonVariants({ size: 'icon', variant: 'ghost' })}
-            to='/profile'
-          >
-            <User className='w-3 h-3' />
-          </Link>
-        )}
-        <ThemeSwitcher />
-      </div>
+      {children}
     </header>
+  )
+}
+
+export function HeaderBreadcrumbs() {
+  const matches = useMatches()
+  return (
+    <ol className='flex items-center gap-2 text-lg tracking-tighter lowercase'>
+      {matches
+        .filter((match) => match.handle && match.handle.breadcrumb)
+        .map((match, index) => (
+          <Fragment key={match.id}>
+            {index !== 0 && (
+              <ChevronRight className='text-gray-300 dark:text-gray-600 h-4 w-4 mt-0.5' />
+            )}
+            <li>{(match.handle as Handle).breadcrumb(match)}</li>
+          </Fragment>
+        ))}
+    </ol>
+  )
+}
+
+export function HeaderActions() {
+  const matches = useMatches()
+  const user = useOptionalUser()
+  return (
+    <div className='flex items-center'>
+      {!matches.some((match) => match.id.includes('login')) && (
+        <Link
+          aria-label={user ? 'Log out' : 'Log in'}
+          className={buttonVariants({ size: 'icon', variant: 'ghost' })}
+          to={user ? '/logout' : '/login'}
+        >
+          {user ? (
+            <LogOut className='w-3 h-3' />
+          ) : (
+            <LogIn className='w-3 h-3' />
+          )}
+        </Link>
+      )}
+      {user && (
+        <Link
+          aria-label='Edit profile'
+          className={buttonVariants({ size: 'icon', variant: 'ghost' })}
+          to='/profile'
+        >
+          <User className='w-3 h-3' />
+        </Link>
+      )}
+      <ThemeSwitcher />
+    </div>
   )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,51 @@
 import { FGIt as Italy } from '@icongo/fg'
-import { type PropsWithChildren } from 'react'
+import { Link } from '@remix-run/react'
+import { ChevronRight, Plus } from 'lucide-react'
+import { type PropsWithChildren, Fragment } from 'react'
+
+import { HeaderWrapper, HeaderActions } from 'components/header'
 
 export default function IndexPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+const links = [
+  { to: '/shows', label: 'Shows' },
+  { to: '/products', label: 'Products' },
+  { to: '/brands', label: 'Brands' },
+  { to: '/designers', label: 'Designers' },
+]
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      <ol className='flex items-center gap-2 text-lg tracking-tighter lowercase'>
+        <h1>dolce</h1>
+        <ChevronRight className='text-gray-300 dark:text-gray-600 h-4 w-4 mt-0.5' />
+        {links.map((link, index) => (
+          <Fragment key={link.to}>
+            {index !== 0 && (
+              <Plus className='text-gray-300 dark:text-gray-600 h-4 w-4 mt-0.5' />
+            )}
+            <li className='text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100 transition-colors'>
+              <Link prefetch='intent' to={link.to}>
+                {link.label}
+              </Link>
+            </li>
+          </Fragment>
+        ))}
+      </ol>
+      <HeaderActions />
+    </HeaderWrapper>
+  )
+}
+
+function Hero() {
   return (
     <main className='flex flex-col sm:flex-row sm:items-center justify-center mt-6 p-6 gap-6'>
       <div className='aspect-square w-full sm:w-72 sm:h-72 bg-gray-100 dark:bg-gray-900'>
@@ -21,7 +65,7 @@ export default function IndexPage() {
             performed in a sweet and soft manner.
           </Definition>
           <Definition label='origin'>
-            <Italy className='border border-gray-900 dark:border-white rounded-full mt-0.5' />
+            <Italy className='border border-gray-900 dark:border-white rounded-full mt-1' />
           </Definition>
         </dl>
       </article>
