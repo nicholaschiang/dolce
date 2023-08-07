nicholaschiang/dolce

commit e1dae960ef766930bde1f853205457d6a1e534b6

feat(index): replace timeline with "dolce" definition
This patch replaces the timeline of my projects with a definition of the
Italian word "dolce" that is now the brand name for this project.

Let's see where this goes.

app/routes/_index.tsx
@@ -1,145 +1,39 @@
-import { Link } from '@remix-run/react'
-import {
-  Projector,
-  ExternalLink,
-  Shirt,
-  CalendarClock,
-  type LucideIcon,
-} from 'lucide-react'
+import { FGIt as Italy } from '@icongo/fg'
+import { type PropsWithChildren } from 'react'
 
 export default function IndexPage() {
   return (
-    <main className='flex w-screen items-center justify-center'>
-      <section>
-        <Milestone
-          start={new Date(2023, 7, 20)}
-          title='/shows'
-          subtitle='rotten tomatoes for fashion'
-          url='/shows'
-          icon={Projector}
-        />
-        <Milestone
-          start={new Date(2023, 3, 18)}
-          title='/life'
-          subtitle='a graphical depiction'
-          url='/life/life.pdf'
-          icon={CalendarClock}
-          openInNew
-        />
-        <Milestone
-          start={new Date(2023, 0, 2)}
-          title='/products'
-          subtitle='a fashion archive'
-          url='/products'
-          icon={Shirt}
-        />
-        <Milestone
-          start={new Date(2023, 0, 1)}
-          title='nicholas.engineering'
-          subtitle='yet another portfolio'
-          url='https://github.com/nicholaschiang/site'
-          openInNew
-        />
-        <Milestone
-          start={new Date(2022, 5, 1)}
-          title='numbersstation.ai'
-          subtitle='a dbt generator'
-          url='https://numbersstation.ai'
-          openInNew
-        />
-        <Milestone
-          start={new Date(2022, 2, 1)}
-          end={new Date(2022, 7, 1)}
-          title='tweetscape.co'
-          subtitle='a twitter client'
-          url='https://github.com/rooteco/tweetscape'
-          openInNew
-        />
-        <Milestone
-          start={new Date(2021, 3, 1)}
-          end={new Date(2021, 11, 1)}
-          title='readhammock.com'
-          subtitle='a newsletter reader'
-          url='https://github.com/nicholaschiang/hammock'
-          openInNew
-        />
-        <Milestone
-          start={new Date(2019, 1, 1)}
-          end={new Date(2022, 6, 1)}
-          title='tutorbook.org'
-          subtitle='a booking platform'
-          url='https://github.com/tutorbookapp/tutorbook'
-          openInNew
-        />
-        <Milestone
-          start={new Date(2018, 8, 1)}
-          end={new Date(2019, 4, 1)}
-          title='sing.stanford.edu'
-          subtitle='some ml research'
-          url='https://dl.acm.org/doi/10.1145/3391906?cid=99659274049'
-          openInNew
-        />
-      </section>
+    <main className='flex flex-col sm:flex-row sm:items-center justify-center mt-6 p-6 gap-6'>
+      <div className='aspect-square w-full sm:w-72 sm:h-72 bg-gray-100 dark:bg-gray-900'>
+        <img src='/brands.jpg' className='object-cover h-full w-full' alt='' />
+      </div>
+      <article>
+        <h1 className='text-2xl font-medium mb-4 tracking-tighter flex items-center gap-0.5'>
+          <span>dol</span>
+          <span>·</span>
+          <span>ce</span>
+        </h1>
+        <dl>
+          <Definition label='adverb'>
+            (especially as a direction) sweetly and softly.
+          </Definition>
+          <Definition label='adjective'>
+            performed in a sweet and soft manner.
+          </Definition>
+          <Definition label='origin'>
+            <Italy className='border border-gray-900 dark:border-white rounded-full mt-0.5' />
+          </Definition>
+        </dl>
+      </article>
     </main>
   )
 }
 
-const DATE_FORMAT: Intl.DateTimeFormatOptions = {
-  month: 'numeric',
-  year: 'numeric',
-}
-
-interface MilestoneProps {
-  start: Date
-  end?: Date
-  title: string
-  subtitle: string
-  url?: string
-  icon?: LucideIcon
-  openInNew?: boolean
-}
-
-function Milestone({
-  start,
-  end,
-  title,
-  subtitle,
-  url,
-  icon,
-  openInNew,
-}: MilestoneProps) {
-  const Icon = icon ?? ExternalLink
+function Definition({ label, children }: PropsWithChildren<{ label: string }>) {
   return (
-    <article className='before:content-[" "] relative m-12 text-lg leading-none before:absolute before:-left-6.5 before:top-1/2 before:h-1 before:w-1 before:-translate-y-1/2 before:rounded-full before:bg-gray-300 after:absolute after:-left-[calc(1.5rem_+_0.5px)] after:top-1/2 after:h-[calc(100%_+_3rem)] after:border-l after:border-gray-300 last:after:hidden dark:before:bg-gray-100 dark:after:border-gray-100'>
-      <p className='text-xs lowercase text-gray-900/50 dark:text-gray-100/50'>
-        {start.toLocaleString(undefined, DATE_FORMAT)}
-        {end ? `–${end.toLocaleString(undefined, DATE_FORMAT)}` : ''}
-      </p>
-      <h1 className='my-2 font-semibold'>
-        {!url && title}
-        {url && !openInNew && (
-          <Link
-            to={url}
-            prefetch='intent'
-            className='items-top link group flex underline items-end'
-          >
-            {title}
-            <Icon className='ml-1 inline-block h-4 w-4 text-gray-400 dark:text-gray-500' />
-          </Link>
-        )}
-        {url && openInNew && (
-          <a
-            href={url}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='items-top link group flex underline items-end'
-          >
-            {title}
-            <Icon className='ml-1 inline-block h-4 w-4 text-gray-400 dark:text-gray-500' />
-          </a>
-        )}
-      </h1>
-      <h2>{subtitle}</h2>
-    </article>
+    <>
+      <dt className='italic text-gray-400 dark:text-gray-600'>{label}</dt>
+      <dd className='mb-4'>{children}</dd>
+    </>
   )
 }

pnpm-lock.yaml
@@ -7,6 +7,9 @@ dependencies:
   '@conform-to/zod':
     specifier: ^0.7.4
     version: 0.7.4(@conform-to/dom@0.7.4)(zod@3.21.4)
+  '@icongo/fg':
+    specifier: ^1.2.0
+    version: 1.2.0
   '@prisma/client':
     specifier: ^4.16.2
     version: 4.16.2(prisma@4.16.2)
@@ -2776,6 +2779,10 @@ packages:
   /@humanwhocodes/object-schema@1.2.1:
     resolution: {integrity: sha512-ZnQMnLV4e7hDlUvw8H+U8ASL02SS2Gn6+9Ac3wGGLIe7+je2AeAOxPY+izIPJDfFDb7eDjev0Us8MO1iFRN8hA==}
     dev: true
+
+  /@icongo/fg@1.2.0:
+    resolution: {integrity: sha512-wS/f2KCKPBYgPJzUWSmBk8hRhyqmGNraa08NY1jePWp3gKulfqscPpo3O2NxTDnoUWnngjlrllS7+2kiscZkVA==}
+    dev: false
 
   /@istanbuljs/schema@0.1.3:
     resolution: {integrity: sha512-ZXRY4jNvVgSVQ8DL3LTcakaAtXwTVUxE81hslsyD2AtoXW/wVob10HkOJ1X/pAlcI7D+2YoZKg5do8G/w6RYgA==}

commit ec91f7be1bd919f79869a2ab87feba50e5f49b73

refactor(index): add Italian flag icon separately
Instead of installing the entire flags icon pack (which for some reason
isn't being tree shaken properly and is causing my Vercel functions to
be around 1.91 MB in size), this patch updates the index page to simply
add that single icon as a React component.

Fixes: e1dae960ef76 ("feat(index): replace timeline with "dolce" definition")

app/routes/_index.tsx
@@ -1,83 +1,0 @@
-import { FGIt as Italy } from '@icongo/fg'
-import { Link } from '@remix-run/react'
-import { ChevronRight, Plus } from 'lucide-react'
-import { type PropsWithChildren, Fragment } from 'react'
-
-import { HeaderWrapper, HeaderActions } from 'components/header'
-
-export default function IndexPage() {
-  return (
-    <>
-      <Header />
-      <Hero />
-    </>
-  )
-}
-
-const links = [
-  { to: '/shows', label: 'Shows' },
-  { to: '/products', label: 'Products' },
-  { to: '/brands', label: 'Brands' },
-  { to: '/designers', label: 'Designers' },
-]
-
-function Header() {
-  return (
-    <HeaderWrapper>
-      <ol className='flex items-center gap-2 text-lg tracking-tighter lowercase'>
-        <h1>dolce</h1>
-        <ChevronRight className='text-gray-300 dark:text-gray-600 h-4 w-4 mt-0.5' />
-        {links.map((link, index) => (
-          <Fragment key={link.to}>
-            {index !== 0 && (
-              <Plus className='text-gray-300 dark:text-gray-600 h-4 w-4 mt-0.5' />
-            )}
-            <li className='text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100 transition-colors'>
-              <Link prefetch='intent' to={link.to}>
-                {link.label}
-              </Link>
-            </li>
-          </Fragment>
-        ))}
-      </ol>
-      <HeaderActions />
-    </HeaderWrapper>
-  )
-}
-
-function Hero() {
-  return (
-    <main className='flex flex-col sm:flex-row sm:items-center justify-center mt-6 p-6 gap-6'>
-      <div className='aspect-square w-full sm:w-72 sm:h-72 bg-gray-100 dark:bg-gray-900'>
-        <img src='/brands.jpg' className='object-cover h-full w-full' alt='' />
-      </div>
-      <article>
-        <h1 className='text-2xl font-medium mb-4 tracking-tighter flex items-center gap-0.5'>
-          <span>dol</span>
-          <span>·</span>
-          <span>ce</span>
-        </h1>
-        <dl>
-          <Definition label='adverb'>
-            (especially as a direction) sweetly and softly.
-          </Definition>
-          <Definition label='adjective'>
-            performed in a sweet and soft manner.
-          </Definition>
-          <Definition label='origin'>
-            <Italy className='border border-gray-900 dark:border-white rounded-full mt-1' />
-          </Definition>
-        </dl>
-      </article>
-    </main>
-  )
-}
-
-function Definition({ label, children }: PropsWithChildren<{ label: string }>) {
-  return (
-    <>
-      <dt className='italic text-gray-400 dark:text-gray-600'>{label}</dt>
-      <dd className='mb-4'>{children}</dd>
-    </>
-  )
-}

app/routes/_index/header.tsx
@@ -1,0 +1,36 @@
+import { Link } from '@remix-run/react'
+import { ChevronRight, Plus } from 'lucide-react'
+import { Fragment } from 'react'
+
+import { HeaderWrapper, HeaderActions } from 'components/header'
+
+const links = [
+  { to: '/shows', label: 'Shows' },
+  { to: '/products', label: 'Products' },
+  { to: '/brands', label: 'Brands' },
+  { to: '/designers', label: 'Designers' },
+]
+
+export function Header() {
+  return (
+    <HeaderWrapper>
+      <ol className='flex items-center gap-2 text-lg tracking-tighter lowercase'>
+        <h1>dolce</h1>
+        <ChevronRight className='text-gray-300 dark:text-gray-600 h-4 w-4 mt-0.5' />
+        {links.map((link, index) => (
+          <Fragment key={link.to}>
+            {index !== 0 && (
+              <Plus className='text-gray-300 dark:text-gray-600 h-4 w-4 mt-0.5' />
+            )}
+            <li className='text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100 transition-colors'>
+              <Link prefetch='intent' to={link.to}>
+                {link.label}
+              </Link>
+            </li>
+          </Fragment>
+        ))}
+      </ol>
+      <HeaderActions />
+    </HeaderWrapper>
+  )
+}

app/routes/_index/hero.tsx
@@ -1,0 +1,40 @@
+import { type PropsWithChildren } from 'react'
+
+import { Italy } from './italy'
+
+export function Hero() {
+  return (
+    <main className='flex flex-col sm:flex-row sm:items-center justify-center mt-6 p-6 gap-6'>
+      <div className='aspect-square w-full sm:w-72 sm:h-72 bg-gray-100 dark:bg-gray-900'>
+        <img src='/brands.jpg' className='object-cover h-full w-full' alt='' />
+      </div>
+      <article>
+        <h1 className='text-2xl font-medium mb-4 tracking-tighter flex items-center gap-0.5'>
+          <span>dol</span>
+          <span>·</span>
+          <span>ce</span>
+        </h1>
+        <dl>
+          <Definition label='adverb'>
+            (especially as a direction) sweetly and softly.
+          </Definition>
+          <Definition label='adjective'>
+            performed in a sweet and soft manner.
+          </Definition>
+          <Definition label='origin'>
+            <Italy className='border border-gray-900 dark:border-white rounded-full mt-1' />
+          </Definition>
+        </dl>
+      </article>
+    </main>
+  )
+}
+
+function Definition({ label, children }: PropsWithChildren<{ label: string }>) {
+  return (
+    <>
+      <dt className='italic text-gray-400 dark:text-gray-600'>{label}</dt>
+      <dd className='mb-4'>{children}</dd>
+    </>
+  )
+}

app/routes/_index/italy.tsx
@@ -1,0 +1,21 @@
+import * as React from 'react'
+
+export const Italy = React.forwardRef<
+  SVGSVGElement,
+  React.SVGProps<SVGSVGElement>
+>((props, ref) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 512 512'
+    height='1em'
+    width='1em'
+    ref={ref}
+    {...props}
+  >
+    <g fillRule='evenodd' strokeWidth='1pt'>
+      <path fill='#fff' d='M0 0h512v512H0z' />
+      <path fill='#009246' d='M0 0h170.7v512H0z' />
+      <path fill='#ce2b37' d='M341.3 0H512v512H341.3z' />
+    </g>
+  </svg>
+))

app/routes/_index/route.tsx
@@ -1,0 +1,11 @@
+import { Header } from './header'
+import { Hero } from './hero'
+
+export default function IndexPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}

pnpm-lock.yaml
@@ -7,9 +7,6 @@ dependencies:
   '@conform-to/zod':
     specifier: ^0.7.4
     version: 0.7.4(@conform-to/dom@0.7.4)(zod@3.21.4)
-  '@icongo/fg':
-    specifier: ^1.2.0
-    version: 1.2.0
   '@prisma/client':
     specifier: ^4.16.2
     version: 4.16.2(prisma@4.16.2)
@@ -2779,10 +2776,6 @@ packages:
   /@humanwhocodes/object-schema@1.2.1:
     resolution: {integrity: sha512-ZnQMnLV4e7hDlUvw8H+U8ASL02SS2Gn6+9Ac3wGGLIe7+je2AeAOxPY+izIPJDfFDb7eDjev0Us8MO1iFRN8hA==}
     dev: true
-
-  /@icongo/fg@1.2.0:
-    resolution: {integrity: sha512-wS/f2KCKPBYgPJzUWSmBk8hRhyqmGNraa08NY1jePWp3gKulfqscPpo3O2NxTDnoUWnngjlrllS7+2kiscZkVA==}
-    dev: false
 
   /@istanbuljs/schema@0.1.3:
     resolution: {integrity: sha512-ZXRY4jNvVgSVQ8DL3LTcakaAtXwTVUxE81hslsyD2AtoXW/wVob10HkOJ1X/pAlcI7D+2YoZKg5do8G/w6RYgA==}