nicholaschiang/dolce

commit 25b9ed63e3d3b6b631f80e7aa04b23905a433b2b

feat: use darker tailwind `dark` theme
This patch updates our `<body>` level styles and various colors
throughout the app to use the new darker `bg-gray-950` when in dark
mode. This change also helps the app better align with the colors that
come with `shadcn-ui` components out-of-box.

See: https://tailwindcss.com/blog/tailwindcss-v3-3#extended-color-palette-for-darker-darks

app/components/empty.tsx
@@ -9,7 +9,8 @@ const emptyVariants = cva(
   {
     variants: {
       variant: {
-        default: 'border-gray-300 dark:border-gray-600 text-gray-500',
+        default:
+          'border-gray-300 dark:border-gray-700 text-gray-500 dark:text-gray-600',
         error: 'border-red-700 text-red-800',
         warning: 'border-amber-600 text-amber-700',
       },

app/routes/_layout.products.$productId.tsx
@@ -116,7 +116,7 @@ export default function ProductPage() {
       onOpenAutoFocus={(event) => event.preventDefault()}
       className='w-full max-w-screen-lg'
     >
-      <div className='flex items-center gap-2.5 border-b border-gray-200/50 bg-gray-50 p-2 px-6 dark:border-gray-700/50 dark:bg-gray-800'>
+      <div className='flex items-center gap-2.5 border-b border-gray-200/50 bg-gray-50 p-2 px-6 dark:border-gray-700/50 dark:bg-gray-900'>
         <Tooltip tip='close' hotkey='esc'>
           <Dialog.Close className='icon-button square'>
             <Cross2Icon />
@@ -155,7 +155,7 @@ export default function ProductPage() {
             style={{ paddingTop: `${widthToHeightImageRatio * 100}%` }}
           >
             <Image
-              className='absolute top-0 h-full w-full overflow-hidden rounded-md bg-gray-100 dark:bg-gray-800'
+              className='absolute top-0 h-full w-full overflow-hidden rounded-md bg-gray-100 dark:bg-gray-900'
               loading='eager'
               decoding='sync'
               src={images[0].url}
@@ -376,7 +376,7 @@ Section.Content = function SectionContent({ children }: PropsWithChildren) {
 
 function Chip({ children }: PropsWithChildren) {
   return (
-    <div className='rounded border border-gray-200 bg-gray-50 px-1 py-0.5 text-2xs dark:border-gray-700 dark:bg-gray-800'>
+    <div className='rounded border border-gray-200 bg-gray-50 px-1 py-0.5 text-2xs dark:border-gray-700 dark:bg-gray-900'>
       {children}
     </div>
   )

app/root.tsx
@@ -53,7 +53,7 @@ function Header() {
   const matches = useMatches()
   const user = useOptionalUser()
   return (
-    <header className='sticky top-0 bg-white/80 dark:bg-gray-900/80 backdrop-blur-xl border-b border-gray-200 dark:border-gray-700 px-6 flex items-center justify-between h-10 z-10'>
+    <header className='sticky top-0 bg-white/80 dark:bg-gray-950/80 backdrop-blur-xl border-b border-gray-200 dark:border-gray-700 px-6 flex items-center justify-between h-10 z-10'>
       <ol className='flex items-center gap-2'>
         {matches
           .filter((match) => match.handle && match.handle.breadcrumb)
@@ -198,7 +198,7 @@ function App({ data, children }: { data?: LoaderData; children: ReactNode }) {
         <Links />
         <ThemeHead ssrTheme={Boolean(data?.theme)} />
       </head>
-      <body className='bg-white text-gray-900 selection:bg-gray-200 selection:text-black dark:bg-gray-900 dark:text-gray-100 dark:selection:bg-gray-700 dark:selection:text-gray-100'>
+      <body className='bg-white text-gray-900 selection:bg-gray-200 selection:text-gray-900 dark:bg-gray-950 dark:text-gray-50 dark:selection:bg-gray-800 dark:selection:text-gray-50'>
         <Header />
         {children}
         <script

app/routes/_layout.products.tsx
@@ -306,7 +306,7 @@ function ProductItem({
         >
           {images.length > 1 && (
             <Image
-              className='absolute top-0 z-20 h-full w-full overflow-hidden rounded-md bg-gray-100 opacity-0 transition-opacity duration-300 group-hover:opacity-100 dark:bg-gray-800'
+              className='absolute top-0 z-20 h-full w-full overflow-hidden rounded-md bg-gray-100 opacity-0 transition-opacity duration-300 group-hover:opacity-100 dark:bg-gray-900'
               loading='lazy'
               decoding='async'
               src={images[1]}
@@ -319,7 +319,7 @@ function ProductItem({
             />
           )}
           <Image
-            className='absolute top-0 z-10 h-full w-full overflow-hidden rounded-md bg-gray-100 dark:bg-gray-800'
+            className='absolute top-0 z-10 h-full w-full overflow-hidden rounded-md bg-gray-100 dark:bg-gray-900'
             loading={index < resultsPerRow * rowsToEagerLoad ? 'eager' : 'lazy'}
             decoding={
               index < resultsPerRow * rowsToEagerLoad ? 'sync' : 'async'

app/routes/shows.$showId/critic-reviews.tsx
@@ -45,7 +45,7 @@ type ReviewProps = {
 
 function Review({ author, publication, url, content }: ReviewProps) {
   return (
-    <figure className='overflow-hidden bg-gray-100 dark:bg-gray-800 pb-4'>
+    <figure className='overflow-hidden bg-gray-100 dark:bg-gray-900 pb-4'>
       <figcaption className='mt-8 text-center'>
         <cite className='text-lg underline underline-offset-4 decoration-2 decoration-gray-300 dark:decoration-gray-600'>
           <span className='text-gray-500'>By </span>

app/routes/shows.$showId/route.tsx
@@ -65,7 +65,7 @@ function Looks({ className }: { className: string }) {
       <ol className='grid grid-cols-2 gap-x-2 gap-y-6'>
         {show.looks.map((look) => (
           <li key={look.id}>
-            <div className='bg-gray-100 dark:bg-gray-800 aspect-person'>
+            <div className='bg-gray-100 dark:bg-gray-900 aspect-person'>
               <img
                 className='object-cover h-full'
                 src={look.image.url}

app/routes/shows.$showId/scores-header.tsx
@@ -70,7 +70,7 @@ export function ScoresHeader() {
   return (
     <div className='grid gap-2'>
       <video
-        className='aspect-video w-full bg-gray-100 dark:bg-gray-800'
+        className='aspect-video w-full bg-gray-100 dark:bg-gray-900'
         controls
         autoPlay
         playsInline
@@ -80,14 +80,14 @@ export function ScoresHeader() {
         Download the <a href={show.video.url}>MP4</a> video.
       </video>
       <div className='flex gap-2'>
-        <div className='flex-none w-40 bg-gray-100 dark:bg-gray-800 h-0 min-h-full'>
+        <div className='flex-none w-40 bg-gray-100 dark:bg-gray-900 h-0 min-h-full'>
           <img
             className='object-cover h-full'
             src={show.looks[0].image.url}
             alt=''
           />
         </div>
-        <article className='flex-1 bg-gray-100 dark:bg-gray-800 text-center px-6 flex flex-col'>
+        <article className='flex-1 bg-gray-100 dark:bg-gray-900 text-center px-6 flex flex-col'>
           <h1 className='font-serif font-bold text-5xl mb-1 mt-8'>
             {show.brands.map((brand) => brand.name).join(', ')}
           </h1>

app/routes/shows._index.tsx
@@ -31,7 +31,7 @@ export default function ShowsPage() {
           {shows.map((show) => (
             <li key={show.id}>
               <Link to={`/shows/${show.id}`}>
-                <div className='bg-gray-100 dark:bg-gray-800 aspect-person mb-2'>
+                <div className='bg-gray-100 dark:bg-gray-900 aspect-person mb-2'>
                   <img
                     className='object-cover h-full'
                     src={show.looks[0].image.url}
@@ -62,7 +62,7 @@ function ScoreKey({ className }: { className: string }) {
   return (
     <section
       className={cn(
-        'flex gap-4 bg-gray-100 dark:bg-gray-700 p-4 items-center',
+        'flex gap-4 bg-gray-100 dark:bg-gray-900 p-4 items-center',
         className,
       )}
     >

commit 4020785f277539442702682d3af6265bbe51ff38

fix: replace `border-gray-700` with `border-gray-800`
This patch ensures that all of the border colors are consistent
throughout the (now darker) app dark theme. This change ensures that my
component borders align with the border colors included with `shadcn-ui`
components.

This is the output of running:

```
$ ruplacer "border-gray-700" "border-gray-800" app --go
```

Ref: https://github.com/your-tools/ruplacer

Fixes: 25b9ed63e3d3 ("feat: use darker tailwind `dark` theme")

app/app.css
@@ -55,13 +55,13 @@
       @apply rounded-full;
     }
     &.outlined {
-      @apply border border-gray-200 bg-gray-100/50 text-gray-900/40 shadow-sm dark:border-gray-700 dark:bg-gray-800/50 dark:text-gray-100/50;
+      @apply border border-gray-200 bg-gray-100/50 text-gray-900/40 shadow-sm dark:border-gray-800 dark:bg-gray-800/50 dark:text-gray-100/50;
       &:not(:disabled):hover,
       &:not(:disabled):focus {
         @apply border-gray-300 bg-gray-200/50 dark:border-gray-600 dark:bg-gray-700/50;
       }
       &:disabled {
-        @apply cursor-not-allowed border-gray-200/50 text-gray-900/20 shadow-none dark:border-gray-700/50 dark:text-gray-100/20;
+        @apply cursor-not-allowed border-gray-200/50 text-gray-900/20 shadow-none dark:border-gray-800/50 dark:text-gray-100/20;
       }
     }
     &:not(.outlined) {
@@ -85,7 +85,7 @@
       }
     }
     &.outlined {
-      @apply border-gray-200 bg-gray-100/50 text-gray-900 shadow-sm dark:border-gray-700 dark:bg-gray-800/50 dark:text-gray-100;
+      @apply border-gray-200 bg-gray-100/50 text-gray-900 shadow-sm dark:border-gray-800 dark:bg-gray-800/50 dark:text-gray-100;
       &:not(:disabled):hover,
       &:not(:disabled):focus {
         @apply border-gray-300 bg-gray-200/50 dark:border-gray-600 dark:bg-gray-700/50;
@@ -94,7 +94,7 @@
   }
 
   .input {
-    @apply h-8 w-full appearance-none rounded border border-gray-200 bg-white px-3 outline-none transition-colors dark:border-gray-700 dark:bg-gray-900;
+    @apply h-8 w-full appearance-none rounded border border-gray-200 bg-white px-3 outline-none transition-colors dark:border-gray-800 dark:bg-gray-900;
     &:not(:disabled):hover,
     &:not(:disabled):focus {
       @apply border-gray-300 dark:border-gray-600;

app/components/dialog.tsx
@@ -24,7 +24,7 @@ export function Dialog({
         <DialogPrimitive.Overlay className='fixed inset-0 z-40 bg-white/50 dark:bg-gray-900/50' />
         <DialogPrimitive.Content
           className={cn(
-            'center fixed z-50 overflow-hidden rounded-lg border border-gray-200/50 bg-white shadow-2xl focus:outline-none dark:border-gray-700/50 dark:bg-gray-900',
+            'center fixed z-50 overflow-hidden rounded-lg border border-gray-200/50 bg-white shadow-2xl focus:outline-none dark:border-gray-800/50 dark:bg-gray-900',
             className,
           )}
           onOpenAutoFocus={onOpenAutoFocus}

app/components/empty.tsx
@@ -10,7 +10,7 @@ const emptyVariants = cva(
     variants: {
       variant: {
         default:
-          'border-gray-300 dark:border-gray-700 text-gray-500 dark:text-gray-600',
+          'border-gray-300 dark:border-gray-800 text-gray-500 dark:text-gray-600',
         error: 'border-red-700 text-red-800',
         warning: 'border-amber-600 text-amber-700',
       },

app/components/filters.tsx
@@ -139,7 +139,7 @@ export function Filters({
         [filters, addOrUpdateFilter, removeFilter],
       )}
     >
-      <nav className='frosted flex items-center justify-between border-b border-gray-200 px-6 py-3 dark:border-gray-700'>
+      <nav className='frosted flex items-center justify-between border-b border-gray-200 px-6 py-3 dark:border-gray-800'>
         <ul className='-mb-1.5 flex flex-wrap'>
           {filters.map((f) => (
             <>

app/components/menu.tsx
@@ -12,7 +12,7 @@ export type RootProps = {
 
 export function Root({ children }: RootProps) {
   return (
-    <Command className='frosted fixed mt-0.5 flex min-w-min max-w-xl origin-top-left flex-col overflow-hidden rounded-lg border border-gray-200 text-xs shadow-xl will-change-transform dark:border-gray-700'>
+    <Command className='frosted fixed mt-0.5 flex min-w-min max-w-xl origin-top-left flex-col overflow-hidden rounded-lg border border-gray-200 text-xs shadow-xl will-change-transform dark:border-gray-800'>
       {children}
     </Command>
   )
@@ -35,7 +35,7 @@ export function Input({
   return (
     <div
       className={cn(
-        'flex items-center border-gray-200 dark:border-gray-700',
+        'flex items-center border-gray-200 dark:border-gray-800',
         count > 0 && 'border-b',
       )}
     >

app/components/tooltip.tsx
@@ -39,7 +39,7 @@ export function Tooltip({ children, tip, hotkey, onHotkey }: TooltipProps) {
               'radix-side-bottom:animate-slide-up-fade',
               'radix-side-left:animate-slide-right-fade',
               'inline-flex items-center rounded px-2 py-1.25',
-              'border border-gray-200 dark:border-gray-700',
+              'border border-gray-200 dark:border-gray-800',
               'frosted z-50 gap-2 lowercase shadow-sm',
             )}
           >

app/routes/_header.$username.tsx
@@ -119,7 +119,7 @@ export default function UserPage() {
 
 function Tabs({ children }: PropsWithChildren) {
   return (
-    <nav className='border-t border-gray-200 dark:border-gray-700 flex justify-center gap-14'>
+    <nav className='border-t border-gray-200 dark:border-gray-800 flex justify-center gap-14'>
       {children}
     </nav>
   )

app/routes/_header.shows.$seasonYear.$seasonName.($sex).$brandSlug/designers.tsx
@@ -33,7 +33,7 @@ export function Designers() {
 function DesignerItem({ designer }: { designer: SerializeFrom<User> }) {
   const [expanded, setExpanded] = useState(false)
   return (
-    <li className='rounded-md shadow-sm p-4 flex gap-4 border border-gray-200 dark:border-gray-700'>
+    <li className='rounded-md shadow-sm p-4 flex gap-4 border border-gray-200 dark:border-gray-800'>
       <Avatar src={designer} className='h-20 w-20' />
       <div className={cn(!expanded && 'flex flex-1 flex-col h-0 min-h-full')}>
         <h3 className='flex items-center group gap-1 font-medium'>

app/routes/_header.shows.$seasonYear.$seasonName.($sex).$brandSlug/rate-and-review.tsx
@@ -119,7 +119,7 @@ export function RateAndReview() {
       <Form asChild>
         <RemixForm
           method='post'
-          className='max-w-sm mt-2 shadow-sm border border-gray-200 dark:border-gray-700 rounded-md p-4 relative'
+          className='max-w-sm mt-2 shadow-sm border border-gray-200 dark:border-gray-800 rounded-md p-4 relative'
           {...form.props}
         >
           {user == null && (

app/routes/_layout.products.$productId.tsx
@@ -118,7 +118,7 @@ export default function ProductPage() {
       onOpenAutoFocus={(event) => event.preventDefault()}
       className='w-full max-w-screen-lg'
     >
-      <div className='flex items-center gap-2.5 border-b border-gray-200/50 bg-gray-50 p-2 px-6 dark:border-gray-700/50 dark:bg-gray-900'>
+      <div className='flex items-center gap-2.5 border-b border-gray-200/50 bg-gray-50 p-2 px-6 dark:border-gray-800/50 dark:bg-gray-900'>
         <Tooltip tip='close' hotkey='esc'>
           <Dialog.Close className='icon-button square'>
             <X className='w-3 h-3' />
@@ -355,7 +355,7 @@ Section.Info = function SectionInfo({ children }: PropsWithChildren) {
           sideOffset={4}
           asChild
         >
-          <article className='frosted max-w-sm rounded border border-gray-200 p-3 text-2xs shadow-lg dark:border-gray-700'>
+          <article className='frosted max-w-sm rounded border border-gray-200 p-3 text-2xs shadow-lg dark:border-gray-800'>
             {children}
           </article>
         </Popover.Content>
@@ -378,7 +378,7 @@ Section.Content = function SectionContent({ children }: PropsWithChildren) {
 
 function Chip({ children }: PropsWithChildren) {
   return (
-    <div className='rounded border border-gray-200 bg-gray-50 px-1 py-0.5 text-2xs dark:border-gray-700 dark:bg-gray-900'>
+    <div className='rounded border border-gray-200 bg-gray-50 px-1 py-0.5 text-2xs dark:border-gray-800 dark:bg-gray-900'>
       {children}
     </div>
   )

app/routes/_layout.profile.tsx
@@ -124,8 +124,8 @@ export default function ProfilePage() {
   useEffect(() => setState(user), [user])
   return (
     <div className='flex h-full flex-1 items-center justify-center overflow-hidden'>
-      <header className='flex-1 px-6 border-r border-gray-200 dark:border-gray-700 h-full flex items-center justify-end bg-gray-50 dark:bg-gray-900'>
-        <div className='border border-gray-200 dark:border-gray-700 shadow-sm rounded-md p-4 bg-white dark:bg-gray-950 max-w-sm'>
+      <header className='flex-1 px-6 border-r border-gray-200 dark:border-gray-800 h-full flex items-center justify-end bg-gray-50 dark:bg-gray-900'>
+        <div className='border border-gray-200 dark:border-gray-800 shadow-sm rounded-md p-4 bg-white dark:bg-gray-950 max-w-sm'>
           <ConsumerReview
             author={state}
             content='Your profile is looking fantastic—the fashion is a bit lacking though... glad you’re here! 😮‍💨'