d2402381861ffa81e9e907eed8eec84857063bdd
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-08 00:24:30 -0600

    feat: add "sets" multi-select when saving a look
    This patch adds a multi-select that opens when a user clicks the "save"
    icon button. This multi-select allows users to filter their sets by name
    and select which one(s) they would like to save the look to.
    
    To implement this, I've added a few components from the `shadcn-ui`
    collection (`Command`, `Dialog`, and `Popover`). I also added an API
    endpoint to list the first 100 sets that include the user's query in
    their name. Note that I decided that adding full text-based search was
    out-of-scope for this particular feature (the set names shouldn't be too
    complex... and, most likely, a single user won't have 100+ sets).
    
    Ref: https://ui.shadcn.com/docs/components/combobox
    Ref: https://cmdk.paco.me/
    Ref: https://github.com/pacocoursey/cmdk#asynchronous-results
    Ref: https://www.prisma.io/docs/concepts/components/prisma-client/full-text-search#postgresql
    
    Closes: NC-713

---
+++ app/routes/api.sets.ts
@@ -0,0 +1,19 @@
+import { type LoaderArgs } from '@vercel/remix'
+
+import { prisma } from 'db.server'
+import { log } from 'log.server'
+import { getUserId } from 'session.server'
+
+export async function loader({ request }: LoaderArgs) {
+  log.debug('getting sets...')
+  const userId = await getUserId(request)
+  if (userId == null) return []
+  const search = new URL(request.url).searchParams.get('search') ?? ''
+  const sets = await prisma.set.findMany({
+    where: { authorId: userId, name: { contains: search.trim() } },
+    orderBy: [{ updatedAt: 'desc' }],
+    take: 100,
+  })
+  log.debug('got %d sets for query %s', sets.length, search)
+  return sets
+}
