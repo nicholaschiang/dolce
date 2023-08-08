2c482e945bd108dd618ffe2a25c155c88d5762d5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-08 15:26:06 -0600

    style(session.server): combine type imports

--- app/session.server.ts
+++ app/session.server.ts
@@ -1,8 +1,7 @@
 import { createCookieSessionStorage, redirect } from '@vercel/remix'
 import invariant from 'tiny-invariant'
 
-import type { User } from 'models/user.server'
-import { getUserById } from 'models/user.server'
+import { type User, getUserById } from 'models/user.server'
 
 invariant(process.env.SESSION_SECRET, 'SESSION_SECRET must be set')
 
