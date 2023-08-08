nicholaschiang/dolce

commit d09c281c33d66b26f07dbb09f7286e3e8bc04de1

perf(show/looks): preload sets on save button hover
This patch includes a minor performance improvement that should also
help avoid a seeming FOUC (because we load sets so fast it doesn't
appear to be a loading state but rather a FOUC).

app/routes/_header.shows.$seasonYear.$seasonName.($sex).$brandSlug/looks.tsx
@@ -87,16 +87,16 @@ function LookItem({ look }: { look: Look }) {
   // Refresh the search results after every keystroke (every few ms).
   const [search, setSearch] = useState('')
   const loaded = useRef('')
+  const endpoint = `/api/sets?search=${encodeURIComponent(search)}`
   useEffect(() => {
-    const endpoint = `/api/sets?search=${encodeURIComponent(search)}`
     const timeoutId = setTimeout(() => {
-      if (loaded.current !== endpoint) {
+      if (open && loaded.current !== endpoint) {
         fetcher.load(endpoint)
         loaded.current = endpoint
       }
     }, 50)
     return () => clearTimeout(timeoutId)
-  }, [fetcher, search])
+  }, [open, fetcher, endpoint])
 
   return (
     <li>
@@ -113,7 +113,11 @@ function LookItem({ look }: { look: Look }) {
         <p className='text-sm'>Look {look.number}</p>
         <Popover open={open} onOpenChange={setOpen}>
           <PopoverTrigger asChild>
-            <Button size='icon' variant='ghost'>
+            <Button
+              size='icon'
+              variant='ghost'
+              onMouseOver={() => fetcher.load(endpoint)}
+            >
               <Bookmark
                 className={cn(
                   'w-3 h-3',
@@ -133,6 +137,7 @@ function LookItem({ look }: { look: Look }) {
                 onValueChange={setSearch}
                 placeholder='Search sets...'
               />
+              {fetcher.state !== 'idle' && <CommandLoading />}
               <CommandEmpty>No sets found.</CommandEmpty>
               <CommandGroup>
                 {sets.map((set) => (

commit 290400bcb853ae77be49de8edcb540bf26ab6ea1

fix(save): don't duplicate preload request
This patch fixes an issue where the sets select would prefetch the sets
results with the current query on icon button hover and then would
immediately re-fetch the same results with the same query after the
select was opened.

Fixes: d09c281c33d6 ("perf(show/looks): preload sets on save button hover")

app/routes/_header.shows.$seasonYear.$seasonName.($sex).$brandSlug/looks.tsx
@@ -7,6 +7,7 @@ import {
   useEffect,
   useRef,
   useState,
+  useCallback,
 } from 'react'
 
 import { type action as saveAPI } from 'routes/api.looks.$lookId.save'
@@ -134,15 +135,16 @@ function SaveMenu({ look }: { look: Look }) {
   const [search, setSearch] = useState('')
   const loaded = useRef('')
   const endpoint = `/api/sets?search=${encodeURIComponent(search)}`
+  const load = useCallback(() => {
+    if (loaded.current !== endpoint) {
+      fetcher.load(endpoint)
+      loaded.current = endpoint
+    }
+  }, [fetcher, endpoint])
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      if (open && loaded.current !== endpoint) {
-        fetcher.load(endpoint)
-        loaded.current = endpoint
-      }
-    }, 50)
+    const timeoutId = setTimeout(open ? load : () => {}, 50)
     return () => clearTimeout(timeoutId)
-  }, [open, fetcher, endpoint])
+  }, [open, load])
 
   // I cannot rely on the CMD-K filtering and sorting as I want to show a
   // "create new set" item when there are no results.
@@ -154,11 +156,7 @@ function SaveMenu({ look }: { look: Look }) {
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <Button
-          size='icon'
-          variant='ghost'
-          onMouseOver={() => fetcher.load(endpoint)}
-        >
+        <Button size='icon' variant='ghost' onMouseOver={load}>
           <Bookmark
             className={cn(
               'w-3 h-3',