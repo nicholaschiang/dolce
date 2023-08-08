e77b7cef4560f794efe050b668873d5eda7cc1b5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-08 14:07:25 -0600

    refactor(save): use `requireUserId` function
    This patch replaces the `getUserId` and error handling with a simple
    `requireUserId` function call. I should replace this everywhere and use
    it going forward.

--- app/routes/api.looks.$lookId.save.create.ts
+++ app/routes/api.looks.$lookId.save.create.ts
@@ -1,17 +1,16 @@
-import { type ActionArgs, redirect } from '@vercel/remix'
+import { type ActionArgs } from '@vercel/remix'
 
 import { prisma } from 'db.server'
-import { getUserId } from 'session.server'
+import { requireUserId } from 'session.server'
 
 export async function action({ request, params }: ActionArgs) {
   const lookId = Number(params.lookId)
   if (Number.isNaN(lookId)) throw new Response('Not Found', { status: 404 })
   const look = await prisma.look.findUnique({ where: { id: lookId } })
   if (look == null) throw new Response('Not Found', { status: 404 })
-  const userId = await getUserId(request)
-  if (userId == null) return redirect(`/login?redirectTo=/shows/${look.showId}`)
+  const userId = await requireUserId(request, `/shows/${look.showId}`)
   const formData = await request.formData()
-  const name = formData.get('name')
+  const name = formData.get('name') || 'DEFAULT'
   if (typeof name !== 'string')
     throw new Response('Bad Request', { status: 400 })
   await prisma.look.update({
