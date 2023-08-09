59037657b46bc0bcee08f0daabc30d8496a6cf97
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-09 00:56:07 -0600

    feat(index): add basic map of fashion show locations
    This patch adds a basic world map showcasing fashion show locations
    using `react-simple-maps` and some `d3` helpers.
    
    Ref: https://www.react-simple-maps.io/examples/world-choropleth-mapchart/
    Ref: https://www.react-simple-maps.io/docs/projections/

--- remix.config.js
+++ remix.config.js
@@ -13,7 +13,7 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   serverModuleFormat: 'cjs',
-  serverDependenciesToBundle: ['nanoid/non-secure'],
+  serverDependenciesToBundle: ['nanoid/non-secure', /d3-.*/],
   images: {
     sizes: [200, 300, 400, 500, 600, 700, 800, 900, 1000],
     domains: ['aritzia.scene7.com'],
