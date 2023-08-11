nicholaschiang/dolce

commit 2e9eb00e7d3fbc5e4fcc15055f8de79c2471aef3

feat(routes/show): add "upload video" for curators
This patch adds "upload show video" functionality to the UI. This is
only shown to curators. Show videos will be uploaded to a new Supabase
bucket called "videos". They will be identified by ID. There is a file
size limit of 5 GB which is enforced by Supabase.

Ref: https://supabase.com/docs/guides/storage/uploads#standard-upload
Ref: https://supabase.com/pricing

Closes: NC-707

app/routes/api.shows.$showId.video.ts
@@ -1,0 +1,53 @@
+import { parse } from '@conform-to/zod'
+import { type ActionArgs, json } from '@vercel/remix'
+
+import { schema } from 'routes/_header.shows.($location).$year.$season.$sex.$level.$brand/scores-header'
+
+import { prisma, supabase } from 'db.server'
+import { log } from 'log.server'
+import { getUserId } from 'session.server'
+
+export async function action({ request, params }: ActionArgs) {
+  log.info('Updating video for show... %o', params)
+  const showId = Number(params.showId)
+  if (Number.isNaN(showId)) throw new Response('Not Found', { status: 404 })
+  const userId = await getUserId(request)
+  if (userId == null) throw new Response('Unauthorized', { status: 401 })
+  const user = await prisma.user.findUnique({ where: { id: userId } })
+  if (user == null) throw new Response('Not Found', { status: 404 })
+  if (user.id !== userId) throw new Response('Forbidden', { status: 403 })
+  if (user.curator === false) throw new Response('Forbidden', { status: 403 })
+
+  const formData = await request.formData()
+  const submission = parse(formData, { schema, stripEmptyValue: true })
+
+  if (!submission.value || submission.intent !== 'submit')
+    return json(submission, { status: 400 })
+
+  const { data, error } = await supabase.storage
+    .from('videos')
+    .upload(`shows/${showId}`, submission.value.video, { upsert: true })
+
+  if (error) {
+    log.error('Error updating video for show (%d): %s', showId, error.stack)
+    submission.error.video = error.message
+    return json(submission, { status: 500 })
+  }
+
+  const video = supabase.storage.from('videos').getPublicUrl(data.path)
+  const url = video.data.publicUrl
+  await prisma.show.update({
+    where: { id: showId },
+    data: {
+      video: {
+        connectOrCreate: {
+          where: { url },
+          create: { url, mimeType: submission.value.video.type },
+        },
+      },
+    },
+  })
+
+  log.info('Updated video for show (%d): %s', showId, url)
+  return json(submission)
+}

app/routes/_header.shows.($location).$year.$season.$sex.$level.$brand/scores-header.tsx
@@ -1,38 +1,36 @@
-import { useLoaderData } from '@remix-run/react'
-import { useMemo } from 'react'
+import { useForm } from '@conform-to/react'
+import { parse } from '@conform-to/zod'
+import { type Video } from '@prisma/client'
+import { useFetcher, useLoaderData } from '@remix-run/react'
+import { useRef, useMemo } from 'react'
+import { z } from 'zod'
+
+import { type action as videoAPI } from 'routes/api.shows.$showId.video'
 
 import { cn } from 'utils/cn'
+import { type Serialize, useOptionalUser } from 'utils/general'
 import { type Score, getScorePercentage } from 'utils/scores'
 import { getShowSeason } from 'utils/show'
 
 import { type loader } from './route'
 
+export const schema = z.object({
+  video: z
+    .instanceof(File)
+    .refine((file) => file.name !== '' && file.size > 0, 'Video is required')
+    .refine((file) => file.size < 5e9, 'Video cannot be larger than 5 GB'),
+})
+
 export function ScoresHeader() {
   const show = useLoaderData<typeof loader>()
+  const user = useOptionalUser()
   return (
     <div className='grid gap-2'>
-      {show.video != null && (
-        <>
-          <video
-            className='aspect-video w-full bg-gray-100 dark:bg-gray-900'
-            preload='auto'
-            controls
-            autoPlay
-            playsInline
-            muted
-            loop
-          >
-            <source src={show.video.url} type={show.video.mimeType} />
-            Download the <a href={show.video.url}>MP4</a> video.
-          </video>
-          <link
-            rel='preload'
-            href={show.video.url}
-            type={show.video.mimeType}
-            as='video'
-          />
-        </>
-      )}
+      {show.video ? (
+        <VideoPlayer video={show.video} />
+      ) : user?.curator ? (
+        <VideoForm />
+      ) : null}
       <div className='flex gap-2'>
         <div className='flex-none w-40 bg-gray-100 dark:bg-gray-900 h-0 min-h-full'>
           {show.looks.length > 0 && show.looks[0].images.length > 0 && (
@@ -95,5 +93,69 @@ function ScoreItem({ score, name }: ScoreItemProps) {
         </p>
       </div>
     </li>
+  )
+}
+
+function VideoPlayer({ video }: { video: Serialize<Video> }) {
+  return (
+    <>
+      <video
+        className='aspect-video w-full bg-gray-100 dark:bg-gray-900'
+        preload='auto'
+        controls
+        autoPlay
+        playsInline
+        muted
+        loop
+      >
+        <source src={video.url} type={video.mimeType} />
+        Download the <a href={video.url}>MP4</a> video.
+      </video>
+      <link rel='preload' href={video.url} type={video.mimeType} as='video' />
+    </>
+  )
+}
+
+function VideoForm() {
+  const fetcher = useFetcher<typeof videoAPI>()
+  const [form, { video }] = useForm({
+    lastSubmission: fetcher.data,
+    onValidate({ formData }) {
+      return parse(formData, { schema, stripEmptyValue: true })
+    },
+  })
+  const inputRef = useRef<HTMLInputElement>(null)
+  const show = useLoaderData<typeof loader>()
+  return (
+    <fetcher.Form
+      method='post'
+      action={`/api/shows/${show.id}/video`}
+      encType='multipart/form-data'
+      className={cn(
+        'relative aspect-video w-full bg-gray-100 dark:bg-gray-900',
+        fetcher.state !== 'idle' && 'animate-pulse',
+      )}
+      onChange={(event) => fetcher.submit(event.currentTarget)}
+      {...form.props}
+    >
+      <button
+        type='button'
+        className='absolute inset-0 w-full h-full flex items-center justify-center text-gray-400 dark:text-gray-600 text-sm'
+        disabled={fetcher.state !== 'idle'}
+        onClick={() => inputRef.current?.click()}
+      >
+        {fetcher.state !== 'idle'
+          ? 'Uploading video...'
+          : video.error ?? 'Click to upload show video'}
+      </button>
+      <input
+        ref={inputRef}
+        name={video.name}
+        className='hidden'
+        accept='video/*'
+        type='file'
+        required
+      />
+    </fetcher.Form>
   )
 }

commit 50ce90b48d8b5513015ea3eae04f1e78844363e8

fix(routes/show): upload video files client-side
This patch fixes an issue where Vercel serverless functions do not
support request body's that are larger than 4 MB (both in their edge
functions [[1]] and their normal Node.js serverless functions [[2]]).

Instead of tunneling the actual uploaded file through the Remix
back-end, I instead rely on the client (i.e. the user's browser) to make
the upload request. The Remix back-end (i.e. the Vercel Edge Function)
is then only responsible for generating a signed upload URL that
Supabase can use client-side to upload the given file.

With the introduction of the client-side Supabase SDK and the
accompanying publicly exposed `SUPABASE_ANON_KEY`, I've enabled RLS on
all tables in my Postgres database hosted by Supabase. I have not added
any rules: Nobody has access to anything using the `ANON_KEY` but instead
must know the database URI. The Remix back-end knows that URI and Prisma
uses it to connect directly to our Postgres instance [[3]].

[1]: https://vercel.com/docs/concepts/functions/edge-functions/limitations#limits-on-requests
[2]: https://vercel.com/docs/concepts/limits/overview#serverless-function-payload-size-limit
[3]: https://supabase.com/docs/guides/auth/row-level-security#you-dont-have-to-use-policies

Ref: https://vercel.com/docs/storage/vercel-blob#server-uploads-size-limit
Ref: https://vercel.com/guides/how-to-upload-and-store-files-with-vercel

Fixes: 2e9eb00e7d3f ("feat(routes/show): add "upload video" for curators")

.env.example
@@ -9,6 +9,7 @@ SESSION_SECRET="super-duper-s3cret"
 # supabase
 SUPABASE_URL=""
 SUPABASE_KEY=""
+SUPABASE_ANON_KEY=""
 
 # analytics
 FULLSTORY_ORG_ID=""

.github/workflows/deploy.yml
@@ -144,6 +144,7 @@ jobs:
           PORT: '8811'
           SUPABASE_URL: ${{ secrets.SUPABASE_URL }}
           SUPABASE_KEY: ${{ secrets.SUPABASE_KEY }}
+          SUPABASE_ANON_KEY: ${{ secrets.SUPABASE_ANON_KEY }}
           CYPRESS_RECORD_KEY: ${{ secrets.CYPRESS_RECORD_KEY }}
           GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
           # override the github commit message for better Cypress Cloud mesages.

app/db.client.ts
@@ -1,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+import invariant from 'tiny-invariant'
+
+const { SUPABASE_URL, SUPABASE_ANON_KEY } = window.env
+invariant(SUPABASE_URL, 'SUPABASE_URL env var not set')
+invariant(SUPABASE_ANON_KEY, 'SUPABASE_ANON_KEY env var not set')
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: { persistSession: false },
+})

app/root.tsx
@@ -147,6 +147,8 @@ type Env = {
   VERCEL_ANALYTICS_ID: string | undefined
   FULLSTORY_ORG_ID: string | undefined
   FULLSTORY_DEV_MODE: string | undefined
+  SUPABASE_URL: string | undefined
+  SUPABASE_ANON_KEY: string | undefined
 }
 
 declare global {
@@ -167,6 +169,8 @@ export async function loader({ request }: LoaderArgs) {
         VERCEL_ANALYTICS_ID: process.env.VERCEL_ANALYTICS_ID,
         FULLSTORY_ORG_ID: process.env.FULLSTORY_ORG_ID,
         FULLSTORY_DEV_MODE: process.env.FULLSTORY_DEV_MODE,
+        SUPABASE_URL: process.env.SUPABASE_URL,
+        SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
       } satisfies Env,
     },
     { headers },

app/routes/_header.shows.($location).$year.$season.$sex.$level.$brand/header.tsx
@@ -2,7 +2,7 @@ import { useForm } from '@conform-to/react'
 import { parse } from '@conform-to/zod'
 import { type Video } from '@prisma/client'
 import { useFetcher, useLoaderData } from '@remix-run/react'
-import { useRef, useMemo } from 'react'
+import { useRef, useMemo, useEffect, useState } from 'react'
 import { z } from 'zod'
 
 import { type action as videoAPI } from 'routes/api.shows.$showId.video'
@@ -11,6 +11,8 @@ import { cn } from 'utils/cn'
 import { type Serialize, useOptionalUser } from 'utils/general'
 import { type Score, getScorePercentage } from 'utils/scores'
 import { getShowSeason } from 'utils/show'
+
+import { supabase } from 'db.client'
 
 import { type loader } from './route'
 
@@ -117,34 +119,65 @@ function VideoPlayer({ video }: { video: Serialize<Video> }) {
 }
 
 function VideoForm() {
+  const show = useLoaderData<typeof loader>()
+  const action = `/api/shows/${show.id}/video`
   const fetcher = useFetcher<typeof videoAPI>()
+  const [file, setFile] = useState<File>()
+  const [uploading, setUploading] = useState(false)
+  const loading = uploading || fetcher.state !== 'idle'
+
+  // Actually upload the file to Supabase Storage. The action only creates a
+  // signed upload URL that is then used client-side to perform the upload.
+  const uploaded = useRef(file)
+  useEffect(() => {
+    async function upload() {
+      if (fetcher.data?.token && file && uploaded.current !== file) {
+        setUploading(true)
+        uploaded.current = file
+        const { data } = await supabase.storage
+          .from('videos')
+          .uploadToSignedUrl(`shows/${show.id}`, fetcher.data?.token, file)
+        if (data)
+          fetcher.submit(
+            { path: data.path, mimeType: file.type },
+            { action, method: 'PATCH' },
+          )
+        setUploading(false)
+      }
+    }
+    void upload()
+  }, [file, show.id, action, fetcher])
+
   const [form, { video }] = useForm({
-    lastSubmission: fetcher.data,
     onValidate({ formData }) {
       return parse(formData, { schema, stripEmptyValue: true })
     },
+    onSubmit(event, { formData }) {
+      event.preventDefault()
+      const submission = parse(formData, { schema, stripEmptyValue: true })
+      const videoFile = submission.value?.video
+      setFile(videoFile)
+      if (videoFile) fetcher.submit({}, { action, method: 'POST' })
+    },
   })
   const inputRef = useRef<HTMLInputElement>(null)
-  const show = useLoaderData<typeof loader>()
+
   return (
-    <fetcher.Form
-      method='post'
-      action={`/api/shows/${show.id}/video`}
-      encType='multipart/form-data'
+    <form
       className={cn(
         'relative aspect-video w-full bg-gray-100 dark:bg-gray-900',
-        fetcher.state !== 'idle' && 'animate-pulse',
+        loading && 'animate-pulse',
       )}
-      onChange={(event) => fetcher.submit(event.currentTarget)}
+      onChange={(event) => event.currentTarget.requestSubmit()}
       {...form.props}
     >
       <button
         type='button'
         className='absolute inset-0 w-full h-full flex items-center justify-center text-gray-400 dark:text-gray-600 text-sm'
-        disabled={fetcher.state !== 'idle'}
+        disabled={loading}
         onClick={() => inputRef.current?.click()}
       >
-        {fetcher.state !== 'idle'
+        {loading
           ? 'Uploading video...'
           : video.error ?? 'Click to upload show video'}
       </button>
@@ -156,6 +189,6 @@ function VideoForm() {
         type='file'
         required
       />
-    </fetcher.Form>
+    </form>
   )
 }

app/routes/api.shows.$showId.video.ts
@@ -1,14 +1,10 @@
-import { parse } from '@conform-to/zod'
 import { type ActionArgs, json } from '@vercel/remix'
-
-import { schema } from 'routes/_header.shows.($location).$year.$season.$sex.$level.$brand/header'
 
 import { prisma, supabase } from 'db.server'
 import { log } from 'log.server'
 import { getUserId } from 'session.server'
 
 export async function action({ request, params }: ActionArgs) {
-  log.info('Updating video for show... %o', params)
   const showId = Number(params.showId)
   if (Number.isNaN(showId)) throw new Response('Not Found', { status: 404 })
   const userId = await getUserId(request)
@@ -18,36 +14,52 @@ export async function action({ request, params }: ActionArgs) {
   if (user.id !== userId) throw new Response('Forbidden', { status: 403 })
   if (user.curator === false) throw new Response('Forbidden', { status: 403 })
 
-  const formData = await request.formData()
-  const submission = parse(formData, { schema, stripEmptyValue: true })
+  switch (request.method) {
+    case 'POST': {
+      log.info('Generating signed video upload URL for show... %o', params)
 
-  if (!submission.value || submission.intent !== 'submit')
-    return json(submission, { status: 400 })
+      const path = `shows/${showId}`
+      const { data, error } = await supabase.storage
+        .from('videos')
+        .createSignedUploadUrl(path)
 
-  const { data, error } = await supabase.storage
-    .from('videos')
-    .upload(`shows/${showId}`, submission.value.video, { upsert: true })
+      if (error) {
+        log.error(`Failed to generate signed video upload URL: %s`, error.stack)
+        throw new Response(error.message, { status: 500 })
+      }
 
-  if (error) {
-    log.error('Error updating video for show (%d): %s', showId, error.stack)
-    submission.error.video = error.message
-    return json(submission, { status: 500 })
-  }
+      log.info('Generated signed video upload URL for show (%d).', showId)
+      return json({ token: data.token }, { status: 201 })
+    }
+    case 'PATCH': {
+      log.info('Updating video for show... %o', params)
 
-  const video = supabase.storage.from('videos').getPublicUrl(data.path)
-  const url = video.data.publicUrl
-  await prisma.show.update({
-    where: { id: showId },
-    data: {
-      video: {
-        connectOrCreate: {
-          where: { url },
-          create: { url, mimeType: submission.value.video.type },
+      const formData = await request.formData()
+      const path = formData.get('path')
+      if (typeof path !== 'string')
+        throw new Response('Bad Request', { status: 400 })
+      const mimeType = formData.get('mimeType')
+      if (typeof mimeType !== 'string')
+        throw new Response('Bad Request', { status: 400 })
+
+      const video = supabase.storage.from('videos').getPublicUrl(path)
+      const url = video.data.publicUrl
+      await prisma.show.update({
+        where: { id: showId },
+        data: {
+          video: {
+            connectOrCreate: {
+              where: { url },
+              create: { url, mimeType },
+            },
+          },
         },
-      },
-    },
-  })
+      })
 
-  log.info('Updated video for show (%d): %s', showId, url)
-  return json(submission)
+      log.info('Updated video for show (%d): %s', showId, url)
+      return json({ token: null }, { status: 200 })
+    }
+    default:
+      throw new Response('Method Not Allowed', { status: 405 })
+  }
 }
50ce90b48d8b5513015ea3eae04f1e78844363e8
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-11 02:20:04 -0600

    fix(routes/show): upload video files client-side
    This patch fixes an issue where Vercel serverless functions do not
    support request body's that are larger than 4 MB (both in their edge
    functions [[1]] and their normal Node.js serverless functions [[2]]).
    
    Instead of tunneling the actual uploaded file through the Remix
    back-end, I instead rely on the client (i.e. the user's browser) to make
    the upload request. The Remix back-end (i.e. the Vercel Edge Function)
    is then only responsible for generating a signed upload URL that
    Supabase can use client-side to upload the given file.
    
    With the introduction of the client-side Supabase SDK and the
    accompanying publicly exposed `SUPABASE_ANON_KEY`, I've enabled RLS on
    all tables in my Postgres database hosted by Supabase. I have not added
    any rules: Nobody has access to anything using the `ANON_KEY` but instead
    must know the database URI. The Remix back-end knows that URI and Prisma
    uses it to connect directly to our Postgres instance [[3]].
    
    [1]: https://vercel.com/docs/concepts/functions/edge-functions/limitations#limits-on-requests
    [2]: https://vercel.com/docs/concepts/limits/overview#serverless-function-payload-size-limit
    [3]: https://supabase.com/docs/guides/auth/row-level-security#you-dont-have-to-use-policies
    
    Ref: https://vercel.com/docs/storage/vercel-blob#server-uploads-size-limit
    Ref: https://vercel.com/guides/how-to-upload-and-store-files-with-vercel
    
    Fixes: 2e9eb00e7d3f ("feat(routes/show): add "upload video" for curators")

--- app/routes/_header.shows.($location).$year.$season.$sex.$level.$brand/header.tsx
+++ app/routes/_header.shows.($location).$year.$season.$sex.$level.$brand/header.tsx
@@ -2,7 +2,7 @@ import { useForm } from '@conform-to/react'
 import { parse } from '@conform-to/zod'
 import { type Video } from '@prisma/client'
 import { useFetcher, useLoaderData } from '@remix-run/react'
-import { useRef, useMemo } from 'react'
+import { useRef, useMemo, useEffect, useState } from 'react'
 import { z } from 'zod'
 
 import { type action as videoAPI } from 'routes/api.shows.$showId.video'
@@ -11,6 +11,8 @@ import { cn } from 'utils/cn'
 import { type Serialize, useOptionalUser } from 'utils/general'
 import { type Score, getScorePercentage } from 'utils/scores'
 import { getShowSeason } from 'utils/show'
+
+import { supabase } from 'db.client'
 
 import { type loader } from './route'
 
@@ -117,34 +119,65 @@ function VideoPlayer({ video }: { video: Serialize<Video> }) {
 }
 
 function VideoForm() {
+  const show = useLoaderData<typeof loader>()
+  const action = `/api/shows/${show.id}/video`
   const fetcher = useFetcher<typeof videoAPI>()
+  const [file, setFile] = useState<File>()
+  const [uploading, setUploading] = useState(false)
+  const loading = uploading || fetcher.state !== 'idle'
+
+  // Actually upload the file to Supabase Storage. The action only creates a
+  // signed upload URL that is then used client-side to perform the upload.
+  const uploaded = useRef(file)
+  useEffect(() => {
+    async function upload() {
+      if (fetcher.data?.token && file && uploaded.current !== file) {
+        setUploading(true)
+        uploaded.current = file
+        const { data } = await supabase.storage
+          .from('videos')
+          .uploadToSignedUrl(`shows/${show.id}`, fetcher.data?.token, file)
+        if (data)
+          fetcher.submit(
+            { path: data.path, mimeType: file.type },
+            { action, method: 'PATCH' },
+          )
+        setUploading(false)
+      }
+    }
+    void upload()
+  }, [file, show.id, action, fetcher])
+
   const [form, { video }] = useForm({
-    lastSubmission: fetcher.data,
     onValidate({ formData }) {
       return parse(formData, { schema, stripEmptyValue: true })
     },
+    onSubmit(event, { formData }) {
+      event.preventDefault()
+      const submission = parse(formData, { schema, stripEmptyValue: true })
+      const videoFile = submission.value?.video
+      setFile(videoFile)
+      if (videoFile) fetcher.submit({}, { action, method: 'POST' })
+    },
   })
   const inputRef = useRef<HTMLInputElement>(null)
-  const show = useLoaderData<typeof loader>()
+
   return (
-    <fetcher.Form
-      method='post'
-      action={`/api/shows/${show.id}/video`}
-      encType='multipart/form-data'
+    <form
       className={cn(
         'relative aspect-video w-full bg-gray-100 dark:bg-gray-900',
-        fetcher.state !== 'idle' && 'animate-pulse',
+        loading && 'animate-pulse',
       )}
-      onChange={(event) => fetcher.submit(event.currentTarget)}
+      onChange={(event) => event.currentTarget.requestSubmit()}
       {...form.props}
     >
       <button
         type='button'
         className='absolute inset-0 w-full h-full flex items-center justify-center text-gray-400 dark:text-gray-600 text-sm'
-        disabled={fetcher.state !== 'idle'}
+        disabled={loading}
         onClick={() => inputRef.current?.click()}
       >
-        {fetcher.state !== 'idle'
+        {loading
           ? 'Uploading video...'
           : video.error ?? 'Click to upload show video'}
       </button>
@@ -156,6 +189,6 @@ function VideoForm() {
         type='file'
         required
       />
-    </fetcher.Form>
+    </form>
   )
 }
