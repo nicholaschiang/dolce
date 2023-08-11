ac226580ac8cf269c7f7fa86bfebd8ce271cbe18
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-11 00:10:03 -0600

    feat(routes/show): add "upload video" for curators
    This patch adds "upload show video" functionality to the UI. This is
    only shown to curators. Show videos will be uploaded to a new Supabase
    bucket called "videos". They will be identified by ID. There is a file
    size limit of 5 GB which is enforced by Supabase.
    
    Ref: https://supabase.com/docs/guides/storage/uploads#standard-upload
    Ref: https://supabase.com/pricing
    
    Closes: NC-707

---
+++ app/routes/api.shows.$showId.video.ts
@@ -0,0 +1,53 @@
+import { parse } from '@conform-to/zod'
+import { type ActionArgs, json } from '@vercel/remix'
+
+import { schema } from 'routes/_header.shows.($location).$year.$season.$sex.$level.$brand/scores-header'
+
+import { prisma, supabase } from 'db.server'
+import { log } from 'log.server'
+import { getUserId } from 'session.server'
+
+export async function action({ request, params }: ActionArgs) {
+  log.info('Updating video for show... %o', params)
+  const showId = Number(params.showId)
+  if (Number.isNaN(showId)) throw new Response('Not Found', { status: 404 })
+  const userId = await getUserId(request)
+  if (userId == null) throw new Response('Unauthorized', { status: 401 })
+  const user = await prisma.user.findUnique({ where: { id: userId } })
+  if (user == null) throw new Response('Not Found', { status: 404 })
+  if (user.id !== userId) throw new Response('Forbidden', { status: 403 })
+  if (user.curator === false) throw new Response('Forbidden', { status: 403 })
+
+  const formData = await request.formData()
+  const submission = parse(formData, { schema, stripEmptyValue: true })
+
+  if (!submission.value || submission.intent !== 'submit')
+    return json(submission, { status: 400 })
+
+  const { data, error } = await supabase.storage
+    .from('videos')
+    .upload(`shows/${showId}`, submission.value.video, { upsert: true })
+
+  if (error) {
+    log.error('Error updating video for show (%d): %s', showId, error.stack)
+    submission.error.video = error.message
+    return json(submission, { status: 500 })
+  }
+
+  const video = supabase.storage.from('videos').getPublicUrl(data.path)
+  const url = video.data.publicUrl
+  await prisma.show.update({
+    where: { id: showId },
+    data: {
+      video: {
+        connectOrCreate: {
+          where: { url },
+          create: { url, mimeType: submission.value.video.type },
+        },
+      },
+    },
+  })
+
+  log.info('Updated video for show (%d): %s', showId, url)
+  return json(submission)
+}
