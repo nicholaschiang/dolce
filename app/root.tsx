nicholaschiang/dolce

commit 0cf43042ad0becc60fb0979a99ae1723afaf71c1

feat: automatically revalidate on focus or reconnect
This patch enables automatic revalidation on page focus or network
reconnect. This mimics SWR behavior (e.g. Vercel's `useSWR`) and makes
for an excellent user experience: if I login to one tab, and then switch
to another tab, that next tab will show me logged in automatically.

Ref: https://sergiodxa.com/articles/automatic-revalidation-in-remix

app/root.tsx
@@ -38,6 +38,10 @@ import {
   useTheme,
 } from 'theme'
 import { NAME } from 'utils'
+import {
+  useRevalidateOnFocus,
+  useRevalidateOnReconnect,
+} from 'utils/revalidate'
 
 export type Handle = { breadcrumb: (match: RouteMatch) => LinkProps }
 
@@ -220,6 +224,10 @@ function App({ data, children }: { data?: LoaderData; children: ReactNode }) {
       return () => clearTimeout(timeoutId)
     }
   }, [navigation.state, type])
+
+  useRevalidateOnFocus()
+  useRevalidateOnReconnect()
+
   return (
     <html lang='en' className={cn('h-full', theme)}>
       <head>

app/utils/revalidate.ts
@@ -1,0 +1,42 @@
+import { useRevalidator } from '@remix-run/react'
+import { useEffect } from 'react'
+
+/**
+ * Hook to automatically revalidate the page data on tab focus.
+ * @see {@link https://sergiodxa.com/articles/automatic-revalidation-in-remix#revalidate-on-focus}
+ */
+export function useRevalidateOnFocus() {
+  const { revalidate } = useRevalidator()
+
+  useEffect(() => {
+    function onFocus() {
+      revalidate()
+    }
+    window.addEventListener('focus', onFocus)
+    return () => window.removeEventListener('focus', onFocus)
+  }, [revalidate])
+
+  useEffect(() => {
+    function onVisibilityChange() {
+      revalidate()
+    }
+    window.addEventListener('visibilitychange', onVisibilityChange)
+    return () =>
+      window.removeEventListener('visibilitychange', onVisibilityChange)
+  }, [revalidate])
+}
+
+/**
+ * Hook to automatically revalidate the page data on network reconnection.
+ * @see {@link https://sergiodxa.com/articles/automatic-revalidation-in-remix#revalidate-on-reconnect
+ */
+export function useRevalidateOnReconnect() {
+  const { revalidate } = useRevalidator()
+  useEffect(() => {
+    function onReconnect() {
+      revalidate()
+    }
+    window.addEventListener('online', onReconnect)
+    return () => window.removeEventListener('online', onReconnect)
+  }, [revalidate])
+}

commit b82c60260b23c1723b443e0a62f2c254638c71cc

fix: put automatic revalidation behind an env flag
This patch disables the automatic revalidation on focus and reconnect by
default and adds two environment variables that can be set to re-enable
it. I was experiencing issues with this where the revalidation would
often fail (e.g. due to Vercel timeout issues) and pages would appear to
simply break on re-focus.

Enabling this during development is also not an ideal experience, as I
no longer take advantage of the Remix HDR (I was revalidating all data
every time I switched between my terminal and Firefox).

Fixes: 0cf43042ad0b ("feat: automatically revalidate on focus or reconnect")

.env.example
@@ -1,5 +1,7 @@
 # feature flags
 OPTIMIZE_IMAGES="false"
+REVALIDATE_ON_FOCUS="false"
+REVALIDATE_ON_RECONNECT="false"
 
 # configuration
 DIRECT_URL="postgresql://postgres:password@localhost:5432/postgres?statement_cache_size=0"

app/root.tsx
@@ -149,6 +149,8 @@ type Env = {
   FULLSTORY_DEV_MODE: string | undefined
   SUPABASE_URL: string | undefined
   SUPABASE_ANON_KEY: string | undefined
+  REVALIDATE_ON_FOCUS: string | undefined
+  REVALIDATE_ON_RECONNECT: string | undefined
 }
 
 declare global {
@@ -171,6 +173,8 @@ export async function loader({ request }: LoaderArgs) {
         FULLSTORY_DEV_MODE: process.env.FULLSTORY_DEV_MODE,
         SUPABASE_URL: process.env.SUPABASE_URL,
         SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+        REVALIDATE_ON_FOCUS: process.env.REVALIDATE_ON_FOCUS,
+        REVALIDATE_ON_RECONNECT: process.env.REVALIDATE_ON_RECONNECT,
       } satisfies Env,
     },
     { headers },

app/utils/revalidate.ts
@@ -10,7 +10,7 @@ export function useRevalidateOnFocus() {
 
   useEffect(() => {
     function onFocus() {
-      revalidate()
+      if (window.env.REVALIDATE_ON_FOCUS === 'true') revalidate()
     }
     window.addEventListener('focus', onFocus)
     return () => window.removeEventListener('focus', onFocus)
@@ -18,7 +18,7 @@ export function useRevalidateOnFocus() {
 
   useEffect(() => {
     function onVisibilityChange() {
-      revalidate()
+      if (window.env.REVALIDATE_ON_FOCUS === 'true') revalidate()
     }
     window.addEventListener('visibilitychange', onVisibilityChange)
     return () =>
@@ -34,7 +34,7 @@ export function useRevalidateOnReconnect() {
   const { revalidate } = useRevalidator()
   useEffect(() => {
     function onReconnect() {
-      revalidate()
+      if (window.env.REVALIDATE_ON_RECONNECT === 'true') revalidate()
     }
     window.addEventListener('online', onReconnect)
     return () => window.removeEventListener('online', onReconnect)