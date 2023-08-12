b56769a7f98de5e16f385c4274f1731023f61663
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-11 20:36:05 -0600

    refactor: create reusable `<Carousel>` component
    This patch breaks the carousel logic from the map page into a separate
    reusable `<Carousel>` component that will be used in the shows list page
    and the show page (to add look / image carousels).

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -2,8 +2,8 @@ import { Location } from '@prisma/client'
 import { Link, useFetcher, useLoaderData } from '@remix-run/react'
 import { type SerializeFrom } from '@vercel/remix'
 import { scaleLinear } from 'd3-scale'
-import { ChevronLeft, ChevronRight, X } from 'lucide-react'
-import { type RefObject, useState, useCallback, useRef, useMemo } from 'react'
+import { X } from 'lucide-react'
+import { useCallback, useRef, useMemo } from 'react'
 import {
   ComposableMap,
   Geographies,
@@ -15,6 +15,7 @@ import {
 
 import { type loader as locationAPI } from 'routes/api.locations.$location'
 
+import { Carousel } from 'components/carousel'
 import { ClientOnly } from 'components/client-only'
 import { LoadingLine } from 'components/loading-line'
 import { Button } from 'components/ui/button'
@@ -159,15 +160,6 @@ const itemWidth = 120
 // The number of shows to show per slide in the location popover.
 const itemsPerSlide = 4
 
-// The width of the pagination dot.
-const dotSize = 6
-
-// The margin between each pagination dot.
-const dotMargin = 2
-
-// The number of dots (should be an odd number so there is a center dot).
-const maxDots = 5
-
 function LocationMarker({
   stats,
   radius,
@@ -178,7 +170,6 @@ function LocationMarker({
   coordinates: [number, number]
 }) {
   const fetcher = useFetcher<typeof locationAPI>()
-  const items = fetcher.data ?? []
   const loaded = useRef('')
   const endpoint = `/api/locations/${stats.location}`
   const load = useCallback(() => {
@@ -188,22 +179,8 @@ function LocationMarker({
     }
   }, [fetcher, endpoint])
 
-  const [index, setIndex] = useState(0)
-  const carouselRef = useRef<HTMLUListElement>(null)
-
   return (
-    <Popover
-      onOpenChange={() => {
-        setTimeout(() => {
-          if (carouselRef.current) {
-            const maxScroll = (items.length - itemsPerSlide) * itemWidth
-            const scroll = index * itemWidth
-            const left = Math.max(Math.min(scroll, maxScroll), 0)
-            carouselRef.current.scrollTo({ left, behavior: 'auto' })
-          }
-        })
-      }}
-    >
+    <Popover>
       <PopoverTrigger asChild onMouseOver={load}>
         <Marker
           coordinates={coordinates}
@@ -222,77 +199,23 @@ function LocationMarker({
         collisionPadding={12}
         className='w-auto text-sm p-0 overflow-hidden'
       >
-        <div className='relative group'>
-          <ul
-            className={cn(
-              'flex overflow-auto snap-x bg-gray-100 dark:bg-gray-900 scrollbar-hide',
-              !fetcher.data && fetcher.state === 'loading' && 'animate-pulse',
-            )}
-            style={{ width: itemWidth * itemsPerSlide }}
-            ref={carouselRef}
-            onScroll={() => {
-              if (carouselRef.current) {
-                const nextIndex = carouselRef.current.scrollLeft / itemWidth
-                setIndex(Math.round(nextIndex))
-              }
-            }}
-          >
-            {items.length === 0 && (
-              <li
-                className='aspect-person flex-none'
-                style={{ width: itemWidth }}
-              />
-            )}
-            {items.map((show) => (
-              <li
-                key={show.id}
-                style={{ width: itemWidth }}
-                className='aspect-person flex-none snap-start'
-              >
-                <Link
-                  to={`/shows/${show.id}`}
-                  prefetch='intent'
-                  className='block w-full h-full'
-                >
-                  <img
-                    src={show.looks[0].images[0].url}
-                    alt=''
-                    className='w-full h-full object-cover'
-                  />
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <div
-            className={cn(
-              'absolute inset-0 flex flex-col p-3 pointer-events-none',
-              items.length > 0 &&
-                'bg-gradient-to-b from-transparent from-80% to-black/25',
-            )}
-          >
-            <div className='flex-1 flex justify-between items-start'>
-              <PopoverClose asChild>
-                <Button
-                  variant='transparent'
-                  className='rounded-full h-5 w-5 pointer-events-auto'
-                  size='icon'
-                >
-                  <X className='h-3 w-3' />
-                </Button>
-              </PopoverClose>
-            </div>
-            {items.length > 0 && (
-              <>
-                <PaginationButtons
-                  index={index}
-                  numOfItems={items.length}
-                  carouselRef={carouselRef}
-                />
-                <Dots index={index} numOfItems={items.length} />
-              </>
-            )}
-          </div>
-        </div>
+        <Carousel
+          items={fetcher.data}
+          item={CarouselItem}
+          itemWidth={itemWidth}
+          itemsPerSlide={itemsPerSlide}
+          loading={fetcher.state === 'loading'}
+        >
+          <PopoverClose asChild>
+            <Button
+              variant='transparent'
+              className='rounded-full h-5 w-5 pointer-events-auto'
+              size='icon'
+            >
+              <X className='h-3 w-3' />
+            </Button>
+          </PopoverClose>
+        </Carousel>
         {fetcher.state === 'loading' && <LoadingLine className='-mt-px' />}
         <article className='py-2 px-3'>
           <h2 className='font-semibold'>{LOCATION_TO_NAME[stats.location]}</h2>
@@ -307,126 +230,24 @@ function LocationMarker({
   )
 }
 
-function PaginationButtons({
-  index,
-  numOfItems,
-  carouselRef,
-}: {
-  index: number
-  numOfItems: number
-  carouselRef: RefObject<HTMLElement | null>
-}) {
-  const minScroll = 0
-  const maxScroll = Math.max(
-    (numOfItems - itemsPerSlide) * itemWidth,
-    minScroll,
-  )
+type Show = SerializeFrom<typeof locationAPI>[number]
+
+function CarouselItem(show?: Show) {
+  if (!show) return <div className='w-full aspect-person' />
+  const { id, looks } = show
   return (
-    <div className='flex-1 flex justify-between items-center opacity-0 transition-opacity group-hover:opacity-100 duration-300'>
-      <Button
-        size='icon'
-        variant='outline'
-        className={cn(
-          'rounded-full group-hover:pointer-events-auto opacity-100 duration-200 transition-all',
-          index === 0 && 'opacity-0 pointer-events-none scale-50',
-        )}
-        onClick={() => {
-          if (carouselRef.current) {
-            const nextIndex = Math.max(index - 1, 0)
-            const left = Math.max(nextIndex * itemWidth, minScroll)
-            carouselRef.current.scrollTo({
-              left,
-              behavior: 'smooth',
-            })
-          }
-        }}
+    <div className='w-full aspect-person'>
+      <Link
+        to={`/shows/${id}`}
+        prefetch='intent'
+        className='block w-full h-full'
       >
-        <ChevronLeft className='h-3 w-3' />
-      </Button>
-      <Button
-        size='icon'
-        variant='outline'
-        className={cn(
-          'rounded-full group-hover:pointer-events-auto opacity-100 duration-200 transition-all',
-          index === numOfItems - itemsPerSlide &&
-            'opacity-0 pointer-events-none scale-50',
-        )}
-        onClick={() => {
-          if (carouselRef.current) {
-            const nextIndex = Math.min(index + 1, numOfItems - 1)
-            const left = Math.min(nextIndex * itemWidth, maxScroll)
-            carouselRef.current.scrollTo({
-              left,
-              behavior: 'smooth',
-            })
-          }
-        }}
-      >
-        <ChevronRight className='h-3 w-3' />
-      </Button>
-    </div>
-  )
-}
-
-function Dots({ index, numOfItems }: { index: number; numOfItems: number }) {
-  // The entire dot width is the dot size + dot margin.
-  const dotWidth = dotSize + dotMargin * 2
-
-  // Show a dot for every slide. Each slide has X items and scrolls by one item
-  // at a time. Thus, users can scroll items.length - X + 1 times.
-  const numOfDots = numOfItems - itemsPerSlide + 1
-
-  // Move the dots so that the active one is always in the center.
-  const center = Math.floor((maxDots - 1) / 2)
-  const transform = Math.max(0, Math.min(index - center, numOfDots - maxDots))
-
-  // The index of the left-most visible dot.
-  const left = transform
-
-  // The index of the right-most visible dot.
-  const right = left + maxDots - 1
-
-  return (
-    <div className='flex-1 flex justify-center items-end'>
-      <div
-        className='flex overflow-clip'
-        style={{ maxWidth: dotWidth * maxDots }}
-      >
-        <div
-          className='flex transition-transform duration-200'
-          style={{
-            transform: `translateX(-${transform * dotWidth}px)`,
-          }}
-        >
-          {Array(numOfDots)
-            .fill(null)
-            .map((_, idx) => {
-              return (
-                <span
-                  key={idx}
-                  className={cn(
-                    'flex-none rounded-full bg-white transition-all opacity-50 duration-200',
-                    idx === index && 'opacity-100',
-                    transform > 0 && idx <= left && 'scale-[.66]',
-                    transform > 0 && idx === left + 1 && 'scale-[.83]',
-                    transform < numOfDots - maxDots &&
-                      idx === right - 1 &&
-                      'scale-[.83]',
-                    transform < numOfDots - maxDots &&
-                      idx >= right &&
-                      'scale-[.66]',
-                  )}
-                  style={{
-                    width: dotSize,
-                    height: dotSize,
-                    marginLeft: dotMargin,
-                    marginRight: dotMargin,
-                  }}
-                />
-              )
-            })}
-        </div>
-      </div>
+        <img
+          src={looks[0].images[0].url}
+          alt=''
+          className='w-full h-full object-cover'
+        />
+      </Link>
     </div>
   )
 }
