ca4e48d5caf51d92c609c80b6c33d3b2c505543f
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-08-19 14:42:42 -0700

    feat(api/designers): use full text-based search

--- app/routes/api.designers.ts
+++ app/routes/api.designers.ts
@@ -1,6 +1,7 @@
 import { type LoaderArgs } from '@vercel/remix'
 
 import { prisma } from 'db.server'
+import { getSearch } from 'filters'
 import { log } from 'log.server'
 import { getUserId } from 'session.server'
 
@@ -11,9 +12,9 @@ export async function loader({ request }: LoaderArgs) {
   const user = await prisma.user.findUnique({ where: { id: userId } })
   if (user == null) throw new Response('Not Found', { status: 404 })
   if (user.curator === false) throw new Response('Forbidden', { status: 403 })
-  const search = new URL(request.url).searchParams.get('search') ?? ''
+  const search = getSearch(request)
   const designers = await prisma.user.findMany({
-    where: { articles: { some: {} }, name: { contains: search.trim() } },
+    where: { articles: { some: {} }, name: { search } },
     orderBy: [{ updatedAt: 'desc' }],
     take: 100,
   })
