nicholaschiang/dolce

commit 7c4c644e8080e42d78736d9c320ab4f53abba3d3

feat(show): make article headers sticky
This patch makes the critic review article headers sticky so that when
users scroll through to read them, they always have context as to who
wrote it and for which publication. Normally, this information is
available in the publication's website header or URL. On my site, this
information is now available persistently in the sticky article header.

app/routes/_layout.shows.($location).$year.$season.$sex.$level.$brand/critic-reviews.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from '@remix-run/react'
 import { ExternalLink } from 'lucide-react'
+import { useId } from 'react'
 
 import { Empty } from 'components/empty'
 
@@ -9,27 +10,27 @@ import { Section } from './section'
 export function CriticReviews() {
   const show = useLoaderData<typeof loader>()
   return (
-    <Section header='Critic Reviews' id='critic-reviews'>
+    <>
+      <Section
+        header='Critic Reviews'
+        id='critic-reviews'
+        className='pb-0 border-0'
+      />
       {show.articles.length === 0 && (
         <Empty className='mt-2'>
           No critic reviews to show yet. Try checking back later.
         </Empty>
       )}
-      {show.articles.length > 0 && (
-        <ol className='mt-2 grid gap-4'>
-          {show.articles.map((review) => (
-            <li key={review.id}>
-              <Review
-                author={review.author}
-                publication={review.publication.name}
-                url={review.url}
-                content={review.content}
-              />
-            </li>
-          ))}
-        </ol>
-      )}
-    </Section>
+      {show.articles.map((review) => (
+        <Review
+          key={review.id}
+          author={review.author}
+          publication={review.publication.name}
+          url={review.url}
+          content={review.content}
+        />
+      ))}
+    </>
   )
 }
 
@@ -41,9 +42,13 @@ type ReviewProps = {
 }
 
 function Review({ author, publication, url, content }: ReviewProps) {
+  const id = useId()
   return (
-    <figure className='overflow-hidden bg-gray-100 dark:bg-gray-900 rounded border border-gray-200 dark:border-gray-800'>
-      <figcaption className='p-3 text-center border-b border-gray-200 dark:border-gray-800 text-base'>
+    <>
+      <figcaption
+        id={id}
+        className='p-3 text-center border border-gray-200 dark:border-gray-800 text-base mx-6 rounded-t sticky -top-1 bg-gray-100 dark:bg-gray-900'
+      >
         <cite className='underline underline-offset-4 decoration-2 decoration-gray-300 dark:decoration-gray-600'>
           {author != null && (
             <>
@@ -68,11 +73,16 @@ function Review({ author, publication, url, content }: ReviewProps) {
           </a>
         </cite>
       </figcaption>
-      <blockquote
-        className='prose prose-sm prose-zinc dark:prose-invert p-3 m-auto'
-        cite={url}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </figure>
+      <figure
+        aria-labelledby={id}
+        className='overflow-hidden mx-6 mb-6 bg-gray-100 dark:bg-gray-900 rounded-b border-b border-x border-gray-200 dark:border-gray-800'
+      >
+        <blockquote
+          className='prose prose-sm prose-zinc dark:prose-invert p-3 m-auto'
+          cite={url}
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </figure>
+    </>
   )
 }

app/routes/_layout.shows.($location).$year.$season.$sex.$level.$brand/section.tsx
@@ -10,10 +10,14 @@ export function Section({
   id,
   header,
   children,
-}: PropsWithChildren<{ id: string; header: string }>) {
+  className,
+}: PropsWithChildren<{ id: string; header: string; className?: string }>) {
   return (
     <section
-      className='p-6 border-b border-gray-200 dark:border-gray-800 last:border-0 group/section'
+      className={cn(
+        'p-6 border-b border-gray-200 dark:border-gray-800 last:border-0 group/section',
+        className,
+      )}
       id={id}
     >
       <h1 className='font-medium mb-4 text-sm flex items-center justify-between gap-1 text-gray-500'>

commit 4088522713611d892dfc986995dc426552a5a8b7

fix(show/critic-reviews): add margin to empty state
This patch fixes an issue where there is no margin for the critic
reviews empty state. This was a regression introduced by the sticky
critic review headers change.

Fixes: 7c4c644e8080 ("feat(show): make article headers sticky")

app/routes/_layout.shows.($location).$year.$season.$sex.$level.$brand/critic-reviews.tsx
@@ -17,7 +17,7 @@ export function CriticReviews() {
         className='pb-0 border-0'
       />
       {show.articles.length === 0 && (
-        <Empty className='mt-2'>
+        <Empty className='mx-6 mb-6'>
           No critic reviews to show yet. Try checking back later.
         </Empty>
       )}