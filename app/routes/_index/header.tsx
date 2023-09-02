c052031004f43980fcfaf39f3cd99f0b2d91af71
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-09-01 23:28:57 -0600

    feat(index/header): link to latest shows season
    This patch updates the link in the homepage header to automatically show
    search results for only the latest season. This mimics the behavior of
    Vogue and other similar fashion publication websites.

--- app/routes/_index/header.tsx
+++ app/routes/_index/header.tsx
@@ -1,10 +1,22 @@
+import { SeasonName } from '@prisma/client'
 import { ChevronRight, Plus } from 'lucide-react'
+import { nanoid } from 'nanoid/non-secure'
 import { Fragment } from 'react'
 
 import { HeaderWrapper, HeaderActions, HeaderLink } from 'components/header'
 
+import { FILTER_PARAM, filterToSearchParam } from 'filters'
+
+// Link to the latest season of shows automatically (like Vogue's homepage).
+const param = filterToSearchParam<'season', 'is'>({
+  id: nanoid(5),
+  name: 'season',
+  condition: 'is',
+  value: { id: 106, name: SeasonName.RESORT, year: 2024 },
+})
+
 const links = [
-  { to: '/shows', label: 'Shows' },
+  { to: `/shows?${FILTER_PARAM}=${encodeURIComponent(param)}`, label: 'Shows' },
   { to: '/products', label: 'Products' },
   { to: '/brands', label: 'Brands' },
   { to: '/styles', label: 'Styles' },
