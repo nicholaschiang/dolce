94f2bc3c6cba1d7576d0c019de13cbcb10db8d55
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-09-01 22:22:16 -0600

    feat(products): add infinite scrolling to products
    This patch adds infinite scrolling to the products page. I refactored
    the infinite scrolling in the shows page to be a more reusable component
    (similar to the `<Carousel>` component that accepts an item component
    and data) and then reused that new `<InfiniteList>` component in the
    products list page.
    
    Closes: NC-710

--- app/routes/_layout.shows._index.tsx
+++ app/routes/_layout.shows._index.tsx
@@ -1,33 +1,21 @@
-import {
-  Link,
-  useNavigation,
-  useLoaderData,
-  useSearchParams,
-  useBeforeUnload,
-} from '@remix-run/react'
-import { type VirtualItem, useVirtualizer } from '@tanstack/react-virtual'
+import { Link, useLoaderData } from '@remix-run/react'
 import {
   type SerializeFrom,
   type LoaderArgs,
   type V2_MetaFunction,
 } from '@vercel/remix'
-import {
-  type RefObject,
-  type Dispatch,
-  type SetStateAction,
-  useState,
-  useEffect,
-  useRef,
-  useCallback,
-} from 'react'
+import { useState } from 'react'
 
 import { Banner } from 'components/banner'
 import { Carousel, type CarouselItemProps } from 'components/carousel'
-import { Empty } from 'components/empty'
 import { FiltersBar, getWhere } from 'components/filters-bar'
+import {
+  getPagination,
+  InfiniteList,
+  type InfiniteListItemProps,
+} from 'components/infinite-list'
 
-import { cn } from 'utils/cn'
-import { NAME, useLayoutEffect } from 'utils/general'
+import { NAME } from 'utils/general'
 import { getShowSeason, getShowPath } from 'utils/show'
 
 import { prisma } from 'db.server'
@@ -51,23 +39,12 @@ export const meta: V2_MetaFunction = () => [
   },
 ]
 
-// Load shows in pages of 100 (each page has 100 shows).
-const defaultTake = 100
-
-// Pre-load the next page of shows on scroll.
-const overscan = 10
-
-const getStartLimit = (searchParams: URLSearchParams) => ({
-  skip: Number(searchParams.get('skip') || '0'),
-  take: Number(searchParams.get('take') || defaultTake.toString()),
-})
-
 export async function loader({ request }: LoaderArgs) {
   const { where, string } = getWhere(request)
   log.debug('getting shows... %s', string)
   const [shows, filteredCount, totalCount] = await Promise.all([
     prisma.show.findMany({
-      ...getStartLimit(new URL(request.url).searchParams),
+      ...getPagination(new URL(request.url).searchParams),
       include: {
         season: true,
         brand: true,
@@ -96,20 +73,6 @@ export async function loader({ request }: LoaderArgs) {
 // The number of rows to display when at max width.
 const itemsPerRowDefault = 5
 
-// The min width of a single show.
-const minItemWidth = 240
-
-// The horizontal padding between the grid and the viewport.
-const padding = 24
-
-// The margin between each show item.
-const itemMargin = 8
-
-// Derive the item height from width (9:16 image + 50px text + 40px margin).
-function getItemHeight(itemWidth: number) {
-  return (itemWidth * 16) / 9 + 50 + 40
-}
-
 // Don't allow users to filter on back-end only fields.
 const hiddenFields: ShowFilterName[] = [
   'collections',
@@ -121,9 +84,8 @@ const hiddenFields: ShowFilterName[] = [
 ]
 
 export default function ShowsPage() {
-  const parentRef = useRef<HTMLDivElement>(null)
   const [itemsPerRow, setItemsPerRow] = useState(itemsPerRowDefault)
-  const { filteredCount, totalCount } = useLoaderData<typeof loader>()
+  const { shows, filteredCount, totalCount } = useLoaderData<typeof loader>()
   return (
     <>
       <Banner>
@@ -146,221 +108,42 @@ export default function ShowsPage() {
         filteredCount={filteredCount}
         totalCount={totalCount}
       />
-      <div
-        ref={parentRef}
-        style={{ padding }}
-        className='h-0 grow overflow-y-auto overflow-x-hidden'
-      >
-        {filteredCount > 0 ? (
-          <InfiniteList
-            parentRef={parentRef}
-            itemsPerRow={itemsPerRow}
-            setItemsPerRow={setItemsPerRow}
-          />
-        ) : (
-          <Empty>There are no fashion shows that match your filters.</Empty>
-        )}
-      </div>
+      <InfiniteList
+        items={shows}
+        item={ShowItem}
+        itemAspectRatio={9 / 16}
+        itemCount={filteredCount}
+        itemsPerRow={itemsPerRow}
+        setItemsPerRow={setItemsPerRow}
+        emptyMessage='There are no fashion shows that match your filters.'
+        className='h-0 grow'
+      />
     </>
   )
 }
 
-function InfiniteList({
-  parentRef,
-  itemsPerRow,
-  setItemsPerRow,
-}: {
-  parentRef: RefObject<HTMLElement>
-  itemsPerRow: number
-  setItemsPerRow: Dispatch<SetStateAction<number>>
-}) {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const { skip, take } = getStartLimit(searchParams)
-  const { shows, filteredCount } = useLoaderData<typeof loader>()
-
-  // The total width of the grid.
-  const [totalWidth, setTotalWidth] = useState(0)
-
-  // The height of each row (480px for item + 40px for margin).
-  const itemWidth = totalWidth / itemsPerRow
-  const itemHeight = getItemHeight(itemWidth)
-
-  // On window resize, recalculate the item width and number of shows per row.
-  useLayoutEffect(() => {
-    const handleResize = () => {
-      const newTotalWidth = window.innerWidth - padding * 2
-      setTotalWidth(newTotalWidth)
-      setItemsPerRow(Math.floor(newTotalWidth / minItemWidth))
-    }
-    handleResize()
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [setItemsPerRow])
-
-  // Infinite scroll the shows grid list.
-  const virtualizer = useVirtualizer({
-    getScrollElement: () => parentRef.current,
-    estimateSize: () => itemHeight,
-    lanes: itemsPerRow,
-    overscan: 10,
-    count: filteredCount,
-  })
-  useEffect(() => virtualizer.measure(), [itemsPerRow, virtualizer])
-
-  // Save the user's scroll position and restore it upon revisiting the page.
-  const navigation = useNavigation()
-  useEffect(() => {
-    if (navigation.location && parentRef.current)
-      sessionStorage.setItem(
-        'infiniteScrollTop',
-        parentRef.current.scrollTop.toString(),
-      )
-  }, [navigation, parentRef])
-  useBeforeUnload(
-    useCallback(() => {
-      if (parentRef.current)
-        sessionStorage.setItem(
-          'infiniteScrollTop',
-          parentRef.current.scrollTop.toString(),
-        )
-    }, [parentRef]),
-  )
-  useLayoutEffect(() => {
-    const infiniteScrollTop = sessionStorage.getItem('infiniteScrollTop')
-    if (parentRef.current && infiniteScrollTop)
-      parentRef.current.scrollTo({ top: Number(infiniteScrollTop) })
-  }, [parentRef])
-
-  // Load the results necessary to show the current window of data.
-  const lowerBoundary = skip + overscan
-  const upperBoundary = skip + take - overscan
-  const middleCount = Math.ceil(take / 2)
-
-  const [firstVirtualItem] = virtualizer.getVirtualItems()
-  const [lastVirtualItem] = [...virtualizer.getVirtualItems()].reverse()
-  if (!firstVirtualItem || !lastVirtualItem)
-    throw new Error('this should never happen')
-
-  let neededSkip = skip
-
-  if (firstVirtualItem.index < lowerBoundary) {
-    // User is scrolling up. Move the window up.
-    neededSkip =
-      Math.floor((firstVirtualItem.index - middleCount) / overscan) * overscan
-  } else if (lastVirtualItem.index > upperBoundary) {
-    // User is scrolling down. Move the window down.
-    neededSkip =
-      Math.ceil((lastVirtualItem.index - middleCount) / overscan) * overscan
-  }
-
-  // Can't go above our data count.
-  if (neededSkip + take > filteredCount) neededSkip = filteredCount - take
-
-  // Can't go below zero.
-  if (neededSkip < 0) neededSkip = 0
-
-  const isMountedRef = useRef(false)
-  useEffect(() => {
-    // We use a `setTimeout` here to avoid DOM exceptions when a user is
-    // scrolling too quickly and there are too many navigate calls.
-    if (isMountedRef.current && neededSkip !== skip) {
-      const timeoutId = setTimeout(() => {
-        setSearchParams(
-          (prev) => {
-            const next = new URLSearchParams(prev)
-            next.set('skip', neededSkip.toString())
-            next.set('take', defaultTake.toString())
-            return next
-          },
-          { replace: true },
-        )
-      }, 100)
-      // This cleanup function is needed in strict mode to prevent an
-      // unnecessary update to the search params `skip`. In strict mode, React
-      // runs effects twice. The first time the effect runs, the virtualizer
-      // hasn't measured the DOM yet and the `firstVirtualItem.index` is `0`.
-      // The second (correct) time it runs, the virtualizer will have measured
-      // the DOM and the index will be correctly set.
-      // @see {@link https://react.dev/learn/synchronizing-with-effects}
-      return () => clearTimeout(timeoutId)
-    }
-  }, [skip, neededSkip, setSearchParams])
-  useEffect(() => {
-    isMountedRef.current = true
-  }, [])
-
-  return (
-    <ol
-      style={{
-        visibility: totalWidth ? 'visible' : 'hidden',
-        height: `${virtualizer.getTotalSize()}px`,
-        margin: `-${itemMargin}px`,
-        position: 'relative',
-      }}
-    >
-      {virtualizer.getVirtualItems().map((virtualRow) => {
-        const index = isMountedRef.current
-          ? Math.abs(skip - virtualRow.index)
-          : virtualRow.index
-        const show = shows[index]
-        return (
-          <ShowItem
-            show={show}
-            itemsPerRow={itemsPerRow}
-            itemWidth={itemWidth}
-            virtualRow={virtualRow}
-            key={virtualRow.key}
-          />
-        )
-      })}
-    </ol>
-  )
-}
-
 type Show = SerializeFrom<typeof loader>['shows'][number]
-type ShowItemProps = {
-  show?: Show
-  virtualRow: VirtualItem
-  itemsPerRow: number
-  itemWidth: number
-}
 
-function ShowItem({ show, virtualRow, itemsPerRow, itemWidth }: ShowItemProps) {
+function ShowItem({ item: show }: InfiniteListItemProps<Show>) {
   return (
-    <li
-      data-id={show?.id}
-      data-index={virtualRow.index}
-      key={virtualRow.key}
-      className={cn(show == null && 'cursor-wait')}
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: `${(virtualRow.lane / itemsPerRow) * 100}%`,
-        width: `${itemWidth}px`,
-        height: `${virtualRow.size}px`,
-        transform: `translateY(${virtualRow.start}px)`,
-        padding: `${itemMargin / 2}px`,
-      }}
+    <Link
+      to={show ? getShowPath(show) : ''}
+      prefetch='intent'
+      className='block'
     >
-      <Link
-        to={show ? getShowPath(show) : ''}
-        prefetch='intent'
-        className='block'
-      >
-        <Carousel
-          className='mb-3'
-          loading={show == null}
-          items={show?.looks}
-          item={ShowLookItem}
-        />
-        <h2 className='text-xl font-serif font-semibold text-center leading-none mb-1'>
-          {show?.brand.name}
-        </h2>
-        <h3 className='text-xs uppercase text-center'>
-          {show ? getShowSeason(show) : ''}
-        </h3>
-      </Link>
-    </li>
+      <Carousel
+        className='mb-3'
+        loading={show == null}
+        items={show?.looks}
+        item={ShowLookItem}
+      />
+      <h2 className='text-xl font-serif font-semibold text-center leading-none mb-1'>
+        {show?.brand.name}
+      </h2>
+      <h3 className='text-xs uppercase text-center'>
+        {show ? getShowSeason(show) : ''}
+      </h3>
+    </Link>
   )
 }
 
