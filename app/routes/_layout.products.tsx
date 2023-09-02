1ab16dcfffb7051891d34876f2d6c398166da9ca
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-09-01 23:59:45 -0600

    fix: remove `looks` from filterable attributes
    Right now, this isn't very useful and is currently only showing empty
    values when filtering nested options.

--- app/routes/_layout.products.tsx
+++ app/routes/_layout.products.tsx
@@ -64,7 +64,7 @@ const minResultsPerRow = 1
 const maxResultsPerRow = 20
 
 // Don't allow users to filter on back-end only fields.
-const hiddenFields: ProductFilterName[] = []
+const hiddenFields: ProductFilterName[] = ['looks']
 
 export default function ProductsPage() {
   const { products, filteredCount, totalCount } = useLoaderData<typeof loader>()
