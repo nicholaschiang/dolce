4394504cf2b134fd0c86f0e148d5c9a344a3a4a6
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-09-27 17:37:54 -0600

    feat(scripts): add spider to scrape Shopify products
    This patch adds a new Shopify Storefront API scraper script that fetches
    the given store's `products.json` data. This commit also includes the
    output of running it for Aime Leon Dore:
    
    ```
    $ scrapy crawl shopify -O aimeleondore.json
    ```
    
    Ref: https://shopify.dev/docs/api/usage/rate-limits#storefront-api-rate-limits
    Ref: https://github.com/yasserqureshi1/Sneaker-Monitors/blob/master/monitors/shopify/monitor.py

---
+++ scripts/python/spiders/shopify.py
@@ -0,0 +1,29 @@
+from urllib.parse import urlparse, parse_qs
+
+import scrapy
+import json
+
+domains = ["kith.com", "aimeleondore.com"]
+
+
+class ShopifySpider(scrapy.Spider):
+    name = "shopify"
+    start_urls = [
+        f"https://www.{domain}/products.json?page=1&limit=250" for domain in domains
+    ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+        print("------- START RESPONSE TEXT -------")
+        print(response.text)
+        print("-------- END RESPONSE TEXT --------")
+        data = json.loads(response.text)
+        yield from data["products"]
+        if len(data["products"]) > 0:
+            url = urlparse(response.url)
+            page = int(parse_qs(url.query)["page"][0])
+            next_url = url._replace(query=f"page={page + 1}&limit=250")
+            yield response.follow(next_url.geturl())
