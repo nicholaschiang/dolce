4973e35314156c2feed1ba1b107029327acd8fe6
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-09-28 16:49:39 -0600

    feat(routes/variants): search variants by color name
    This patch updates the variant filter route to get results by searching
    for the given string in the color names. This is not a perfect solution
    as it only works once a user inputs a full color name (e.g. "B" will
    have no results while "Blue" will show a bunch).
    
    Ref: https://www.prisma.io/docs/concepts/components/prisma-client/full-text-search

--- app/routes/_layout.variants.tsx
+++ app/routes/_layout.variants.tsx
@@ -1,18 +1,21 @@
 import { Link, useLoaderData } from '@remix-run/react'
+import { type LoaderArgs } from '@vercel/remix'
 
 import { ListLayout } from 'components/list-layout'
 
 import { getColorFilter } from 'utils/variant'
 
 import { prisma } from 'db.server'
-import { FILTER_PARAM, filterToSearchParam } from 'filters'
+import { FILTER_PARAM, filterToSearchParam, getSearch } from 'filters'
 import { log } from 'log.server'
 
-export async function loader() {
+export async function loader({ request }: LoaderArgs) {
   log.debug('getting variants...')
+  const search = getSearch(request)
   const variants = await prisma.variant.findMany({
     take: 100,
     include: { colors: true },
+    where: search ? { colors: { some: { name: { search } } } } : undefined,
   })
   log.debug('got %d variants', variants.length)
   return variants
