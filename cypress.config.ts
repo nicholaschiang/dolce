e3f0831d82ba2d74e718056f468dcd991d4a9ec8
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-09-29 22:26:06 -0600

    test(cypress): define custom commands in support
    This patch moves all the custom commands into the support file. There
    were some compilation issues with the new Cypress version.

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     projectId: '9dj6b9',
+    supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges
       const port = process.env.PORT ?? (isDev ? '6969' : '8811')
