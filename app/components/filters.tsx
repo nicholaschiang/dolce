nicholaschiang/dolce

commit 240e3dcfda83b79e098b9df583cfff5cb5a15507

feat(prisma): products can be added to sets too
This patch updates the `Set` data model so that it can contain both
products and looks.

app/components/filters.tsx
@@ -60,6 +60,7 @@ const MODEL_TO_ROUTE: Record<string, string> = {
   Show: '/shows',
   User: '/designers',
   Video: '/videos',
+  Set: '/sets',
 }
 
 //////////////////////////////////////////////////////////////////

app/filters.ts
@@ -47,6 +47,7 @@ type PrismaProductFilter =
   | Prisma.BrandListRelationFilter
   | Prisma.UserListRelationFilter
   | Prisma.LookListRelationFilter
+  | Prisma.SetListRelationFilter
 type PrismaShowFilter =
   | Prisma.IntFilter<'Show'>
   | Prisma.IntNullableFilter<'Show'>

prisma/schema.prisma
@@ -655,6 +655,9 @@ model Product {
   // The product's looks (runway outfits that it was included in).
   looks Look[]
 
+  // The user-curated sets that the product is a part of.
+  sets Set[]
+
   // @todo perhaps we should also store the product's original source?
 
   // @todo products must have a unique name per brand(s).
@@ -667,9 +670,14 @@ model Product {
 // similar looks).
 //
 // Sets are separate from collections for simplicity. Collections are officially
-// curated by a brand while sets are simple groupings of looks created by users.
-// I may opt to combine these two data models in the future, but for now, simply
-// adding an additional data model was easiest.
+// curated by a brand while sets are simple groupings of looks and products
+// created by users. I may opt to combine these two data models in the future, 
+// but for now, simply adding an additional data model was easiest.
+//
+// Users can import looks and products from anywhere to add to their sets (e.g.
+// if I see an outfit I like on Instagram, I can click "share" to DOLCE and the
+// app will add it as a look to the selected set... that look will then be
+// automatically augmented with possible products to purchase).
 //
 // The name "set" was inspired by the website "Polyvore" which allowed users to
 // add products to a shared index called a "set".
@@ -693,6 +701,9 @@ model Set {
 
   // The set's looks.
   looks Look[]
+
+  // The set's products.
+  products Product[]
 
   // A user can only have one set with a given name.
   @@unique([name, authorId])

prisma/migrations/20231001201618_sets_can_include_products_and_looks/migration.sql
@@ -1,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_ProductToSet" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ProductToSet_AB_unique" ON "_ProductToSet"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ProductToSet_B_index" ON "_ProductToSet"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ProductToSet" ADD CONSTRAINT "_ProductToSet_A_fkey" FOREIGN KEY ("A") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProductToSet" ADD CONSTRAINT "_ProductToSet_B_fkey" FOREIGN KEY ("B") REFERENCES "Set"("id") ON DELETE CASCADE ON UPDATE CASCADE;

commit 41c82fecb703cb80a8c304cecce3d8aed1fc5f3e

fix(prisma): save specific product variants to sets
This patch updates the data model to save specific product variants to
sets instead of simply saving the entire product. This addresses an
oversight in a patchable way instead of trying to amend that previous
commit. I did not want to have to handle manual Prisma migrations, so
that is why this is a patch forward when ideally it should've been
amended unto the original commit.

Fixes: 240e3dcfda83 ("feat(prisma): products can be added to sets too")

app/components/filters.tsx
@@ -60,7 +60,6 @@ const MODEL_TO_ROUTE: Record<string, string> = {
   Show: '/shows',
   User: '/designers',
   Video: '/videos',
-  Set: '/sets',
 }
 
 //////////////////////////////////////////////////////////////////

app/filters.ts
@@ -47,7 +47,6 @@ type PrismaProductFilter =
   | Prisma.BrandListRelationFilter
   | Prisma.UserListRelationFilter
   | Prisma.LookListRelationFilter
-  | Prisma.SetListRelationFilter
 type PrismaShowFilter =
   | Prisma.IntFilter<'Show'>
   | Prisma.IntNullableFilter<'Show'>

prisma/migrations/20231001202758_save_specific_variants_to_sets_instead_of_products/migration.sql
@@ -1,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_ProductToSet` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_ProductToSet" DROP CONSTRAINT "_ProductToSet_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_ProductToSet" DROP CONSTRAINT "_ProductToSet_B_fkey";
+
+-- DropTable
+DROP TABLE "_ProductToSet";
+
+-- CreateTable
+CREATE TABLE "_SetToVariant" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_SetToVariant_AB_unique" ON "_SetToVariant"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_SetToVariant_B_index" ON "_SetToVariant"("B");
+
+-- AddForeignKey
+ALTER TABLE "_SetToVariant" ADD CONSTRAINT "_SetToVariant_A_fkey" FOREIGN KEY ("A") REFERENCES "Set"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_SetToVariant" ADD CONSTRAINT "_SetToVariant_B_fkey" FOREIGN KEY ("B") REFERENCES "Variant"("id") ON DELETE CASCADE ON UPDATE CASCADE;

prisma/schema.prisma
@@ -471,6 +471,9 @@ model Variant {
   // are different prices per size. Can be empty if the variant is sold out.
   prices Price[]
 
+  // The user-curated sets that the product variant is a part of.
+  sets Set[]
+
   // @todo each product can only have a single variant per color and size combo.
 }
 
@@ -655,9 +658,6 @@ model Product {
   // The product's looks (runway outfits that it was included in).
   looks Look[]
 
-  // The user-curated sets that the product is a part of.
-  sets Set[]
-
   // @todo perhaps we should also store the product's original source?
 
   // @todo products must have a unique name per brand(s).
@@ -702,8 +702,8 @@ model Set {
   // The set's looks.
   looks Look[]
 
-  // The set's products.
-  products Product[]
+  // The set's product variants.
+  variants Variant[]
 
   // A user can only have one set with a given name.
   @@unique([name, authorId])