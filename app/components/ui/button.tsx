7b7712fb6bdff22e771c3644f4fe7bfae2aeaae0
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-10-01 16:48:43 -0600

    fix(ui/button): show expanded state on outlined
    This patch updates the button styling to show the expanded state for the
    outlined variant just like how we do for the ghost variant.

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         destructive:
           'bg-red-500 text-gray-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-red-50 dark:hover:bg-red-900/90',
         outline:
-          'border border-gray-200 bg-white shadow-sm hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50',
+          'border border-gray-200 bg-white shadow-sm hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 aria-expanded:bg-gray-100 dark:aria-expanded:bg-gray-800 aria-expanded:text-gray-900 dark:aria-expanded:text-gray-50',
         secondary:
           'bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80',
         ghost:
