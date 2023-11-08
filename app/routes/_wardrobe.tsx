nicholaschiang/dolce

commit 7b6dc098edd45fa4fcb0210dd86ec8976c8fe67e

feat(wardrobe): add product saving buttons

app/routes/_wardrobe.tsx
@@ -1,7 +1,18 @@
-import { Outlet, Link, useLoaderData } from '@remix-run/react'
+import { Outlet, Link, useLoaderData, useFetcher } from '@remix-run/react'
 import { type DataFunctionArgs, type SerializeFrom } from '@vercel/remix'
+import { Minus } from 'lucide-react'
+import {
+  type Dispatch,
+  type SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from 'react'
+
+import { type Product } from 'routes/_wardrobe.products._index'
 
 import { Avatar } from 'components/avatar'
+import { Empty } from 'components/empty'
 import { Header } from 'components/header'
 import {
   Layout,
@@ -10,8 +21,10 @@ import {
   LayoutDivider,
 } from 'components/layout'
 import { badgeVariants } from 'components/ui/badge'
+import { Button } from 'components/ui/button'
 
 import { cn } from 'utils/cn'
+import { useUser } from 'utils/general'
 import { OWN_SET_NAME } from 'utils/set'
 import { getShowSeason } from 'utils/show'
 
@@ -26,25 +39,33 @@ export async function loader({ request }: DataFunctionArgs) {
     include: {
       author: true,
       images: true,
+      products: { include: { variants: { include: { images: true } } } },
       sets: { include: { author: true } },
       show: { include: { season: true, brand: true } },
     },
+    orderBy: { createdAt: 'desc' },
   })
   return looks
 }
 
+type ProductsContextT = [Product[], Dispatch<SetStateAction<Product[]>>]
+const ProductsContext = createContext<ProductsContextT>([[], () => {}])
+export const useProducts = () => useContext(ProductsContext)
+
 export default function WardrobePage() {
   return (
-    <Layout className='h-auto fixed inset-0'>
-      <LayoutLeft>
-        <Content />
-      </LayoutLeft>
-      <LayoutDivider />
-      <LayoutRight className='flex flex-col'>
-        <WardrobeHeader />
-        <Looks />
-      </LayoutRight>
-    </Layout>
+    <ProductsContext.Provider value={useState<Product[]>([])}>
+      <Layout className='h-auto fixed inset-0'>
+        <LayoutLeft>
+          <Content />
+        </LayoutLeft>
+        <LayoutDivider />
+        <LayoutRight className='flex flex-col'>
+          <WardrobeHeader />
+          <Looks />
+        </LayoutRight>
+      </Layout>
+    </ProductsContext.Provider>
   )
 }
 
@@ -68,11 +89,78 @@ function WardrobeHeader() {
   )
 }
 
+function CreateItem() {
+  const fetcher = useFetcher()
+  const [products, setProducts] = useContext(ProductsContext)
+  const user = useUser()
+  return (
+    <fetcher.Form
+      action='/api/looks'
+      method='post'
+      className='border border-gray-200 dark:border-gray-800 rounded-md col-span-2 bg-white dark:bg-gray-950'
+    >
+      <div className='p-2 flex items-center gap-1'>
+        <Avatar className='h-6 w-6 text-3xs' src={user} />
+        <strong className='font-medium text-xs'>{user.username}</strong>
+      </div>
+      <ul className='grid grid-cols-3 relative'>
+        {products.length === 0 && (
+          <>
+            <li className='w-full aspect-product' />
+            <Empty className='absolute inset-2 h-auto'>Select products</Empty>
+          </>
+        )}
+        {products.map((product) => (
+          <li
+            key={product.id}
+            className='w-full aspect-product bg-gray-100 dark:bg-gray-900 relative group'
+          >
+            <Button
+              className='absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity'
+              size='icon'
+              onClick={() =>
+                setProducts((prev) => {
+                  const idx = prev.findIndex((p) => p.id === product.id)
+                  return [...prev.slice(idx), ...prev.slice(idx + 1)]
+                })
+              }
+            >
+              <Minus className='w-3 h-3' />
+            </Button>
+            <img
+              src={product.variants[0].images[0].url}
+              alt=''
+              className='object-cover h-full w-full'
+            />
+          </li>
+        ))}
+      </ul>
+      {products.map((p) => (
+        <input type='hidden' name='productId' value={p.id} />
+      ))}
+      <div className='flex gap-2 p-2 justify-end'>
+        <Button size='sm' variant='ghost'>
+          Cancel
+        </Button>
+        <Button
+          size='sm'
+          type='submit'
+          variant='outline'
+          disabled={fetcher.state !== 'idle'}
+        >
+          Save
+        </Button>
+      </div>
+    </fetcher.Form>
+  )
+}
+
 function Looks() {
   const looks = useLoaderData<typeof loader>()
   return (
     <div className='flex-1 bg-gray-50 dark:bg-gray-900'>
       <ul className='grid grid-cols-2 gap-2 p-2'>
+        <CreateItem />
         {looks.map((look) => (
           <LookItem key={look.id} look={look} />
         ))}
@@ -97,12 +185,26 @@ function LookItem({ look }: { look: Look }) {
         />
         <strong className='font-medium'>{username}</strong>
       </div>
-      <div className='w-full aspect-person bg-gray-100 dark:bg-gray-900'>
-        <img
-          className='w-full h-full object-cover'
-          src={look.images[0].url}
-          alt=''
-        />
+      <div className='w-full aspect-person bg-gray-100 dark:bg-gray-900 overflow-y-auto overflow-x-hidden'>
+        {look.images.length ? (
+          <img
+            className='w-full h-full object-cover'
+            src={look.images[0].url}
+            alt=''
+          />
+        ) : (
+          look.products
+            .filter((p) => p.variants.flatMap((v) => v.images).length)
+            .map((p) => (
+              <div className='aspect-product w-full'>
+                <img
+                  className='w-full h-full object-cover'
+                  src={p.variants[0].images[0].url}
+                  alt=''
+                />
+              </div>
+            ))
+        )}
       </div>
       <div className='p-2 flex flex-col gap-2'>
         <p className='truncate'>

app/routes/_wardrobe.products._index.tsx
@@ -4,8 +4,11 @@ import {
   type DataFunctionArgs,
   type MetaFunction,
 } from '@vercel/remix'
+import { Plus, Minus } from 'lucide-react'
 import { useState, useRef } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
+
+import { useProducts } from 'routes/_wardrobe'
 
 import { Carousel, type CarouselItemProps } from 'components/carousel'
 import { FiltersBar, getWhere } from 'components/filters-bar'
@@ -22,6 +25,7 @@ import {
   ItemDescription,
 } from 'components/item'
 import { SaveMenu } from 'components/save-menu'
+import { Button } from 'components/ui/button'
 
 import { NAME, PRODUCT_ASPECT_RATIO, useOptionalUser } from 'utils/general'
 import { getBrandName } from 'utils/product'
@@ -125,7 +129,7 @@ export default function ProductsPage() {
 
 //////////////////////////////////////////////////////////////////
 
-type Product = SerializeFrom<typeof loader>['products'][number]
+export type Product = SerializeFrom<typeof loader>['products'][number]
 
 function ProductItem({ item: product }: InfiniteListItemProps<Product>) {
   // real users don't care about cents. most reputable brands won't include
@@ -140,16 +144,19 @@ function ProductItem({ item: product }: InfiniteListItemProps<Product>) {
   return (
     <Item to={`${product?.slug}/variants/${variant?.id}`}>
       <Carousel items={variant?.images} item={ProductImage}>
-        {user && variant && (
-          <SaveMenu
-            ref={ref}
-            saveAPI={`/api/variants/${variant.id}/save`}
-            createAPI={`/api/variants/${variant.id}/save/create`}
-            sets={variant.sets}
-            aria-label='Save product'
-            className='ml-auto pointer-events-auto'
-          />
-        )}
+        <div className='ml-auto flex flex-col gap-1'>
+          {product && <AddButton product={product} />}
+          {user && variant && (
+            <SaveMenu
+              ref={ref}
+              saveAPI={`/api/variants/${variant.id}/save`}
+              createAPI={`/api/variants/${variant.id}/save/create`}
+              sets={variant.sets}
+              aria-label='Save product'
+              className='pointer-events-auto'
+            />
+          )}
+        </div>
       </Carousel>
       {product && (
         <ItemContent>
@@ -164,6 +171,27 @@ function ProductItem({ item: product }: InfiniteListItemProps<Product>) {
         </ItemContent>
       )}
     </Item>
+  )
+}
+
+function AddButton({ product }: { product: Product }) {
+  const [products, setProducts] = useProducts()
+  const selected = products.some((p) => p.id === product.id)
+  return (
+    <Button
+      size='icon'
+      variant={selected ? 'default' : 'ghost'}
+      className='pointer-events-auto'
+      onClick={() =>
+        setProducts((prev) => {
+          const idx = prev.findIndex((p) => p.id === product.id)
+          if (idx < 0) return [product, ...prev]
+          return [...prev.slice(0, idx), ...prev.slice(idx + 1)]
+        })
+      }
+    >
+      {selected ? <Minus className='w-3 h-3' /> : <Plus className='w-3 h-3' />}
+    </Button>
   )
 }
 

app/routes/api.looks.ts
@@ -1,0 +1,30 @@
+import { type DataFunctionArgs } from '@vercel/remix'
+
+import { OWN_SET_NAME } from 'utils/set'
+
+import { prisma } from 'db.server'
+import { requireUserId } from 'session.server'
+
+export async function action({ request }: DataFunctionArgs) {
+  const userId = await requireUserId(request)
+  const data = await request.formData()
+  const productIds = data.getAll('productId').map((id) => Number(id))
+  const number = await prisma.look.findFirst({
+    where: { authorId: userId },
+    orderBy: { number: 'desc' },
+  })
+  const look = await prisma.look.create({
+    data: {
+      authorId: userId,
+      number: number ? number.number + 1 : 1,
+      products: { connect: productIds.map((id) => ({ id })) },
+      sets: {
+        connectOrCreate: {
+          where: { name_authorId: { name: OWN_SET_NAME, authorId: userId } },
+          create: { name: OWN_SET_NAME, authorId: userId },
+        },
+      },
+    },
+  })
+  return look
+}

commit 8a806570076070cd5c668a5d118fc4eecf6f5705

fix(wardrobe): hide wardrobe when not logged in
This patch simply hides the wardrobe side bar when the user is not
logged in. Ideally, this change should be replaced with a skeleton UI
that simply redirects to the login page on interaction (similar to the
"review" submission form in the show page).

Fixes: 7b6dc098edd4 ("feat(wardrobe): add product saving buttons")
Fixes: 76d9e2b6ba74 ("feat(app): add persistent wardrobe sidebar")

app/routes/_wardrobe.tsx
@@ -24,7 +24,7 @@ import { badgeVariants } from 'components/ui/badge'
 import { Button } from 'components/ui/button'
 
 import { cn } from 'utils/cn'
-import { useUser } from 'utils/general'
+import { useUser, useOptionalUser } from 'utils/general'
 import { OWN_SET_NAME } from 'utils/set'
 import { getShowSeason } from 'utils/show'
 
@@ -53,17 +53,22 @@ const ProductsContext = createContext<ProductsContextT>([[], () => {}])
 export const useProducts = () => useContext(ProductsContext)
 
 export default function WardrobePage() {
+  const user = useOptionalUser()
   return (
     <ProductsContext.Provider value={useState<Product[]>([])}>
       <Layout className='h-auto fixed inset-0'>
         <LayoutLeft>
           <Content />
         </LayoutLeft>
-        <LayoutDivider />
-        <LayoutRight className='flex flex-col'>
-          <WardrobeHeader />
-          <Looks />
-        </LayoutRight>
+        {user && (
+          <>
+            <LayoutDivider />
+            <LayoutRight className='flex flex-col'>
+              <WardrobeHeader />
+              <Looks />
+            </LayoutRight>
+          </>
+        )}
       </Layout>
     </ProductsContext.Provider>
   )
@@ -86,6 +91,20 @@ function WardrobeHeader() {
         Add products and looks to your wardrobe
       </p>
     </header>
+  )
+}
+
+function Looks() {
+  const looks = useLoaderData<typeof loader>()
+  return (
+    <div className='flex-1 bg-gray-50 dark:bg-gray-900'>
+      <ul className='grid grid-cols-2 gap-2 p-2'>
+        <CreateItem />
+        {looks.map((look) => (
+          <LookItem key={look.id} look={look} />
+        ))}
+      </ul>
+    </div>
   )
 }
 
@@ -152,20 +171,6 @@ function CreateItem() {
         </Button>
       </div>
     </fetcher.Form>
-  )
-}
-
-function Looks() {
-  const looks = useLoaderData<typeof loader>()
-  return (
-    <div className='flex-1 bg-gray-50 dark:bg-gray-900'>
-      <ul className='grid grid-cols-2 gap-2 p-2'>
-        <CreateItem />
-        {looks.map((look) => (
-          <LookItem key={look.id} look={look} />
-        ))}
-      </ul>
-    </div>
   )
 }
 