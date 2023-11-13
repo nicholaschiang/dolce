ca027a164b3141954524152a0f7df7f0961ffc44
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-11-12 18:59:19 -0700

    fix(image): always proxy images through API route
    This patch updates the front-end to always proxy `<Image>` images
    through a resource route to circumvent `cross-origin` policies.

--- app/routes/image.ts
+++ app/routes/image.ts
@@ -1,10 +1,12 @@
 import type { LoaderFunction } from '@vercel/remix'
 
+import { OPTIMIZE_IMAGES } from 'utils/general'
+
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url)
   const route = '_vercel/image'
   return fetch(
-    process.env.VERCEL
+    OPTIMIZE_IMAGES && process.env.VERCEL
       ? `${url.protocol}//${url.host}/${route}?${url.searchParams.toString()}`
       : decodeURIComponent(url.searchParams.get('url') ?? ''),
   )
