b6514652ffe1be110ebf9e741efa9b5e85d0d0e0
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2024-11-24 03:30:11 -0700

    feat(svelte-ui): add basic collections grid

--- svelte-ui/src/routes/+layout.svelte
+++ svelte-ui/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import "../app.css"
+  import { logStayCurious } from "$lib/curious"
   let { children } = $props()
+  $effect(logStayCurious)
 </script>
 
 {@render children()}
--- svelte-ui/src/routes/+page.server.ts
+++ svelte-ui/src/routes/+page.server.ts
@@ -1,11 +1,54 @@
+import { asc, ilike, sql } from "drizzle-orm"
+import * as tables from "$lib/server/db/schema/tables"
 import { db } from "$lib/server/db"
 
-export async function load() {
-  return {
-    collections: db.query.collection.findMany({
-      with: {
-        brand: true,
+const collectionsPageQuery = db.query.collection
+  .findMany({
+    columns: { id: true, name: true, level: true, location: true, sex: true },
+    with: {
+      brand: {
+        columns: {
+          id: true,
+          name: true,
+        },
       },
+      season: {
+        columns: { id: true, year: true, name: true },
+      },
+      looks: {
+        columns: { id: true, number: true },
+        with: {
+          images: {
+            limit: 1,
+            columns: { id: true, url: true },
+          },
+        },
+        orderBy: [asc(tables.look.number)],
+      },
+    },
+    where: ilike(tables.collection.name, sql.placeholder("search")),
+    orderBy: [
+      asc(tables.collection.name),
+      asc(tables.collection.level),
+      asc(tables.collection.sex),
+    ],
+    limit: sql.placeholder("limit"),
+  })
+  .prepare("collectionsPageQuery")
+
+export async function load({ url }) {
+  const search = url.searchParams.get("q") ?? ""
+  const query = collectionsPageQuery.getQuery()
+  let sql = query.sql
+  for (let i = 0; i < query.params.length; i++) {
+    sql = sql.replace(`$${i + 1}`, `${query.params[i]}`)
+  }
+  return {
+    search,
+    sql,
+    collections: collectionsPageQuery.execute({
+      search: `%${search}%`,
+      limit: search ? 100_000 : 100,
     }),
   }
 }
