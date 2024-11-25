a6357ba94cd0d38a1bb675ccb9610d012d8d8c77
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2024-11-24 18:07:00 -0700

    feat: use neon for managed postgres in production
    I will use Neon for simplicity until I switch to a local client-side
    IndexedDB or SQLite solution. Neon is what Vercel Postgres is. I figured
    I would use Neon directly.
    
    Ref: https://vercel.com/docs/storage/vercel-postgres#neon-partnership
    Ref: https://orm.drizzle.team/docs/get-started/neon-new

--- svelte-ui/src/lib/server/db/index.ts
+++ svelte-ui/src/lib/server/db/index.ts
@@ -1,15 +1,15 @@
-import { drizzle } from "drizzle-orm/node-postgres"
+import { drizzle as postgres } from "drizzle-orm/node-postgres"
+import { drizzle as neon } from "drizzle-orm/neon-http"
 import { env } from "$env/dynamic/private"
 
 import * as tables from "./schema/tables"
 import * as relations from "./schema/relations"
 
-const schema = { ...tables, ...relations }
-
 if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set")
 
-export const db = drizzle({
-  schema,
-  logger: true,
-  connection: env.DATABASE_URL,
-})
+const schema = { ...tables, ...relations }
+const config = { schema, logger: true, connection: env.DATABASE_URL }
+
+export const db = env.DATABASE_URL.includes("neon")
+  ? neon(config)
+  : postgres(config)
