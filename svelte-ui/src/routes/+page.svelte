924eed454d647ba51e852a0a1495d86c9d25f9c6
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2024-11-29 22:41:01 -0800

    fix(svelte-ui): tweak styling on home page links

--- svelte-ui/src/routes/+page.svelte
+++ svelte-ui/src/routes/+page.svelte
@@ -4,11 +4,15 @@
   import Link from "$lib/components/Link.svelte"
 </script>
 
-<div class="flex h-screen w-screen flex-col items-center justify-center gap-4 lowercase">
+<div class="flex h-screen w-screen flex-col items-center justify-center gap-4">
   <Library class="h-8 w-8" />
-  <div class="flex flex-col gap-2 items-center">
-  <Link href="/collections">Collections</Link>
-  <Link href="/products">Products</Link>
-  <Link href="https://github.com/nicholaschiang/dolce/tree/main/svelte-ui" target="_blank" rel="noopener noreferrer">Source</Link>
-</div>
+  <div class="flex flex-col items-center gap-1 lowercase tracking-tighter">
+    <Link href="/collections">Collections</Link>
+    <Link href="/products">Products</Link>
+    <Link
+      href="https://github.com/nicholaschiang/dolce/tree/main/svelte-ui"
+      target="_blank"
+      rel="noopener noreferrer">Source</Link
+    >
+  </div>
 </div>
