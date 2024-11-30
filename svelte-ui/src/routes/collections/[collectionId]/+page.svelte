nicholaschiang/dolce

commit 420d0d27b1d6c06b1d581604559f05d2431c6048

feat(svelte-ui): add basic landing page with links

svelte-ui/src/routes/collections/[collectionId]/+page.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <Header>
-  <a href="/">Collections</a>
+  <a href="/collections">Collections</a>
   <ChevronRight class="h-4 w-4 text-gray-200 dark:text-gray-800" />
   {#await data.data then data}
     <a href="/?q={encodeURIComponent(data.collection.brand.name)}"

svelte-ui/src/routes/+page.svelte
@@ -1,74 +1,14 @@
 <script lang="ts">
-  import { formatDate } from "$lib/formatDate"
-  import { formatLocation } from "$lib/formatLocation"
-  import { DEFAULT_COLLECTIONS_SEARCH } from "$lib/constants"
+  import { Library } from "lucide-svelte"
 
-  import Header from "$lib/components/Header.svelte"
-  import Subtitle from "$lib/components/Subtitle.svelte"
-  import SearchInput from "$lib/components/SearchInput.svelte"
-  import ResultsCount from "$lib/components/ResultsCount.svelte"
-
-  let { data } = $props()
-
-  type Collection = Awaited<typeof data.data>["collections"][number]
-  const sort = (a: Collection, b: Collection) =>
-    new Date(b.collectionDate ?? new Date()).valueOf() -
-    new Date(a.collectionDate ?? new Date()).valueOf()
+  import Link from "$lib/components/Link.svelte"
 </script>
 
-<Header>
-  <a href="/">Collections</a>
-</Header>
-<div class="flex flex-col gap-6 p-6">
-  <SearchInput defaultSearch={DEFAULT_COLLECTIONS_SEARCH} />
-  {#await data.data}
-    <ResultsCount loading />
-  {:then data}
-    <ResultsCount count={data.collections.length} time={data.time} />
-  {:catch error}
-    <ResultsCount {error} />
-  {/await}
-  <div
-    class="grid gap-x-2 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6"
-  >
-    {#await data.data then data}
-      {#each data.collections.sort(sort) as collection (collection.collectionId)}
-        <a
-          href="/collections/{collection.collectionId}"
-          class="flex flex-col gap-2 text-xs"
-        >
-          <div
-            class="flex aspect-person w-full items-center justify-center bg-gray-100 dark:bg-gray-800"
-          >
-            {#if collection.lookImageUrl}
-              <img
-                loading="lazy"
-                decoding="async"
-                src={collection.lookImageUrl}
-                alt="Look {collection.lookNumber} of {collection.collectionName}"
-                class="flex h-full w-full items-center justify-center object-cover"
-              />
-            {:else}
-              <span class="text-gray-200 dark:text-gray-700"
-                >No look images to show</span
-              >
-            {/if}
-          </div>
-          <div>
-            <h2>{collection.collectionName}</h2>
-            {#if collection.collectionDate}
-              <Subtitle>
-                {formatDate(new Date(collection.collectionDate))}
-              </Subtitle>
-            {/if}
-            {#if collection.collectionLocation}
-              <Subtitle>
-                {formatLocation(collection.collectionLocation)}
-              </Subtitle>
-            {/if}
-          </div>
-        </a>
-      {/each}
-    {/await}
-  </div>
+<div class="flex h-screen w-screen flex-col items-center justify-center gap-4 lowercase">
+  <Library class="h-8 w-8" />
+  <div class="flex flex-col gap-2 items-center">
+  <Link href="/collections">Collections</Link>
+  <Link href="/products">Products</Link>
+  <Link href="https://github.com/nicholaschiang/dolce/tree/main/svelte-ui" target="_blank" rel="noopener noreferrer">Source</Link>
+</div>
 </div>

svelte-ui/src/routes/collections/+page.svelte
@@ -1,0 +1,74 @@
+<script lang="ts">
+  import { formatDate } from "$lib/formatDate"
+  import { formatLocation } from "$lib/formatLocation"
+  import { DEFAULT_COLLECTIONS_SEARCH } from "$lib/constants"
+
+  import Header from "$lib/components/Header.svelte"
+  import Subtitle from "$lib/components/Subtitle.svelte"
+  import SearchInput from "$lib/components/SearchInput.svelte"
+  import ResultsCount from "$lib/components/ResultsCount.svelte"
+
+  let { data } = $props()
+
+  type Collection = Awaited<typeof data.data>["collections"][number]
+  const sort = (a: Collection, b: Collection) =>
+    new Date(b.collectionDate ?? new Date()).valueOf() -
+    new Date(a.collectionDate ?? new Date()).valueOf()
+</script>
+
+<Header>
+  <a href="/collections">Collections</a>
+</Header>
+<div class="flex flex-col gap-6 p-6">
+  <SearchInput defaultSearch={DEFAULT_COLLECTIONS_SEARCH} />
+  {#await data.data}
+    <ResultsCount loading />
+  {:then data}
+    <ResultsCount count={data.collections.length} time={data.time} />
+  {:catch error}
+    <ResultsCount {error} />
+  {/await}
+  <div
+    class="grid gap-x-2 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6"
+  >
+    {#await data.data then data}
+      {#each data.collections.sort(sort) as collection (collection.collectionId)}
+        <a
+          href="/collections/{collection.collectionId}"
+          class="flex flex-col gap-2 text-xs"
+        >
+          <div
+            class="flex aspect-person w-full items-center justify-center bg-gray-100 dark:bg-gray-800"
+          >
+            {#if collection.lookImageUrl}
+              <img
+                loading="lazy"
+                decoding="async"
+                src={collection.lookImageUrl}
+                alt="Look {collection.lookNumber} of {collection.collectionName}"
+                class="flex h-full w-full items-center justify-center object-cover"
+              />
+            {:else}
+              <span class="text-gray-200 dark:text-gray-700"
+                >No look images to show</span
+              >
+            {/if}
+          </div>
+          <div>
+            <h2>{collection.collectionName}</h2>
+            {#if collection.collectionDate}
+              <Subtitle>
+                {formatDate(new Date(collection.collectionDate))}
+              </Subtitle>
+            {/if}
+            {#if collection.collectionLocation}
+              <Subtitle>
+                {formatLocation(collection.collectionLocation)}
+              </Subtitle>
+            {/if}
+          </div>
+        </a>
+      {/each}
+    {/await}
+  </div>
+</div>

svelte-ui/src/lib/components/Header.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+  import { ChevronRight, Library } from "lucide-svelte"
+
   let { children } = $props()
 </script>
+
 <header
-  class="flex h-10 items-center gap-2 border-b border-gray-200 px-6 dark:border-gray-800 tracking-tighter text-lg lowercase sticky top-0 bg-white/80 dark:bg-gray-950/80 backdrop-blur-xl z-10"
+  class="sticky top-0 z-10 flex h-10 items-center gap-2 border-b border-gray-200 bg-white/80 px-6 text-lg lowercase tracking-tighter backdrop-blur-xl dark:border-gray-800 dark:bg-gray-950/80"
 >
+  <a href="/"><Library class="h-4 w-4" /></a>
+  <ChevronRight class="h-4 w-4 text-gray-200 dark:text-gray-800" />
   {@render children()}
 </header>

svelte-ui/src/lib/components/Link.svelte
@@ -1,0 +1,9 @@
+<script lang="ts">
+  let { children, ...props } = $props()
+</script>
+
+<a
+  {...props}
+  class="underline decoration-gray-400 transition-colors hover:decoration-inherit dark:decoration-gray-500"
+  >{@render children()}</a
+>

commit f991b4c1b02aa9050b5c6f8cfba1e618434b799e

fix(svelte-ui): update collection navigation links
Fixes: 420d0d27b1d6 ("feat(svelte-ui): add basic landing page with links")

svelte-ui/src/routes/collections/[collectionId]/+page.svelte
@@ -16,11 +16,11 @@
   <a href="/collections">Collections</a>
   <ChevronRight class="h-4 w-4 text-gray-200 dark:text-gray-800" />
   {#await data.data then data}
-    <a href="/?q={encodeURIComponent(data.collection.brand.name)}"
+    <a href="/collections?q={encodeURIComponent(data.collection.brand.name)}"
       >{data.collection.brand.name}</a
     >
     <ChevronRight class="h-4 w-4 text-gray-200 dark:text-gray-800" />
-    <a href="/?q={encodeURIComponent(formatSeasonName(data.collection.season))}"
+    <a href="/collections?q={encodeURIComponent(formatSeasonName(data.collection.season))}"
       >{formatSeasonName(data.collection.season)}
       {formatLevelName(data.collection.level)}</a
     >
@@ -33,7 +33,7 @@
       {#if data.collection.location}
         <span
           >(<a
-            href="/?q={encodeURIComponent(
+            href="/collections?q={encodeURIComponent(
               formatLocation(data.collection.location).split(',')[0] ?? "",
             )}">{formatLocation(data.collection.location)}</a
           >)</span